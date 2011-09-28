8724e2ecbfaefad6f583a467d13f50e07f41f960
subversion <svn@deadlybossmods.com>
2011-09-28 17:40:37 +0200

    nbluewiz via svn r6536: #4.3 mods : prevent lua error from live server.

--- DBM-BaradinHold/Alizabal.lua
+++ DBM-BaradinHold/Alizabal.lua
@@ -1,7 +1,9 @@
+if tonumber((select(2, GetBuildInfo()))) <= 14545 then return end
+
 local mod	= DBM:NewMod(339, "DBM-BaradinHold", nil, 74)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6531 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6536 $"):sub(12, -3))
 mod:SetCreatureID(12345)
 mod:SetModelID(21252)
 mod:SetZone()
--- DBM-DragonSoul/Zonozz.lua
+++ DBM-DragonSoul/Zonozz.lua
@@ -1,7 +1,9 @@
+if tonumber((select(2, GetBuildInfo()))) <= 14545 then return end
+
 local mod	= DBM:NewMod(324, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6528 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6536 $"):sub(12, -3))
 mod:SetCreatureID(12345)
 mod:SetModelID(39138)
 mod:SetZone()
