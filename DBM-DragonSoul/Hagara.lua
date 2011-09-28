1c32c64546493c34fc0c2fe281340b152df04ea3
subversion <svn@deadlybossmods.com>
2011-09-28 08:24:23 +0200

    arta via svn r6525: Encounter IDs + Zone ID from Dungeon Journal (model IDs coming later)

--- DBM-DragonSoul/Hagara.lua
+++ DBM-DragonSoul/Hagara.lua
@@ -1,8 +1,7 @@
---[[
-local mod	= DBM:NewMod( <EJ encounter ID >, "DBM-DragonSoul", nil, < EJ zone ID >)
+local mod	= DBM:NewMod(317, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6480 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6525 $"):sub(12, -3))
 mod:SetCreatureID(12345)
 mod:SetModelID(12345)
 mod:SetZone()
@@ -12,4 +11,3 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 )
---]]
