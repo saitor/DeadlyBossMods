c13856bd6351b400dc9a8c2b027d11f300e627ed
subversion <svn@deadlybossmods.com>
2011-09-28 11:44:12 +0200

    arta via svn r6529: 3 creature IDs and populated the Baine and Tyrande mod a little. PTR stuff :p

--- DBM-Party-Cataclysm/EndTime/Murozond.lua
+++ DBM-Party-Cataclysm/EndTime/Murozond.lua
@@ -1,8 +1,8 @@
 local mod	= DBM:NewMod("Murozond", "DBM-Party-Cataclysm", 12)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6526 $"):sub(12, -3))
-mod:SetCreatureID(12345)
+mod:SetRevision(("$Revision: 6529 $"):sub(12, -3))
+mod:SetCreatureID(54432)
 mod:SetModelID(38931)
 mod:SetZone()
 
