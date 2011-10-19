ea1532f25f4de402dbe393d5cbbb5189f81066b2
subversion <svn@deadlybossmods.com>
2011-10-19 09:45:41 +0200

    Omega via svn r6604: conclave is STILL bugging out badly and engaging on boss death, i'm not sure if it's still from a sync somehow, or the fact the anti spam code is only in ENGAGE trigger and maybe it's repulling conclave cause of combat regen or some other pull mechanic, either way, i put the anti spam in the start combat function itself, that way no matter what is triggering the pull, hopefully the darn mod will stop engaging a boss that's dead

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6603 $"):sub(12, -3),
+	Revision = ("$Revision: 6604 $"):sub(12, -3),
 	DisplayVersion = "4.10.0-alpha", -- the string that is shown as version
 	ReleaseRevision = 6582 -- the revision of the latest stable version that is available
 }
@@ -2189,10 +2189,7 @@ do
 		if combatInfo[LastZoneText] then
 			for i, v in ipairs(combatInfo[LastZoneText]) do
 				if v.type == "combat" and isBossEngaged(v.multiMobPullDetection or v.mob) then
-					-- HACK: makes sure that we don't detect a false pull if the event fires again when the boss dies...
-					if not v.mod.lastKillTime or GetTime() - v.mod.lastKillTime > 10 then
-						DBM:StartCombat(v.mod, 0)
-					end
+					DBM:StartCombat(v.mod, 0)
 				end
 			end
 		end
@@ -2200,9 +2197,7 @@ do
 		if combatInfo[LastZoneMapID] then
 			for i, v in ipairs(combatInfo[LastZoneMapID]) do
 				if v.type == "combat" and isBossEngaged(v.multiMobPullDetection or v.mob) then
-					if not v.mod.lastKillTime or GetTime() - v.mod.lastKillTime > 10 then
-						DBM:StartCombat(v.mod, 0)
-					end
+					DBM:StartCombat(v.mod, 0)
 				end
 			end
 		end
@@ -2307,6 +2302,8 @@ end
 
 function DBM:StartCombat(mod, delay, synced)
 	if not checkEntry(inCombat, mod) then
+		-- HACK: makes sure that we don't detect a false pull if the event fires again when the boss dies...
+		if mod.lastKillTime and GetTime() - mod.lastKillTime < 10 then return end
 		if not mod.combatInfo then return end
 		if mod.combatInfo.noCombatInVehicle and UnitInVehicle("player") then -- HACK
 			return
@@ -2377,8 +2374,6 @@ function DBM:EndCombat(mod, wipe)
 	if removeEntry(inCombat, mod) then
 		if not wipe then
 			mod.lastKillTime = GetTime()
-		end
-		if not wipe then
 			mod:UnregisterAllEvents()
 		end
 		mod:Stop()
