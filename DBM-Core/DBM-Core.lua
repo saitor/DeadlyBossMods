ebd621fe871cdd39c8e505f14d81f1b99486a1c8
subversion <svn@deadlybossmods.com>
2011-10-28 22:59:28 +0200

    Omega via svn r6637: with a new transcriptor log of council, was able to remove dependancy on localized phase change triggers, should now work seemlessly in ANY language off the UNIT events. 2 timers might be off by a second at most while i wait to redo fight on heroic next week but overall it should be a quality improvement code and client compatablity wise.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6636 $"):sub(12, -3),
+	Revision = ("$Revision: 6637 $"):sub(12, -3),
 	DisplayVersion = "4.10.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 6615 -- the revision of the latest stable version that is available
 }
