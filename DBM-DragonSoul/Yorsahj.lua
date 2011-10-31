06b93c5f554599086d5acdc1d208b4e3635dea8b
subversion <svn@deadlybossmods.com>
2011-10-31 21:04:33 +0100

    tandanu via svn r6649: fix ooze colors for 4 colors on heroic

--- DBM-DragonSoul/Yorsahj.lua
+++ DBM-DragonSoul/Yorsahj.lua
@@ -3,7 +3,7 @@ if tonumber((select(2, GetBuildInfo()))) <= 14545 then return end
 local mod	= DBM:NewMod(325, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6647 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6649 $"):sub(12, -3))
 mod:SetCreatureID(55312)
 mod:SetModelID(39101)
 mod:SetZone()
@@ -91,7 +91,7 @@ end
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellID)
 	if uId ~= "boss1" then return end--Anti spam to ignore all other args (like target/focus/mouseover)
 	if oozeColors[spellID] then
-		warnOozes:Show(oozeColors[spellID][1], oozeColors[spellID][2], oozeColors[spellID][3], oozeColors[spellID][4] or "")--According to EJ, there are 4 on heroic but don't know the spellIDs for color combinations of 4 yet. Could not find on wowhead either.
+		warnOozes:Show(table.concat(oozeColors[spellID], ", "))
 --		timerVoidBoltCD:Start(40)
 		timerOozesCD:Start()
 	end
