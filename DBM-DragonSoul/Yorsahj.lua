134e8d46c317ab8f2421b11e4a07fff10c0e77ec
subversion <svn@deadlybossmods.com>
2011-10-31 21:06:12 +0100

    Omega via svn r6650: also add in special warning i forgot to actually add

--- DBM-DragonSoul/Yorsahj.lua
+++ DBM-DragonSoul/Yorsahj.lua
@@ -3,7 +3,7 @@ if tonumber((select(2, GetBuildInfo()))) <= 14545 then return end
 local mod	= DBM:NewMod(325, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6649 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6650 $"):sub(12, -3))
 mod:SetCreatureID(55312)
 mod:SetModelID(39101)
 mod:SetZone()
@@ -92,6 +92,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellID)
 	if uId ~= "boss1" then return end--Anti spam to ignore all other args (like target/focus/mouseover)
 	if oozeColors[spellID] then
 		warnOozes:Show(table.concat(oozeColors[spellID], ", "))
+		specWarnOozes:Show()
 --		timerVoidBoltCD:Start(40)
 		timerOozesCD:Start()
 	end
