78e37da27a201f14618bb03e33924d1c18b26f53
subversion <svn@deadlybossmods.com>
2011-11-07 10:40:29 +0100

    arta via svn r6674: Madness of Deathwing model ID

--- DBM-DragonSoul/MadnessDeathwing.lua
+++ DBM-DragonSoul/MadnessDeathwing.lua
@@ -3,9 +3,9 @@ if tonumber((select(2, GetBuildInfo()))) <= 14545 then return end
 local mod	= DBM:NewMod(333, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6599 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6674 $"):sub(12, -3))
 mod:SetCreatureID(56173)
-mod:SetModelID(39355)
+mod:SetModelID(40087)
 mod:SetZone()
 mod:SetUsedIcons()
 
