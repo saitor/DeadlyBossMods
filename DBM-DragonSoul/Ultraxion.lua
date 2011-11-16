13dbb1f9ed06b872873974c3d73f307c5aed54f5
subversion <svn@deadlybossmods.com>
2011-11-16 23:17:39 +0100

    Omega via svn r6695: oops forgot to account for shorter hour of twilight cast on heroic

--- DBM-DragonSoul/Ultraxion.lua
+++ DBM-DragonSoul/Ultraxion.lua
@@ -3,7 +3,7 @@ if tonumber((select(2, GetBuildInfo()))) <= 14545 then return end
 local mod	= DBM:NewMod(331, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6694 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6695 $"):sub(12, -3))
 mod:SetCreatureID(55294)
 mod:SetModelID(39099)
 mod:SetZone()
@@ -61,7 +61,7 @@ function mod:SPELL_CAST_START(args)
 		specWarnHourofTwilight:Show()
 		timerHourofTwilightCD:Start()
 		if self:IsDifficulty("heroic10", "heroic25") then
-			timerFadingLightCD:Start(15)
+			timerFadingLightCD:Start(13)
 		else
 			timerFadingLightCD:Start(20)--Same in raid finder too? too many difficulties now
 		end
