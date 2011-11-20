00d1b1362979c924d0540343e93fb6d5f00caa7b
subversion <svn@deadlybossmods.com>
2011-11-20 22:01:54 +0100

    Omega via svn r6708: disable LFR difficulty for now it's not functional and actualloy breaks normal25 mods

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6707 $"):sub(12, -3),
+	Revision = ("$Revision: 6708 $"):sub(12, -3),
 	DisplayVersion = "4.10.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 6615 -- the revision of the latest stable version that is available
 }
@@ -3147,10 +3147,10 @@ end
 
 function bossModPrototype:GetDifficulty() 
 	local _, instanceType, difficulty, _, maxPlayers, playerDifficulty, isDynamicInstance = GetInstanceInfo()
-	local inLFGParty = GetLFGInfoServer()
-	if inLFGParty and difficulty == 2 and instanceType == "raid" and maxPlayers == 25 then
-		return "lfr25"
-	elseif difficulty == 1 then 
+--	local inLFGParty = GetLFGInfoServer()
+--[[	if inLFGParty and difficulty == 2 and instanceType == "raid" and maxPlayers == 25 then
+		return "lfr25"--]]
+	if difficulty == 1 then 
 		return instanceType == "raid" and "normal10" or "normal5"
 	elseif difficulty == 2 then 
 		return instanceType == "raid" and "normal25" or "heroic5"
