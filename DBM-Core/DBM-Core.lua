51377ba0aa0ef412911376c73dffeb17e474eb42
subversion <svn@deadlybossmods.com>
2011-11-30 14:58:12 +0100

    Swix via svn r6744: ruRU update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6743 $"):sub(12, -3),
+	Revision = ("$Revision: 6744 $"):sub(12, -3),
 	DisplayVersion = "4.10.1 alpha", -- the string that is shown as version
 	ReleaseRevision = 6737 -- the revision of the latest stable version that is available
 }
