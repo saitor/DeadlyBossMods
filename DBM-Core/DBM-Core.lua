6a59c4e8adbd141c932f8f1e505791bcf5148aec
subversion <svn@deadlybossmods.com>
2011-12-01 21:45:36 +0100

    Snamor via svn r6765: esES update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,11 +41,10 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6763 $"):sub(12, -3),
-	DisplayVersion = "4.10.2", -- the string that is shown as version
+	Revision = ("$Revision: 6765 $"):sub(12, -3),
+	DisplayVersion = "4.10.3 alpha", -- the string that is shown as version
 	ReleaseRevision = 6763 -- the revision of the latest stable version that is available
 }
-
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
 -- Some functions that should be using ReleaseRevision still use this one, so we will just keep it and set to ReleaseRevision
