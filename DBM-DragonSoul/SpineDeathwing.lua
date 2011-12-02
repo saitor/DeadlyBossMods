0966c57490e97d188228cb57bcaf35a6c74c396b
subversion <svn@deadlybossmods.com>
2011-12-02 16:07:47 +0100

    Omega via svn r6778: Account for firstAspect rule which seems new (did not see this behavior on PTR) Based on logs, all abilities come 15 seconds earlier on first assaulted dragon aspect only. Changed pull trigger for spine to yell, so please localizers find your yell, because otherwise mod won't work (ie registereventsincombat stuff)

--- DBM-DragonSoul/SpineDeathwing.lua
+++ DBM-DragonSoul/SpineDeathwing.lua
@@ -1,13 +1,13 @@
 local mod	= DBM:NewMod(318, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6770 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6778 $"):sub(12, -3))
 mod:SetCreatureID(53879)
 mod:SetModelID(35268)
 mod:SetZone()
 mod:SetUsedIcons(6, 5, 4, 3, 2, 1)
 
-mod:RegisterCombat("combat")--May need to use a diff engage trigger, probably chat message.
+mod:RegisterCombat("yell", L.Pull)--Engage trigger comes 30 seconds after encounter starts, because of this, the mod can miss the first round of ability casts such as first grip targets. have to use yell
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
