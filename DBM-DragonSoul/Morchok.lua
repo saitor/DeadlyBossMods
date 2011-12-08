722408b598892db19e5125ffdb49df16534d2d18
subversion <svn@deadlybossmods.com>
2011-12-09 00:39:20 +0100

    Omega via svn r6860: also fix some fail ass math in morchok berserk timer, it's 7 min not 6

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(311, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6858 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6860 $"):sub(12, -3))
 mod:SetCreatureID(55265)
 mod:SetModelID(39094)
 mod:SetZone()
@@ -42,7 +42,7 @@ local timerKohcromCD	= mod:NewTimer(5, "KohcromCD", 55342)--Enable when we have 
 --We will not start timers using Kohcrom's casts, it'll waste WAY too much space.
 --EJ is pretty clear, they are cast shortly after morchok, always. So echo timer is perfect and clean solution.
 
-local berserkTimer		= mod:NewBerserkTimer(360)
+local berserkTimer		= mod:NewBerserkTimer(420)
 
 mod:AddBoolOption("RangeFrame", false)--For achievement
 
