d4cc6a60f17df73843b746f5096d0b5d80e1379a
subversion <svn@deadlybossmods.com>
2011-12-12 11:29:18 +0100

    arta via svn r6901: Oozes are on a 1:15 timer instead of 1:30

--- DBM-DragonSoul/Yorsahj.lua
+++ DBM-DragonSoul/Yorsahj.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(325, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6882 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6901 $"):sub(12, -3))
 mod:SetCreatureID(55312)
 mod:SetModelID(39101)
 mod:SetZone()
@@ -116,7 +116,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellID)
 			specWarnOozes:Show()
 			timerOozesActive:Start()
 --			timerVoidBoltCD:Start(40)
-			timerOozesCD:Start()
+			timerOozesCD:Start(75)
 		end
 	else
 		if oozeColors[spellID] then
