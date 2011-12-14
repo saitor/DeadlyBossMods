3d13ab8638a69a905d6c663d4c7e9fb1ccfef3ef
subversion <svn@deadlybossmods.com>
2011-12-14 07:54:54 +0100

    Omega via svn r6926: Add audio countdown to hagara's specials. enabled audio countdown on ultraxion now that i figured out why it was always wrong, stupid -5 isn't needed in the prototype, it's handled automatically, that's why it was always 5 seconds off, duh

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6925 $"):sub(12, -3),
+	Revision = ("$Revision: 6926 $"):sub(12, -3),
 	DisplayVersion = "4.10.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 6839 -- the revision of the latest stable version that is available
 }
