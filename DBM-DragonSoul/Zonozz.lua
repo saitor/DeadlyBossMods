c957247f95b884cf380d59fd7d9941da8a5fd287
subversion <svn@deadlybossmods.com>
2011-12-15 13:54:10 +0100

    nbluewiz via svn r6942: # Zonozz : remove GazeWarn option. # lots of koKR update.

--- DBM-DragonSoul/Zonozz.lua
+++ DBM-DragonSoul/Zonozz.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(324, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6936 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6942 $"):sub(12, -3))
 mod:SetCreatureID(55308)
 mod:SetModelID(39138)
 mod:SetZone()
@@ -23,7 +23,6 @@ local warnVoidDiffusion			= mod:NewStackAnnounce(106836, 2)
 local warnFocusedAnger			= mod:NewStackAnnounce(104543, 3, nil, false)
 local warnPsychicDrain			= mod:NewSpellAnnounce(104322, 4, nil, mod:IsTank())
 local warnShadows				= mod:NewSpellAnnounce(103434, 3)
-local warnShadowGaze			= mod:NewAnnounce("warnShadowGaze", 2, 104604)
 
 local specWarnVoidofUnmaking	= mod:NewSpecialWarningSpell(103571, nil, nil, nil, true)
 local specWarnBlackBlood		= mod:NewSpecialWarningSpell(104378, nil, nil, nil, true)
