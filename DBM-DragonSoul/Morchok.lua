d4c35304469104557480ea2949688b60631ac4c4
subversion <svn@deadlybossmods.com>
2011-12-15 12:18:40 +0100

    nbluewiz via svn r6938: # Morchok : re-added crystal check for next stomp. because he do not cast stomps during blood phase :)

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(311, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 6933 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 6938 $"):sub(12, -3))
 mod:SetCreatureID(55265)
 mod:SetModelID(39094)
 mod:SetZone()
@@ -97,12 +97,14 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(103414, 108571, 109033, 109034) then
 		if args:GetSrcCreatureID() == 55265 then
 			warnStomp:Show()
-			timerStomp:Start()
-			if UnitExists("boss2") then
-				if self:IsDifficulty("heroic25") then
-					timerKohcromCD:Start(5, args.spellName)--I went through another dozon logs on WoL Expression editor. it comes earlier on 25 man. because 6 was way wrong
-				else
-					timerKohcromCD:Start(6, args.spellName)--it was 6 in all the 10 man logs though.
+			if crystalCount < 3 or (firstPhase and crystalCount < 2) then
+				timerStomp:Start()
+				if UnitExists("boss2") then
+					if self:IsDifficulty("heroic25") then
+						timerKohcromCD:Start(5, args.spellName)--I went through another dozon logs on WoL Expression editor. it comes earlier on 25 man. because 6 was way wrong
+					else
+						timerKohcromCD:Start(6, args.spellName)--it was 6 in all the 10 man logs though.
+					end
 				end
 			end
 		else
