a3ff649febe5ed544209c1dea02c3c5853cc2506
subversion <svn@deadlybossmods.com>
2012-01-11 12:39:32 +0100

    nbluewiz via svn r7149: # Blackhorn : cancel countdown on phase 2

--- DBM-DragonSoul/Blackhorn.lua
+++ DBM-DragonSoul/Blackhorn.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(332, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7136 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7149 $"):sub(12, -3))
 mod:SetCreatureID(56598)--56427 is Boss, but engage trigger needs the ship which is 56598
 mod:SetModelID(39399)
 mod:SetZone()
@@ -60,6 +60,7 @@ local function Phase2Delay()
 	mod:UnscheduleMethod("AddsRepeat")
 	timerAdd:Cancel()
 	timerTwilightOnslaughtCD:Cancel()
+	twilightOnslaughtCountdown:Cancel()
 	timerSapperCD:Cancel()
 	timerRoarCD:Start(11)
 	timerTwilightFlamesCD:Start(12)
