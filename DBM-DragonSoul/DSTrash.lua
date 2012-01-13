6f4e4ba142ae775616ae82ce7c393e04750dbca6
subversion <svn@deadlybossmods.com>
2012-01-13 01:46:37 +0100

    Omega via svn r7169: fix a bug with mod assocation (DragonSoul not Dragonsoul)

--- DBM-DragonSoul/DSTrash.lua
+++ DBM-DragonSoul/DSTrash.lua
@@ -1,7 +1,7 @@
-local mod	= DBM:NewMod("DSTrash", "DBM-Dragonsoul")
+local mod	= DBM:NewMod("DSTrash", "DBM-DragonSoul")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7167 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7169 $"):sub(12, -3))
 --mod:SetModelID(29539)
 
 mod:RegisterEvents(
