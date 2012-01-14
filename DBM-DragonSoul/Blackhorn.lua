05a554d6351edd64d347c422b7ad31c346f79937
subversion <svn@deadlybossmods.com>
2012-01-14 11:35:09 +0100

    nbluewiz via svn r7175: # Blackhorn : tweak first Roar timer. # Spine of Deathwing : added experimental Residue (concerned with Absorb Blood) stack warning stuff. (heroic raid team suggests) maybe it will be better showing on info frame?

--- DBM-DragonSoul/Blackhorn.lua
+++ DBM-DragonSoul/Blackhorn.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(332, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7174 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7175 $"):sub(12, -3))
 mod:SetCreatureID(56598)--56427 is Boss, but engage trigger needs the ship which is 56598
 mod:SetModelID(39399)
 mod:SetZone()
@@ -71,7 +71,7 @@ local function Phase2Delay()
 	twilightOnslaughtCountdown:Cancel()
 	timerBroadsideCD:Cancel()
 	timerSapperCD:Cancel()
-	timerRoarCD:Start(11)
+	timerRoarCD:Start(10)
 	timerTwilightFlamesCD:Start(12)
 	timerShockwaveCD:Start(13)--13-16 second variation
 	timerConsumingShroud:Start(45)	-- 45seconds once P2 starts?
