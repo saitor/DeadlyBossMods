df64a3e52ab36a96dbe1105de1299f84adb89a1e
subversion <svn@deadlybossmods.com>
2012-01-21 05:22:44 +0100

    Omega via svn r7217: apply same fix to dps too

--- DBM-DragonSoul/Ultraxion.lua
+++ DBM-DragonSoul/Ultraxion.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(331, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7216 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7217 $"):sub(12, -3))
 mod:SetCreatureID(55294)
 mod:SetModelID(39099)
 mod:SetZone()
@@ -179,7 +179,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		self:Schedule(0.3, warnFadingLightTargets)
 	elseif args:IsSpellID(109075, 110078, 110079, 110080) then--Non Tank IDs
 		fadingLightTargets[#fadingLightTargets + 1] = args.destName
-		if args:IsPlayer() then
+		if args:IsPlayer() or UnitDebuff("player", GetSpellInfo(109416)) then
 			local _, _, _, _, _, duration, expires = UnitDebuff("player", args.spellName)--Find out what our specific fading light is
 			specWarnFadingLight:Show()
 			FadingLightCountdown:Start(duration-1)
