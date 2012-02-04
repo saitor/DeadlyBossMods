3e29b5f7e6c2bde0f1a30b1ccca2b3e2994841d8
subversion <svn@deadlybossmods.com>
2012-02-04 02:06:47 +0100

    Omega via svn r7263: Think this should cover the DS mod conversions. to make sure the alpha is at least functional there, gonna work on rest of raids now.

--- DBM-DragonSoul/Blackhorn.lua
+++ DBM-DragonSoul/Blackhorn.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(332, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7233 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7263 $"):sub(12, -3))
 mod:SetCreatureID(56598)--56427 is Boss, but engage trigger needs the ship which is 56598
 mod:SetMainBossID(56427)
 mod:SetModelID(39399)
@@ -248,14 +248,14 @@ function mod:SPELL_SUMMON(args)
 	end
 end
 
-function mod:SPELL_DAMAGE(args)
-	if args:IsSpellID(108076, 109222, 109223, 109224) then
-		if args:IsPlayer() and GetTime() - lastFlames > 3  then
+function mod:SPELL_DAMAGE(sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlag, spellId)
+	if spellId == 108076 or spellId == 109222 or spellId == 109223 or spellId == 109224 then
+		if destName == UnitName("player") and GetTime() - lastFlames > 3  then
 			specWarnTwilightFlames:Show()
 			lastFlames = GetTime()
 		end
-	elseif args:IsSpellID(110095) then
-		if args:IsPlayer() and GetTime() - lastFlames > 3  then
+	elseif spellId == 110095 then
+		if destName == UnitName("player") and GetTime() - lastFlames > 3  then
 			specWarnDeckFire:Show()
 			lastFlames = GetTime()
 		end
