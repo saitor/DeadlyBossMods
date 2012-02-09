d1c0bf26d5af837fabd6c37534746c4892afb70b
subversion <svn@deadlybossmods.com>
2012-02-09 16:12:01 +0100

    nbluewiz via svn r7326: # Morchok : prevent double warn on Furious warning.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,8 +42,8 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7325 $"):sub(12, -3)),
-	DisplayVersion = "4.10.10", -- the string that is shown as version
+	Revision = tonumber(("$Revision: 7326 $"):sub(12, -3)),
+	DisplayVersion = "4.10.11 alpha", -- the string that is shown as version
 	ReleaseRevision = 7325 -- the revision of the latest stable version that is available
 }
 
