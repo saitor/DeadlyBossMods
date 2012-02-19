887faaaef82ab19efcf74fec6739c8d8ab5c56ef
subversion <svn@deadlybossmods.com>
2012-02-19 09:05:17 +0100

    nbluewiz via svn r7380: # Madness of Deathwing : Remove Parasite canceling. I have bad log. sorry. (-

--- DBM-DragonSoul/MadnessDeathwing.lua
+++ DBM-DragonSoul/MadnessDeathwing.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(333, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7379 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7380 $"):sub(12, -3))
 mod:SetCreatureID(56173)
 mod:SetModelID(40087)
 mod:SetZone()
@@ -185,7 +185,6 @@ function mod:SPELL_AURA_APPLIED(args)
 		timerHemorrhageCD:Cancel()--Does this one cancel in event you super overgear this and stomp his ass this fast?
 		timerCataclysm:Cancel()
 		timerCataclysmCD:Cancel()
-		timerParasiteCD:Cancel()
 	elseif args:IsSpellID(106400) then
 		warnImpale:Show(args.destName)
 		timerImpale:Start(args.destName)
