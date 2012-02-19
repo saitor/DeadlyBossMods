saitor/DeadlyBossMods

commit 1b27665fade3a8c73eab1ae6d6cb7cc3550cb248

Updated generic yells also accept custom yells.
git-svn-id: svn://dev.deadlybossmods.com/dbm4/trunk@5518 a6602f14-c1bc-49d1-8d65-da3458cc0765

DBM-BlackwingDescent/DarkIronGolemCouncil.lua
@@ -46,7 +46,7 @@ local specWarnBarrier			= mod:NewSpecialWarningSpell(79582, not mod:IsHealer())
 local specWarnAcquiringTarget	= mod:NewSpecialWarningYou(92037)
 local yellAcquiringTarget		= mod:NewYell(92037)
 local specWarnEncasingShadows	= mod:NewSpecialWarningTarget(92023, false)--Heroic Ability
-mod:AddBoolOption("YellOnTargetLock", true, "announce")--This one isn't generic because it uses custom text.
+local yellEncasingShadows		= mod:NewYell(92023)--Custom string L.YellTargetLock
 --Electron
 local specWarnUnstableShield	= mod:NewSpecialWarningSpell(79900, not mod:IsHealer())
 local specWarnConductor			= mod:NewSpecialWarningYou(79888)
@@ -175,7 +175,7 @@ end
 
 function mod:CheckEncasing() -- prevent two yells at a time
 	if encasing and self.Options.YellOnTargetLock then
-		SendChatMessage(L.YellTargetLock, "SAY")
+		yellEncasingShadows:Yell(L.YellTargetLock)
 	elseif not encasing then
 		yellAcquiringTarget:Yell()
 	end

DBM-Core/DBM-Core.lua
@@ -3017,9 +3017,9 @@ do
 		return obj
 	end
 
-	function yellPrototype:Yell(...)
+	function yellPrototype:Yell(yellText)
 		if not self.option or self.mod.Options[self.option] then
-			SendChatMessage(self.text, "SAY")
+			SendChatMessage(yellText or self.text, "SAY")
 		end
 	end
 

DBM-BlackwingDescent/Atramedes.lua
@@ -32,7 +32,7 @@ local specWarnSearingFlame	= mod:NewSpecialWarningSpell(77840)
 local specWarnSonarPulse	= mod:NewSpecialWarningSpell(92411, false)
 local specWarnTracking		= mod:NewSpecialWarningYou(78092)
 local specWarnPestered		= mod:NewSpecialWarningYou(92685)
-mod:AddBoolOption("YellOnPestered", true, "announce")--Custom string so we don't use generic here.
+local yellPestered			= mod:NewYell(92685)--Custom string L.YellPestered
 local specWarnObnoxious		= mod:NewSpecialWarningInterrupt(92702, false)
 local specWarnAddTargetable	= mod:NewSpecialWarning("specWarnAddTargetable", false)
 
@@ -151,8 +151,6 @@ function mod:UNIT_AURA(uId)
 	if UnitDebuff("player", pestered) then
 		pesteredWarned = true--This aura is a periodic trigger, so we don't want to spam warn for it.
 		specWarnPestered:Show()
-		if self.Options.YellOnPestered then
-			SendChatMessage(L.YellPestered, "SAY")
-		end
+		yellPestered:Yell(L.YellPestered)
 	end
 end

DBM-BlackwingDescent/localization.en.lua
@@ -55,7 +55,6 @@ L:SetOptionLocalization({
 	timerNefAblity				= "Show timer for heroic ability buff cooldown",
 	SpecWarnActivated			= "Show special warning when new boss activated",
 	specWarnGenerator			= "Show special warning when a boss gains $spell:91557",
-	YellOnTargetLock			= "Yell when you are affected by $spell:92023",
 	AcquiringTargetIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79501),
 	ConductorIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79888),
 	ShadowConductorIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(92053)
@@ -165,7 +164,6 @@ L:SetOptionLocalization({
 	TimerAirphase			= "Show timer for next airphase",
 	TimerGroundphase		= "Show timer for next groundphase",
 	InfoFrame				= "Show info frame for sound levels",
-	YellOnPestered			= "Yell when you are affected by $spell:92685",
 	TrackingIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(78092)
 })
 

DBM-BlackwingDescent/localization.es.lua
@@ -58,7 +58,6 @@ L:SetOptionLocalization({
 	timerNefAblity				= "Mostrar tiempo de CoolDown de las habilidades de bufos en heroico",
 	SpecWarnActivated			= "Mostrar aviso especial cuando se activa un nuevo boss",
 	specWarnGenerator			= "Mostrar aviso especial cuando un boss gana $spell:91557",
-	YellOnTargetLock			= "Gritar cuando tengas $spell:92023",
 	AcquiringTargetIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79501),
 	ConductorIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79888),
 	ShadowConductorIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(92053)
@@ -168,7 +167,6 @@ L:SetOptionLocalization({
 	TimerAirphase		= "Mostrar tiempo para siguiente Fase aerea",
 	TimerGroundphase	= "Mostrar tiempo para siguiente Fase en tierra",
 	InfoFrame			= "Mostrar información de los niveles de sonido",
-	YellOnPestered			= "Gritar cuando tengas $spell:92685",
 	TrackingIcon		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(78092)
 })
 

DBM-BlackwingDescent/localization.kr.lua
@@ -47,7 +47,7 @@ L:SetTimerLocalization({
 	timerArcaneBlowbackCast		= "폭발!",
 	timerShadowConductorCast	= "암흑 전도체 변환",
 	timerNefAblity				= "스킬 강화 쿨다운",
-	timerArcaneLockout			= "비전 파괴자 침묵"
+	timerArcaneLockout			= "비전 파괴자 쿨다운"
 })
 
 L:SetOptionLocalization({
@@ -57,7 +57,6 @@ L:SetOptionLocalization({
 	timerArcaneLockout			= "$spell:91542 침묵 타이머 보기",
 	SpecWarnActivated			= "새로운 보스가 활성화 될 때 특수 경고 보기",
 	specWarnGenerator			= "보스가 $spell:91557의 영향을 받을 경우 특수 경고 보기",
-	YellOnTargetLock			= "$spell:92023 외치기",
 	AcquiringTargetIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79501),
 	ConductorIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79888),
 	ShadowConductorIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(92053)
@@ -167,7 +166,6 @@ L:SetOptionLocalization({
 	TimerAirphase			= "다음 공중 단계 경고 보기",
 	TimerGroundphase		= "다음 지상 단계 경고 보기",
 	InfoFrame				= "소음계 정보 프레임 보기",
-	YellOnPestered			= "$spell:92685 외치기",
 	TrackingIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(78092)
 })
 

DBM-BlackwingDescent/localization.ru.lua
@@ -57,7 +57,6 @@ L:SetOptionLocalization({
 	timerNefAblity				= "Показывать таймер восстановления баффа (героический режим)",
 	SpecWarnActivated			= "Показывать особое предупреждение при активации нового босса",
 	specWarnGenerator			= "Показывать особое предупреждение, когда босс стоит в $spell:91557",
-	YellOnTargetLock			= "Кричать, когда на вас $spell:92023",
 	AcquiringTargetIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79501),
 	ConductorIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79888),
 	ShadowConductorIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(92053)
@@ -167,7 +166,6 @@ L:SetOptionLocalization({
 	TimerAirphase			= "Показывать таймер до следующей воздушной фазы",
 	TimerGroundphase		= "Показывать таймер до следующей наземной фазы",
 	InfoFrame				= "Показывать информационное окно для уровня звуков",
-	YellOnPestered			= "Кричать, когда на вас $spell:92685",
 	TrackingIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(78092)
 })
 

DBM-BlackwingDescent/localization.tw.lua
@@ -57,7 +57,6 @@ L:SetOptionLocalization({
 	timerNefAblity			= "為困難技能增益冷卻顯示計時器",
 	SpecWarnActivated		= "當新首領啟動時顯示特別警告",
 	specWarnGenerator		= "當首領獲得$spell:91557時顯示特別警告",
-	YellOnTargetLock		= "中了$spell:92023時大喊",
 	AcquiringTargetIcon		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79501),
 	ConductorIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(79888),
 	BombTargetIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(80094),
@@ -169,7 +168,6 @@ L:SetOptionLocalization({
 	TimerAirphase			= "為下一次 空中階段顯示計時器",
 	TimerGroundphase		= "為下一次 地上階段顯示計時器",
 	InfoFrame			= "為音波值顯示資訊框架",
-	YellOnPestered			= "中了$spell:92685時大喊",
 	TrackingIcon			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(78092)
 })
 

commit dc55dad0a65a38252fa8ac322a34796652ba0246

ebmor via svn r7382: BWD Omnotron Defense System: fix Encasing Shadows yell (seems to be broken since r5518)

DBM-BlackwingDescent/DarkIronGolemCouncil.lua
@@ -2,7 +2,7 @@
 local mod	= DBM:NewMod("DarkIronGolemCouncil", "DBM-BlackwingDescent")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7271 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7382 $"):sub(12, -3))
 mod:SetCreatureID(42180, 42178, 42179, 42166)
 mod:SetModelID(32688)
 mod:SetZone()
@@ -177,9 +177,9 @@ local bossInactive = function(boss)
 end
 
 function mod:CheckEncasing() -- prevent two yells at a time
-	if encasing and self.Options.YellOnTargetLock then
+	if encasing then
 		yellEncasingShadows:Yell()
-	elseif not encasing then
+	else
 		yellAcquiringTarget:Yell()
 	end
 	encasing = false

DBM-Core/DBM-Core.lua
@@ -42,7 +42,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7381 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7382 $"):sub(12, -3)),
 	DisplayVersion = "4.10.11 alpha", -- the string that is shown as version
 	ReleaseRevision = 7325 -- the revision of the latest stable version that is available
 }