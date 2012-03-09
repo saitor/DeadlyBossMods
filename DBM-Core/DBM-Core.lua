db7448a085de8db7afb796c99421d26fe18db33a
subversion <svn@deadlybossmods.com>
2012-03-10 00:23:29 +0100

    Omega via svn r7446: Small DMF update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,7 +42,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7445 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7446 $"):sub(12, -3)),
 	DisplayVersion = "4.10.11 alpha", -- the string that is shown as version
 	ReleaseRevision = 7325 -- the revision of the latest stable version that is available
 }
