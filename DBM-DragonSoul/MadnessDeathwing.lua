35e3410569a0bb773eb0ecd731941f72df13f187
subversion <svn@deadlybossmods.com>
2012-03-12 15:26:41 +0100

    Omega via svn r7452: Make unstable corruption cast countdown off by default. 2 countdowns on same fight are confusing. Someone might mistake it for a bolt about to hit (since bolts have no mob where you can watch a cast bar, most depend solely on the countdown for it). Parasites are usually stupid obvious since most are on it and can flat out see cast bar so it was logical choice for the "off by default". If someone blows a CD on a parasite cast instead of a bolt, a wipe is very likely.

--- DBM-DragonSoul/MadnessDeathwing.lua
+++ DBM-DragonSoul/MadnessDeathwing.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(333, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7451 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7452 $"):sub(12, -3))
 mod:SetCreatureID(56173)
 mod:SetModelID(40087)
 mod:SetModelSound("sound\\CREATURE\\Deathwing\\VO_DS_DEATHWING_MAELSTROMEVENT_01.OGG", "sound\\CREATURE\\Deathwing\\VO_DS_DEATHWING_MAELSTROMSPELL_04.OGG")
@@ -75,7 +75,7 @@ local timerTetanusCD				= mod:NewCDTimer(3.5, 109605, nil, mod:IsTank())
 local berserkTimer					= mod:NewBerserkTimer(900)
 
 local countdownBoltBlast			= mod:NewCountdown(8, 109600)
-local countdownUnstableCorruption	= mod:NewCountdown(10, 108813)
+local countdownUnstableCorruption	= mod:NewCountdown(10, 108813, false)--Even though not at same time as bolt, 2 countdowns in one fight is always confusing. So off by default for this one since it's more obvious then the bolt one where you can not watch a long cast bar.
 local countdownShrapnel				= mod:NewCountdown(6, 109598, not mod:IsTank())
 
 mod:AddBoolOption("RangeFrame", true)--For heroic parasites, with debuff filtering.
