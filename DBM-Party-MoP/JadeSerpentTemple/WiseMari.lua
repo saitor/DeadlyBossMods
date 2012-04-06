71cbddb6939e9a3be6ebe7b1fa84b3ae6aad1190
subversion <svn@deadlybossmods.com>
2012-04-06 22:03:11 +0200

    Omega via svn r7491: ThalnosSoulrender done, i thought i had done this already but forgot Added correct CiDs to some of the fights in ToJS, will do the actual encounters soon.

--- DBM-Party-MoP/JadeSerpentTemple/WiseMari.lua
+++ DBM-Party-MoP/JadeSerpentTemple/WiseMari.lua
@@ -1,12 +1,12 @@
 local mod	= DBM:NewMod("WiseMari", "DBM-Party-MoP", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7463 $"):sub(12, -3))
---mod:SetCreatureID(54432)
+mod:SetRevision(("$Revision: 7491 $"):sub(12, -3))
+mod:SetCreatureID(56448)
 --mod:SetModelID(41125)
 mod:SetZone()
 
---mod:RegisterCombat("combat")
+mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
