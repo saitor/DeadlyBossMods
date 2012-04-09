e2c9ea037a76cbbadd36c2540140d9bfbe71b4ea
subversion <svn@deadlybossmods.com>
2012-04-09 23:49:46 +0200

    Omega via svn r7495: Some small fixes and comment updates to Jade temple. Still need to do rest later.

--- DBM-Party-MoP/JadeSerpentTemple/LibraryEvent.lua
+++ DBM-Party-MoP/JadeSerpentTemple/LibraryEvent.lua
@@ -1,8 +1,8 @@
 local mod	= DBM:NewMod("LibraryEvent", "DBM-Party-MoP", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7492 $"):sub(12, -3))
-mod:SetCreatureID(59051, 59726, 58826)--59051 (Strife), 59726 (Anger), 58826 (Zao Sunseeker)
+mod:SetRevision(("$Revision: 7495 $"):sub(12, -3))
+mod:SetCreatureID(59051, 59726, 58826)--59051 (Strife), 59726 (Anger), 58826 (Zao Sunseeker). This event has a random chance to be Zao (solo) or Anger and Strife (together)
 --mod:SetModelID(39506)
 mod:SetZone()
 
