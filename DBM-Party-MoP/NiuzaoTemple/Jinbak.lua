63084515b0b9d636a965bd7be59b260acfa05569
subversion <svn@deadlybossmods.com>
2012-04-12 05:48:48 +0200

    Omega via svn r7500: More boss updates based on new EJ info from latest build

--- DBM-Party-MoP/NiuzaoTemple/Jinbak.lua
+++ DBM-Party-MoP/NiuzaoTemple/Jinbak.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("ResinharvesterPH", "DBM-Party-MoP", 6)
+local mod	= DBM:NewMod("Jinbak", "DBM-Party-MoP", 6)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 7464 $"):sub(12, -3))
