9e849fd87015f913df8f218646689a3fcfa8d605
subversion <svn@deadlybossmods.com>
2012-07-04 06:37:32 +0200

    nbluewiz via svn r7598: # Remove MoP code in live version. # Update Salyis creature id. (I lost my combat log. Boss waring will be added later.)

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -38,7 +38,7 @@
 --
 --
 
-local revision =("$Revision: 7486 $"):sub(12, -3) 
+local revision =("$Revision: 7598 $"):sub(12, -3) 
 local FrameTitle = "DBM_GUI_Option_"	-- all GUI frames get automatically a name FrameTitle..ID
 
 local PanelPrototype = {}
@@ -2128,16 +2128,10 @@ do
 	end
 
 	local Categories = {}
-	local WowBuild = select(2, GetBuildInfo())
 	function DBM_GUI:UpdateModList()
 		for k,addon in ipairs(DBM.AddOns) do
 			if not Categories[addon.category] then
-				-- Create a Panel for "Wrath of the Lich King" "Burning Crusade" ...
-				if select(4, _G.GetBuildInfo()) >= 50000 then--Choose default expanded catagory based on toc. expands MoP or Cataclysm based on toc
-					Categories[addon.category] = DBM_GUI:CreateNewPanel(L["TabCategory_"..addon.category:upper()] or L.TabCategory_Other, nil, (addon.category:upper()=="MOP"))
-				else
-					Categories[addon.category] = DBM_GUI:CreateNewPanel(L["TabCategory_"..addon.category:upper()] or L.TabCategory_Other, nil, (addon.category:upper()=="CATA"))
-				end
+				Categories[addon.category] = DBM_GUI:CreateNewPanel(L["TabCategory_"..addon.category:upper()] or L.TabCategory_Other, nil, (addon.category:upper()=="CATA"))
 				if L["TabCategory_"..addon.category:upper()] then
 					local ptext = Categories[addon.category]:CreateText(L["TabCategory_"..addon.category:upper()])
 					ptext:SetPoint('TOPLEFT', Categories[addon.category].frame, "TOPLEFT", 10, -10)
