85fd9d2e6e6adcf135901bb3cae6e3181c8fcc72
subversion <svn@deadlybossmods.com>
2012-07-27 06:37:09 +0200

    nbluewiz via svn r7662: # update comments (homorrhage bug)

--- DBM-DragonSoul/MadnessDeathwing.lua
+++ DBM-DragonSoul/MadnessDeathwing.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(333, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7658 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7662 $"):sub(12, -3))
 mod:SetCreatureID(56173)
 mod:SetModelID(40087)
 mod:SetModelSound("sound\\CREATURE\\Deathwing\\VO_DS_DEATHWING_MAELSTROMEVENT_01.OGG", "sound\\CREATURE\\Deathwing\\VO_DS_DEATHWING_MAELSTROMSPELL_04.OGG")
@@ -331,6 +331,9 @@ end
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellId)
 	if spellId == 110663 and self:AntiSpam(2, 3) then--Elementium Meteor Transform (apparently this doesn't fire UNIT_DIED anymore, need to use this alternate method)
 		self:SendSync("BoltDied")--Send sync because Elementium bolts do not have a bossN arg, which means event only fires if it's current target/focus.
+	-- hemorrhage warning works bad in koKR client
+	-- sometimes this fucntion spams every 2 sec even boss is idle. (especially heroic25)
+	-- sometimes no warn shows at all. (especially heroic10)
 	elseif spellName == hemorrhage and self:AntiSpam(2, 2) then
 		warnHemorrhage:Show()
 		specWarnHemorrhage:Show()
