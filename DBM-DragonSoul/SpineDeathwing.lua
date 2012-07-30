04ae27a918d91e95ca9d0da6d9d478e792665c4e
subversion <svn@deadlybossmods.com>
2012-07-30 18:01:57 +0200

    nbluewiz via svn r7668: # Spine of Deathwing: Apply fast update to Plasma Shield bars.

--- DBM-DragonSoul/SpineDeathwing.lua
+++ DBM-DragonSoul/SpineDeathwing.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(318, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7599 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7668 $"):sub(12, -3))
 mod:SetCreatureID(53879)
 mod:SetModelID(35268)
 mod:SetModelSound("sound\\CREATURE\\Deathwing\\VO_DS_DEATHWING_BACKEVENT_01.OGG", "sound\\CREATURE\\Deathwing\\VO_DS_DEATHWING_BACKSLAY_01.OGG")
@@ -101,6 +101,7 @@ do
 	function mod:SPELL_HEAL(_, _, _, _, destGUID, _, _, _, _, _, _, _, _, absorbed)
 		if plasmaTargets[destGUID] then
 			healed[destGUID] = healed[destGUID] + (absorbed or 0)
+			DBM.BossHealth:Update()
 		end
 	end
 	mod.SPELL_PERIODIC_HEAL = mod.SPELL_HEAL
