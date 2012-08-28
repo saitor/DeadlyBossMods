e23a5342839bf2aacf39ba93a8fa5f92a9d21e1f
subversion <svn@deadlybossmods.com>
2012-08-28 16:42:49 +0200

    tandanu via svn r7791: increment version number for DBM 5.0 alpha builds

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,9 +42,9 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7785 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7791 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
-	ReleaseRevision = 7705 -- the revision of the latest stable version that is available
+	ReleaseRevision = 7790 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
@@ -1279,6 +1279,10 @@ do
 			enableIcons = true
 			fireEvent("raidLeave", UnitName("player"))
 		end
+	end
+	
+	function DBM:IsInRaid()
+		return inRaid
 	end
 
 	function DBM:GetGroupMembers()
