f975dcc68892eab56578ccbaa1131896e901f8aa
subversion <svn@deadlybossmods.com>
2012-09-15 20:52:09 +0200

    Omega via svn r7844: Some testing of auto marking adds on wise mari

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,7 +42,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7843 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7844 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 7811 -- the revision of the latest stable version that is available
 }
