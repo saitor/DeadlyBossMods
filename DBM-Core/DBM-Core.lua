edb70d54153db88ca1b5b4f7db9a3dadf1894114
subversion <svn@deadlybossmods.com>
2012-09-16 16:40:44 +0200

    Omega via svn r7848: wish there was a better way to do this, but set a 3 min wipe time on anger and salyis

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,9 +44,9 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7846 $"):sub(12, -3)),
-	DisplayVersion = "4.11.2", -- the string that is shown as version
-	ReleaseRevision = 7846 -- the revision of the latest stable version that is available
+	Revision = tonumber(("$Revision: 7848 $"):sub(12, -3)),
+	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
+	ReleaseRevision = 7811 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
