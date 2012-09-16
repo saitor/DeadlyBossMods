fbb541e5595a8dafa50386ffbe4d71708b1284f0
subversion <svn@deadlybossmods.com>
2012-09-16 17:28:00 +0200

    Omega via svn r7849: increment alpha, so it stops telling me i'm out of date when i'm in raid with someone using 4.11.2 (bad tandanu, you forgot something)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,9 +44,9 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7848 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7849 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
-	ReleaseRevision = 7811 -- the revision of the latest stable version that is available
+	ReleaseRevision = 7846 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
