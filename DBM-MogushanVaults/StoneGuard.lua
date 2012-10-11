f417a7d3cee82acf531ae9183da5966e5b0a27ec
subversion <svn@deadlybossmods.com>
2012-10-11 16:58:52 +0200

    nbluewiz via svn r7936: # Mogushan Vaults Update 1. StoneGuard : - Fix typo. - Changed AntiSpam method. (seems AntiSpam do not prevent spam. this change is unconfirmed.) 2. Feng : - Added count to special timer. - Added Wildfire Infusion stack warn. - Update Null Barrier stuff. This is important to all raid members. - All stuff is confirmed and no error. 3. Garajal : - Change Spiritual Innervation detect method. SPELL_ARUA_APPLIED fires very lately except tanker. - Added Suicide countdown. - All stuff is confirmed. but I not have tank char. tank stuff is not confirmed. # Liu Frameheart : added experimental Jade Fire scanning stuff. (needs more tweak) # koKR update.

--- DBM-MogushanVaults/StoneGuard.lua
+++ DBM-MogushanVaults/StoneGuard.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(679, "DBM-MogushanVaults", nil, 317)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7934 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7936 $"):sub(12, -3))
 mod:SetCreatureID(60051, 60043, 59915, 60047)--Cobalt: 60051, Jade: 60043, Jasper: 59915, Amethyst: 60047
 mod:SetModelID(41892)
 mod:SetZone()
@@ -70,6 +70,17 @@ local function warnAmethystPoolTargets()
 	table.wipe(amethystPoolTargets)
 end
 
+local function poolTargetCheck(name)
+	if #amethystPoolTargets > 0 and name then
+		for i = 1, #amethystPoolTargets do
+			if amethystPoolTargets[i] == name then
+				return false
+			end
+		end
+	end
+	return true
+end
+
 local function warnJasperChainsTargets()
 	warnJasperChains:Show(table.concat(jasperChainsTargets, "<, >"))
 	table.wipe(jasperChainsTargets)
@@ -77,7 +88,7 @@ end
 
 local function getBossuId()
 	local uId
-	if UnitExits("boss1") or UnitExits("boss2") or UnitExits("boss3") or UnitExits("boss4") then
+	if UnitExists("boss1") or UnitExists("boss2") or UnitExists("boss3") or UnitExists("boss4") then
 		for i = 1, 4 do
 			if UnitName("boss"..i) == Cobalt then
 				uId = "boss"..i
@@ -250,7 +261,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnJadeShards:Show()
 		timerJadeShardsCD:Start()
 	elseif args:IsSpellID(116235, 130774) then--is 116235 still used? my logs show ONLY 130774 being used.
-		if self:AntiSpam(3, args.destName) then--because for some reason it fires 3 times per player, we don't want more than one occurance of each player, so we use playername as antispam filter.
+		if poolTargetCheck(args.destName) then--antispam can not prevent spam, try another way.
 			amethystPoolTargets[#amethystPoolTargets + 1] = args.destName
 			self:Unschedule(warnAmethystPoolTargets)
 			self:Schedule(0.5, warnAmethystPoolTargets)
