32c4ad85a1ab1b6c3ccdc14a8eb3c77a3a4b286a
subversion <svn@deadlybossmods.com>
2012-10-20 08:00:12 +0200

    Omega via svn r7972: whisper stats off by default too. greater majority of players just don't like stat reporting and don't know how to open dbm options still (still think 80% of our users just play with default settings). I hear a lot about people wanting to be able to turn this of?\226?\128?\166sigh

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7971 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7972 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 7956 -- the revision of the latest stable version that is available
 }
@@ -91,7 +91,7 @@ DBM.DefaultOptions = {
 	ShowRecoveryMessage = true,
 	AutoRespond = true,
 	StatusEnabled = true,
-	WhisperStats = true,
+	WhisperStats = false,
 	HideBossEmoteFrame = false,
 	SpamBlockRaidWarning = true,
 	SpamBlockBossWhispers = false,
