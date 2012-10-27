9ed3defe2f9ebe2ef5fd6448aa3e1725934fc3a4
subversion <svn@deadlybossmods.com>
2012-10-27 02:13:59 +0200

    Omega via svn r7989: by request, arc arrow stuff. off by default though since it's still subjective. it's going to be wrong if you're a dps/healer and aren't behind the boss, or a tank and not in front of boss. naturally, since sometimes tanks end up behind the boss and dps end up in front during combo, you see why this is off by default. There is no actual way to detect where you are in relation to the bosses orientation

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 7988 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 7989 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 7956 -- the revision of the latest stable version that is available
 }
