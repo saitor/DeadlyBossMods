829338348b7e15b908e98653e3a0cb1d52428504
subversion <svn@deadlybossmods.com>
2012-10-31 03:21:04 +0100

    Omega via svn r7999: Try using inactive visual instead of cancel activation do to reports of cancel not working on heroic

--- DBM-MogushanVaults/SpiritKings.lua
+++ DBM-MogushanVaults/SpiritKings.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(687, "DBM-MogushanVaults", nil, 317)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7901 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 7999 $"):sub(12, -3))
 mod:SetCreatureID(60701, 60708, 60709, 60710)--Adds: 60731 Undying Shadow, 60958 Pinning Arrow
 mod:SetModelID(41813)
 mod:SetZone()
@@ -234,7 +234,9 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		timerVolleyCD:Start()
 	elseif spellId == 118121 and self:AntiSpam(2, 2) then--Rain of Arrows
 		timerRainOfArrowsCD:Start()
-	elseif spellId == 118219 and self:AntiSpam(2, 3) then--Cancel Activation
+--	"<63.5 21:23:16> [UNIT_SPELLCAST_SUCCEEDED] Qiang the Merciless [[boss1:Inactive Visual::0:118205]]", -- [14066]
+--	"<63.5 21:23:16> [UNIT_SPELLCAST_SUCCEEDED] Qiang the Merciless [[boss1:Cancel Activation::0:118219]]", -- [14068]
+	elseif spellId == 118205 and self:AntiSpam(2, 3) then--Cancel Activation
 		if UnitName(uId) == Zian then
 			zianActive = false
 			timerChargingShadowsCD:Cancel()
