aeb696d2d06176d82b328dbed5065343e8ebb9f1
subversion <svn@deadlybossmods.com>
2012-11-01 23:10:26 +0100

    swix via svn r8018: ruRU update for HoF

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8017 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 8018 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 7956 -- the revision of the latest stable version that is available
 }
