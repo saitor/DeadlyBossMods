26a99d05d6e891c3b2a04948e422d6ddac7a32e0
subversion <svn@deadlybossmods.com>
2012-11-01 07:57:34 +0100

    Omega via svn r8015: remove unused variable

--- DBM-HeartofFear/Garalon.lua
+++ DBM-HeartofFear/Garalon.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(713, "DBM-HeartofFear", nil, 330)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8012 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8015 $"):sub(12, -3))
 mod:SetCreatureID(63191)--Also has CID 62164. He has 2 CIDs for a single target, wtf? It seems 63191 is one players attack though so i'll try just it.
 mod:SetModelID(42368)
 mod:SetZone()
@@ -46,11 +46,9 @@ local berserkTimer				= mod:NewBerserkTimer(420)
 --mod:AddBoolOption("InfoFrame", true)--Not sure how to do yet, i need to see 25 man first to get a real feel for number of people with debuff at once.
 mod:AddBoolOption("PheromonesIcon", true)
 
-local madeUpNumber = 0
 local brokenLegs = 0
 
 function mod:OnCombatStart(delay)
-	madeUpNumber = 0
 	brokenLegs = 0
 	timerFuriousSwipeCD:Start(-delay)--8-11 sec on pull
 	berserkTimer:Start(-delay)
