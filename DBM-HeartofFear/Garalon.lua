8d0a97d4655360e51be5487a221744fd370a46cd
subversion <svn@deadlybossmods.com>
2012-11-05 03:01:11 +0100

    nbluewiz via svn r8036: # Garalon : fix Broken Leg warning bug. # Meljarak : fix CC warns. # Tayak : fix Unseen Strike double warns. fix phase 2 Intensify timer. added experimental Storm Unleashed CD bar. # koKR update.

--- DBM-HeartofFear/Garalon.lua
+++ DBM-HeartofFear/Garalon.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(713, "DBM-HeartofFear", nil, 330)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8029 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8036 $"):sub(12, -3))
 mod:SetCreatureID(63191)--Also has CID 62164. He has 2 CIDs for a single target, wtf? It seems 63191 is one players attack though so i'll try just it.
 mod:SetModelID(42368)
 mod:SetZone()
@@ -13,6 +13,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
+	"SPELL_AURA_REMOVED_DOSE",
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_DAMAGE",
@@ -61,6 +62,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnFury:Show(args.destName, args.amount or 1)
 		timerFury:Start()
 	elseif args:IsSpellID(122786) and args:GetDestCreatureID() == 63191 then--This one also hits both the leg and the boss, so filter second one here as well.
+		-- this warn seems not works? needs review.
 		warnBrokenLeg:Show(args.destName, args.amount or 1)
 	elseif args:IsSpellID(122835) then
 		warnPheromones:Show(args.destName)
@@ -95,9 +97,9 @@ end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpellID(122786) then
+	if args:IsSpellID(122786) and args:GetDestCreatureID() == 63191 then
 		brokenLegs = (args.amount or 0)
-		warnBrokenLeg:Show(brokenLegs)
+		warnBrokenLeg:Show(args.destName, brokenLegs)
 	elseif args:IsSpellID(122835) then
 		if self.Options.PheromonesIcon then
 			self:SetIcon(args.destName, 0)
