716f370aed7416d7ecdcdaacf5296ff724d4585d
subversion <svn@deadlybossmods.com>
2012-11-06 05:29:50 +0100

    Omega via svn r8051: also make sure to cancel schedule on reapply too and not just removal

--- DBM-HeartofFear/Meljarak.lua
+++ DBM-HeartofFear/Meljarak.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(741, "DBM-HeartofFear", nil, 330)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8050 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8051 $"):sub(12, -3))
 mod:SetCreatureID(62397)
 mod:SetModelID(42645)
 mod:SetZone()
@@ -112,6 +112,7 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(122224) and args.sourceName == UnitName("player") then
+		warnImpalingSpear:Cancel()
 		warnImpalingSpear:Schedule(45)
 		countdownImpalingSpear:Cancel()
 		countdownImpalingSpear:Start()
