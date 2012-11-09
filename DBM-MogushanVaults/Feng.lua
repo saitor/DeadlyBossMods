c1d7a29dd17b7c460c61c0f71f94da1ac7245f57
subversion <svn@deadlybossmods.com>
2012-11-09 23:25:35 +0100

    Omega via svn r8083: option default tweak

--- DBM-MogushanVaults/Feng.lua
+++ DBM-MogushanVaults/Feng.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(689, "DBM-MogushanVaults", nil, 317)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8026 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8083 $"):sub(12, -3))
 mod:SetCreatureID(60009)--60781 Soul Fragment
 mod:SetModelID(41192)
 mod:SetZone()
@@ -111,7 +111,7 @@ local timerNullBarrior				= mod:NewBuffFadesTimer(6, 115817)
 local timerNullBarriorCD			= mod:NewCDTimer(55, 115817)
 
 local soundEpicenter				= mod:NewSound(116018)
-local soundWildSpark				= mod:NewSound(116784, nil, false)
+local soundWildSpark				= mod:NewSound(116784)
 
 mod:AddBoolOption("RangeFrame", mod:IsRanged())
 
