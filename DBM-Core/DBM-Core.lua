cfcdd21750a62c7f19469542bad0acdc0507930d
subversion <svn@deadlybossmods.com>
2012-11-21 03:00:24 +0100

    Omega via svn r8147: i'm a dumb fuck, why didn't i catch this problem leaving debug in to verify it but i bet this was the probelm

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8146 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 8147 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 8086 -- the revision of the latest stable version that is available
 }
