93251b55494249121fdb70409093e1889ab9beaf
subversion <svn@deadlybossmods.com>
2012-11-22 22:16:35 +0100

    Omega via svn r8155: Fix a local bug

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8154 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 8155 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 8086 -- the revision of the latest stable version that is available
 }
