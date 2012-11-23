003ec485797750aea1075feb7acb482b96eff1a6
subversion <svn@deadlybossmods.com>
2012-11-23 02:52:53 +0100

    Omega via svn r8158: comment out some of the debug on unsok, those areas are ruled out. still need more info from other spots. i need debug from someone who isn't bad at constructs and actually doesn't have amber strike on CD when monstrosity casts amber explosion. :\

--- DBM-HeartofFear/Zorlok.lua
+++ DBM-HeartofFear/Zorlok.lua
@@ -1,8 +1,8 @@
 local mod	= DBM:NewMod(745, "DBM-HeartofFear", nil, 330)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8157 $"):sub(12, -3))
-mod:SetCreatureID(62980)
+mod:SetRevision(("$Revision: 8158 $"):sub(12, -3))
+mod:SetCreatureID(62980)--63554 (Special invisible Vizier that casts the direction based spellid versions of attenuation)
 mod:SetModelID(42807)
 mod:SetZone()
 
