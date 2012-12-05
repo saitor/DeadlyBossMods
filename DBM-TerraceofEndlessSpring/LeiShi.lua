a3a080db9d9d5d08c2ab60bfe4f6012e3d254dc3
subversion <svn@deadlybossmods.com>
2012-12-05 04:39:47 +0100

    Omega via svn r8232: fix lua error

--- DBM-TerraceofEndlessSpring/LeiShi.lua
+++ DBM-TerraceofEndlessSpring/LeiShi.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(729, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8206 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8232 $"):sub(12, -3))
 mod:SetCreatureID(62983)--62995 Animated Protector
 mod:SetModelID(42811)
 
@@ -141,7 +141,7 @@ mod:RegisterOnUpdateHandler(function(self)
 			local uId = "raid"..i.."target"
 			local guid = UnitGUID(uId)
 			if guards[guid] then
-				local existingIcons = GetRaidTargetIndex(uid)
+				local existingIcons = GetRaidTargetIndex(uId)
 				if not existingIcons then
 					local icon = getAvailableIcons()
 					SetRaidTarget(uId, icon)
