807d91585a4bf437250c148995cbae5c20672657
subversion <svn@deadlybossmods.com>
2012-12-08 22:18:03 +0100

    tandanu via svn r8254: ignore oQueue sync messages

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8253 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 8254 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 8187 -- the revision of the latest stable version that is available
 }
@@ -2985,6 +2985,10 @@ do
 	
 	-- sender is a presenceId for real id messages, a character name otherwise
 	local function onWhisper(msg, sender, isRealIdMessage)
+		-- ignore oQueue messages
+		if msg and msg:sub(1, 3) == "OQ," then
+			return
+		end
 		if msg == "status" and #inCombat > 0 and DBM.Options.StatusEnabled then
 			if not difficultyText then -- prevent error when timer recovery function worked and etc (StartCombat not called)
 				difficultyText = select(2, DBM:GetCurrentInstanceDifficulty())
