b3a439826e176fdda6368700138b4d5c72abd58e
subversion <svn@deadlybossmods.com>
2012-12-18 22:43:39 +0100

    Omega via svn r8320: add gg engineering rocket warnings to rank 7

--- DBM-Brawlers/Rank4.lua
+++ DBM-Brawlers/Rank4.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrawlRank4", "DBM-Brawlers")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8255 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8320 $"):sub(12, -3))
 --mod:SetCreatureID(60491)
 mod:SetModelID(28115)
 mod:SetZone()
@@ -12,3 +12,5 @@ mod:RegisterEvents(
 
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
+
+local brawlersMod = DBM:GetModByName("Brawlers")
--- DBM-Brawlers/Rank7.lua
+++ DBM-Brawlers/Rank7.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrawlRank7", "DBM-Brawlers")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8255 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8320 $"):sub(12, -3))
 --mod:SetCreatureID(60491)
 mod:SetModelID(46798)
 mod:SetZone()
@@ -10,5 +10,22 @@ mod:RegisterEvents(
 	"SPELL_CAST_START"
 )
 
+local warnRockets				= mod:NewCastAnnounce(133212, 4)
+
+local timerRockets				= mod:NewBuffActiveTimer(9, 133212)
+
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
+
+local brawlersMod = DBM:GetModByName("Brawlers")
+
+function mod:SPELL_CAST_START(args)
+	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end--Spectator mode is disabled, do nothing.
+	if args:IsSpellID(133212) then
+		warnRockets:Show()
+		timerRockets:Start()--First one is 5 seconds after combat start
+		if brawlersMod:PlayerFighting() then
+			specWarnFireWall:Show()
+		end
+	end
+end
