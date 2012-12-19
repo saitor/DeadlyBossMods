a3e5ba7edbb250cc7a1ca25f6e458b9e20fde12f
subversion <svn@deadlybossmods.com>
2012-12-19 09:35:05 +0100

    Omega via svn r8336: maybe fix a bug on battletron, truth is i just have crappy log for him right now, need more data on him tomorrow

--- DBM-Brawlers/Rank7.lua
+++ DBM-Brawlers/Rank7.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrawlRank7", "DBM-Brawlers")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8333 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8336 $"):sub(12, -3))
 --mod:SetCreatureID(60491)
 mod:SetModelID(46798)
 mod:SetZone()
@@ -39,11 +39,12 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end
 	if args:IsSpellID(133015) then
+		remainingMines = 8
 		warnMinesSpawning:Start()
 		if brawlersMod:PlayerFighting() then
 			specWarnMinesSpawning:Show()
 		end
-	elseif args:IsSpellID(133018) then
-		remainingMines = 8
+--[[	elseif args:IsSpellID(133018) then
+		remainingMines = 8--]]
 	end
 end
