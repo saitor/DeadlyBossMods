39c7e8c209122a60ca273e296317aa485c67a19a
subversion <svn@deadlybossmods.com>
2012-12-20 23:12:38 +0100

    nbluewiz via svn r8365: # Protectors of Endless : Cleansing Water is CD. not NEXT.

--- DBM-TerraceofEndlessSpring/ProtectorsofEndless.lua
+++ DBM-TerraceofEndlessSpring/ProtectorsofEndless.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(683, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8283 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8365 $"):sub(12, -3))
 mod:SetCreatureID(60585, 60586, 60583)--60583 Protector Kaolan, 60585 Elder Regail, 60586 Elder Asani
 mod:SetModelID(41503)--Protector Kaolan, 41502 and 41504 are elders
 mod:SetZone()
@@ -53,7 +53,7 @@ local specWarnExpelCorruption		= mod:NewSpecialWarningSpell(117975, nil, nil, ni
 local specWarnCorruptedEssence		= mod:NewSpecialWarningStack(118191, true, 9)--Amount may need adjusting depending on what becomes an accepted strategy
 
 --Elder Asani
-local timerCleansingWatersCD		= mod:NewNextTimer(32.5, 117309)
+local timerCleansingWatersCD		= mod:NewCDTimer(32.5, 117309)
 local timerCorruptingWatersCD		= mod:NewNextTimer(42, 117227)
 --Elder Regail
 local timerLightningPrisonCD		= mod:NewCDTimer(25, 111850)
