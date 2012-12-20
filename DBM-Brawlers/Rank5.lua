0be810bb95511a3858cad15a05f906cf006b4acb
subversion <svn@deadlybossmods.com>
2012-12-20 20:29:34 +0100

    Omega via svn r8361: Fix rank 5 bad spellid bug

--- DBM-Brawlers/Rank5.lua
+++ DBM-Brawlers/Rank5.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrawlRank5", "DBM-Brawlers")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8335 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8361 $"):sub(12, -3))
 --mod:SetCreatureID(60491)
 mod:SetModelID(6923)
 mod:SetZone()
@@ -44,7 +44,7 @@ function mod:SPELL_CAST_START(args)
 		if brawlersMod:PlayerFighting() then
 			specWarnDarkZone:Show()
 		end
-	elseif args:IsSpellID(124935) then
+	elseif args:IsSpellID(124860) then
 		warnRainDance:Show()
 		if brawlersMod:PlayerFighting() then
 			specWarnRainDance:Show()
