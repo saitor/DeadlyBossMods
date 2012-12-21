dd9eaa8f2911cca76b5db0e603ae434ed2f68b37
subversion <svn@deadlybossmods.com>
2012-12-21 23:01:46 +0100

    Omega via svn r8386: that last "fix" to end match and start match broke shit. if you login to brawl pub currentZone returns invalid. so if you don't want berserk timers/warnings leave gropu when you leave zone.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8385 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 8386 $"):sub(12, -3)),
 	DisplayVersion = "5.0.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 8290 -- the revision of the latest stable version that is available
 }
