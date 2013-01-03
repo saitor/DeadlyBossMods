53da52e867f5dc85f818bbc4e6708054a00b139f
subversion <svn@deadlybossmods.com>
2013-01-03 02:50:09 +0100

    Omega via svn r8469: Probably fix lua error

--- DBM-Pandaria/Oondasta.lua
+++ DBM-Pandaria/Oondasta.lua
@@ -1,8 +1,8 @@
-if not select(4, GetBuildInfo()) >= 50200 then return end--Don't load on live
+if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(826, "DBM-Pandaria", nil, 322)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8468 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8469 $"):sub(12, -3))
 mod:SetCreatureID(69161)--Not yet available in PTR, CID not known
 --mod:SetModelID(41448)
 mod:SetZone(929)--No map ID for area yet. Lightning Isle
