f6642155065f8cd7192ff1012405ad7980e1dd2a
subversion <svn@deadlybossmods.com>
2013-01-03 02:26:04 +0100

    Omega via svn r8467: Oondasta mod (4.2 world boss) Bump alpha version to 5.2.0. 5.2 Work has begun.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,8 +44,8 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8466 $"):sub(12, -3)),
-	DisplayVersion = "5.1.0 alpha", -- the string that is shown as version
+	Revision = tonumber(("$Revision: 8467 $"):sub(12, -3)),
+	DisplayVersion = "5.2.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 8421 -- the revision of the latest stable version that is available
 }
 
