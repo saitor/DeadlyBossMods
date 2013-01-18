0b9dd3637fe8c9c35d5fcc25a555d9e9d33b7dbf
subversion <svn@deadlybossmods.com>
2013-01-18 17:52:51 +0100

    arta via svn r8567: Throne of Thunder Model IDs taken from Encounter Journal (Ra'den is most likely a PH)

--- DBM-ThroneofThunder/Primordius.lua
+++ DBM-ThroneofThunder/Primordius.lua
@@ -2,9 +2,9 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(820, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8479 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8567 $"):sub(12, -3))
 --mod:SetCreatureID(62983)
---mod:SetModelID(42811)
+mod:SetModelID(47009)
 
 mod:RegisterCombat("combat")
 
--- DBM-ThroneofThunder/Tortos.lua
+++ DBM-ThroneofThunder/Tortos.lua
@@ -2,9 +2,9 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(825, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8479 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8567 $"):sub(12, -3))
 --mod:SetCreatureID(62983)
---mod:SetModelID(42811)
+mod:SetModelID(46559)
 
 mod:RegisterCombat("combat")
 
