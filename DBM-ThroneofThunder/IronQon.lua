e59ae8997ff1cfb93a68404274fa5b219df48af7
subversion <svn@deadlybossmods.com>
2013-01-18 01:32:16 +0100

    Omega via svn r8564: creature IDs

--- DBM-ThroneofThunder/IronQon.lua
+++ DBM-ThroneofThunder/IronQon.lua
@@ -2,8 +2,8 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(817, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8479 $"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetRevision(("$Revision: 8564 $"):sub(12, -3))
+mod:SetCreatureID(68079, 68080, 68081)--Ro'shak 68079, Quet'zal 68080, Dam'ren 68081
 --mod:SetModelID(42811)
 
 mod:RegisterCombat("combat")
