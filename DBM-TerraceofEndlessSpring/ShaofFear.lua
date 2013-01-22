9610036dd982b68011b4c85e9a23777f73d81406
subversion <svn@deadlybossmods.com>
2013-01-22 05:46:27 +0100

    Omega via svn r8592: Fix platform check, among other issues (SPELL_AURA_REMOVED not registered)

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(709, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8584 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8592 $"):sub(12, -3))
 mod:SetCreatureID(60999)--61042 Cheng Kang, 61046 Jinlun Kun, 61038 Yang Guoshi, 61034 Terror Spawn
 mod:SetModelID(41772)
 mod:SetUsedIcons(8, 7, 6, 5, 4)
@@ -11,6 +11,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
+	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"UNIT_DIED",
