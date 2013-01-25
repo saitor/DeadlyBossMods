679f1ebca970b27698df4815d37170371d8614fd
subversion <svn@deadlybossmods.com>
2013-01-25 20:01:31 +0100

    Omega via svn r8611: jinrokh creatureid

--- DBM-ThroneofThunder/Jinrokh.lua
+++ DBM-ThroneofThunder/Jinrokh.lua
@@ -2,8 +2,8 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(827, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8567 $"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetRevision(("$Revision: 8611 $"):sub(12, -3))
+mod:SetCreatureID(69465)
 mod:SetModelID(47552)
 
 mod:RegisterCombat("combat")
