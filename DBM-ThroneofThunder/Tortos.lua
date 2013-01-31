699ae410a40c2340d291855e8d52175a531efbc7
subversion <svn@deadlybossmods.com>
2013-01-31 20:22:41 +0100

    Omega via svn r8655: throw some quick stone breath warnings in

--- DBM-ThroneofThunder/Tortos.lua
+++ DBM-ThroneofThunder/Tortos.lua
@@ -2,7 +2,7 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(825, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8654 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8655 $"):sub(12, -3))
 mod:SetCreatureID(67977)
 mod:SetModelID(46559)
 
@@ -11,3 +11,19 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START"
 )
+
+local warnStoneBreath				= mod:NewCastAnnounce(133939, 4)
+
+local specWarnStoneBreath			= mod:NewSpecialWarningInterrupt(133939)
+
+function mod:OnCombatStart(delay)
+
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(133939) then
+		warnStoneBreath:Show()
+		specWarnStoneBreath:Show(args.sourceName)
+	end
+end
+
