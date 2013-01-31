c04f2f02082bc76368ad63c1d74ed37e0dd0abf0
subversion <svn@deadlybossmods.com>
2013-01-31 19:40:17 +0100

    Omega via svn r8654: Tortos CID

--- DBM-ThroneofThunder/Tortos.lua
+++ DBM-ThroneofThunder/Tortos.lua
@@ -2,8 +2,8 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(825, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8567 $"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetRevision(("$Revision: 8654 $"):sub(12, -3))
+mod:SetCreatureID(67977)
 mod:SetModelID(46559)
 
 mod:RegisterCombat("combat")
