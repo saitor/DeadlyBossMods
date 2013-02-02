e18217200bd1be09f17a805f4a448986cca0a610
subversion <svn@deadlybossmods.com>
2013-02-02 06:34:56 +0100

    Omega via svn r8662: that should be good enough for now for tortos mod Add CIDs for consorts Much more to do tomorrow

--- DBM-ThroneofThunder/TwinConsorts.lua
+++ DBM-ThroneofThunder/TwinConsorts.lua
@@ -2,8 +2,8 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(829, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8567 $"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetRevision(("$Revision: 8662 $"):sub(12, -3))
+mod:SetCreatureID(68905, 68904)--Lu'lin 68905, Suen 68904
 mod:SetModelID(46975)--Lu'lin, 46974 Suen
 
 mod:RegisterCombat("combat")
