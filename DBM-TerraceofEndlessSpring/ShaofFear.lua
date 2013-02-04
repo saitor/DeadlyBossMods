71001a1b5cb71660b873821568a821fe005da2df
subversion <svn@deadlybossmods.com>
2013-02-05 00:52:42 +0100

    Omega via svn r8669: so yeah, that's not what sha of fear looks like, oops. he's been using sha of anger model all this time

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -1,9 +1,9 @@
 local mod	= DBM:NewMod(709, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8651 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8669 $"):sub(12, -3))
 mod:SetCreatureID(60999)--61042 Cheng Kang, 61046 Jinlun Kun, 61038 Yang Guoshi, 61034 Terror Spawn
-mod:SetModelID(41772)
+mod:SetModelID(45065)
 mod:SetUsedIcons(8, 7, 6, 5, 4)
 
 mod:RegisterCombat("combat")
