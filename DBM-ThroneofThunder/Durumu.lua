ec76b91bdbfd96cf725b12aa9e327b6bb3b63bab
subversion <svn@deadlybossmods.com>
2013-02-06 23:18:58 +0100

    Omega via svn r8680: push rest of Jikun mod put in CID for durumu

--- DBM-ThroneofThunder/Durumu.lua
+++ DBM-ThroneofThunder/Durumu.lua
@@ -2,8 +2,8 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(818, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8673 $"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetRevision(("$Revision: 8680 $"):sub(12, -3))
+mod:SetCreatureID(68036)
 mod:SetModelID(47189)
 
 mod:RegisterCombat("combat")
