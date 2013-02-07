af06cde507b6113dc096539ed77e7cdf0d0d800d
subversion <svn@deadlybossmods.com>
2013-02-07 04:54:46 +0100

    nbluewiz via svn r8682: # koKR update.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 8681 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 8682 $"):sub(12, -3)),
 	DisplayVersion = "5.2.0 alpha", -- the string that is shown as version
 	ReleaseRevision = 8421 -- the revision of the latest stable version that is available
 }
