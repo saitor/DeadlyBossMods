6d3086a9d5f2c6ede4c20d7acc3372830be93afa
subversion <svn@deadlybossmods.com>
2013-02-09 08:16:52 +0100

    Omega via svn r8696: fix bug where countdown on breath of fear wasn't canceled going into phase 2

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(709, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8695 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8696 $"):sub(12, -3))
 mod:SetCreatureID(60999)--61042 Cheng Kang, 61046 Jinlun Kun, 61038 Yang Guoshi, 61034 Terror Spawn
 mod:SetModelID(45065)
 mod:SetUsedIcons(8, 7, 6, 5, 4)
@@ -438,6 +438,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		specialCount = 0
 		timerThrashCD:Cancel()
 		timerBreathOfFearCD:Cancel()
+		countdownBreathOfFear:Cancel()
 		timerOminousCackleCD:Cancel()
 		timerDreadSpray:Cancel()
 		timerDreadSprayCD:Cancel()
