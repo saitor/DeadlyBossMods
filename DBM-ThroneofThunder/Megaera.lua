f9c556ebf8b4ad125bc42e3ea153ab7c3886b9f1
subversion <svn@deadlybossmods.com>
2013-02-14 22:25:27 +0100

    Omega via svn r8720: add debug for next boss, complete heroic mod for previous boss

--- DBM-ThroneofThunder/Megaera.lua
+++ DBM-ThroneofThunder/Megaera.lua
@@ -2,7 +2,7 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(821, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8713 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8720 $"):sub(12, -3))
 mod:SetCreatureID(68065, 70212, 70235, 70247)--flaming 70212. Frozen 70235, Venomous 70247
 mod:SetModelID(47414)--Hydra Fire Head, 47415 Frost Head, 47416 Poison Head
 
@@ -200,6 +200,8 @@ function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
 					venomBehind = venomBehind - 1
 				end
 			end
+			print("DBM Boss Debug: ", "Active Heads: ".."Fire: "..fireInFront.." Ice: "..iceInFront.." Venom: "..venomInFront)
+			print("DBM Boss Debug: ", "Inactive Heads: ".."Fire: "..fireBehind.." Ice: "..iceInBehind.." Venom: "..venomBehind)
 		end
 	end
 end
