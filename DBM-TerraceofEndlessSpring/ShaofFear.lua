8102583696903703a9abda29a976370385788d2d
subversion <svn@deadlybossmods.com>
2013-02-20 09:07:09 +0100

    Omega via svn r8753: naked and afraid timers should not be on by default for dps, guess I overlooked that when reviewing that part of mod.

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(709, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8740 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8753 $"):sub(12, -3))
 mod:SetCreatureID(60999)--61042 Cheng Kang, 61046 Jinlun Kun, 61038 Yang Guoshi, 61034 Terror Spawn
 mod:SetModelID(45065)
 mod:SetUsedIcons(8, 7, 6, 5, 4)
@@ -69,8 +69,8 @@ local timerDeathBlossom					= mod:NewBuffActiveTimer(5, 119888)
 local timerFearless						= mod:NewBuffFadesTimer(30, 118977)
 -- Heroic Phase 2
 local timerDreadTrashCD					= mod:NewCDTimer(9, 132007)--Share Trash CD.
-local timerNakedAndAfraid				= mod:NewTargetTimer(25, 120669)--25 on 10 man, 50 on 25 (requiring 3 tanks)
-local timerNakedAndAfraidCD				= mod:NewCDTimer(30, 120669)-- varies, but mostly 30. can get delayed upwards of 15 seconds though between submerge and specials
+local timerNakedAndAfraid				= mod:NewTargetTimer(25, 120669, nil, mod:IsTank() or mod:IsHealer())--25 on 10 man, 50 on 25 (requiring 3 tanks)
+local timerNakedAndAfraidCD				= mod:NewCDTimer(30, 120669, nil, mod:IsTank() or mod:IsHealer())-- varies, but mostly 30. can get delayed upwards of 15 seconds though between submerge and specials
 local timerSubmergeCD					= mod:NewCDCountTimer(51.5, 120455)
 mod:AddBoolOption("timerSpecialAbility", true, "timer")--Better to have one option for his shared special timer than 7
 local timerWaterspoutCD					= mod:NewCDTimer(10, 120519, nil, nil, false)
