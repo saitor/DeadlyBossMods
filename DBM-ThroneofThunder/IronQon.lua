bb3c577630a5f51f56615a10586f6eddb5e1e614
subversion <svn@deadlybossmods.com>
2013-03-08 01:01:52 +0100

    Omega via svn r8846: change stack count for 25 man back down to old number

--- DBM-ThroneofThunder/IronQon.lua
+++ DBM-ThroneofThunder/IronQon.lua
@@ -2,7 +2,7 @@ if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
 local mod	= DBM:NewMod(817, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8845 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 8846 $"):sub(12, -3))
 mod:SetCreatureID(68078, 68079, 68080, 68081)--Ro'shak 68079, Quet'zal 68080, Dam'ren 68081, Iron Qon 68078
 mod:SetMainBossID(68078)
 mod:SetModelID(46627) -- Iron Qon, 46628 Ro'shak, 46629 Quet'zal, 46630 Dam'ren
@@ -116,7 +116,7 @@ function mod:OnCombatStart(delay)
 		if self:IsDifficulty("normal10", "heroic10") then
 			DBM.RangeCheck:Show(10, nil, nil, 2)
 		else
-			DBM.RangeCheck:Show(10, nil, nil, 6)
+			DBM.RangeCheck:Show(10, nil, nil, 4)
 		end
 	end
 	if self:IsDifficulty("heroic10", "heroic25") then
