29cb22f52bf4d7108cb458f0a4c12a13161e02dd
subversion <svn@deadlybossmods.com>
2013-03-27 00:56:44 +0100

    Omega via svn r9052: Half of a warlock scenario mod. other boss not supported because logger didn't know how to properly log and i can't figure out where the hell they pulled the first boss. time stamp mismatches from combat log and transcriptor and everything. FYI, i prefer transcriptor logs that INCLUDE the CLEU and logs aren't split with two different time stamp formats.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9051 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9052 $"):sub(12, -3)),
 	DisplayVersion = "5.2.2 alpha", -- the string that is shown as version
 	ReleaseRevision = 8892 -- the revision of the latest stable version that is available
 }
