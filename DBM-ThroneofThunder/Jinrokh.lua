2d265005ec18c0ead850e1f217347932debc332a
subversion <svn@deadlybossmods.com>
2013-04-05 16:50:42 +0200

    nbluewiz via svn r9163: # Jinrokh : Fix first Ionization countdown. # Megaera : UNIT_AURA confirmed, good works. Remove sync stuff.

--- DBM-ThroneofThunder/Jinrokh.lua
+++ DBM-ThroneofThunder/Jinrokh.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(827, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9153 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9163 $"):sub(12, -3))
 mod:SetCreatureID(69465)
 mod:SetModelID(47552)
 
@@ -80,7 +80,7 @@ function mod:OnCombatStart(delay)
 	timerThrowCD:Start(30-delay)
 	if self:IsDifficulty("heroic10", "heroic25") then
 		timerIonizationCD:Start(60-delay)
-		countdownIonization:Start(-delay)
+		countdownIonization:Start(60-delay)
 		berserkTimer:Start(360-delay)
 	else
 		berserkTimer:Start(-delay)
