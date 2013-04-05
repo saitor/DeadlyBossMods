085c9d59d0bdaf43d6e740126dc7ef331fa3af80
subversion <svn@deadlybossmods.com>
2013-04-05 12:03:04 +0200

    nbluewiz via svn r9162: # Core : Prevent wipe on combat. / Increase normal wipe time. / Fix combat log bug. (Re-engaged before 4s) # Durumu : Fix lfr filter. # koKR update.

--- DBM-ThroneofThunder/Durumu.lua
+++ DBM-ThroneofThunder/Durumu.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(818, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9147 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9162 $"):sub(12, -3))
 mod:SetCreatureID(68036)--Crimson Fog 69050, 
 mod:SetModelID(47189)
 mod:SetUsedIcons(7, 6, 4, 1)
@@ -339,7 +339,7 @@ function mod:CHAT_MSG_MONSTER_EMOTE(msg, npc, _, _, target)
 		if self:IsDifficulty("lfr25") and npc == azureFog and not lfrAzureFogRevealed then
 			lfrAzureFogRevealed = true
 			specWarnFogRevealed:Show(npc)
-		else
+		elseif not lfrAzureFogRevealed or not self:IsDifficulty("lfr25") then
 			specWarnFogRevealed:Show(npc)
 		end
 	elseif msg:find("spell:133795") then
