c1023a3b53019640b06bfcb7fd9ce0c667f13dc5
subversion <svn@deadlybossmods.com>
2013-04-26 20:44:58 +0200

    Omega via svn r9368: improve the monk filter on iron qon

--- DBM-ThroneofThunder/IronQon.lua
+++ DBM-ThroneofThunder/IronQon.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(817, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9365 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9368 $"):sub(12, -3))
 mod:SetCreatureID(68078, 68079, 68080, 68081)--Ro'shak 68079, Quet'zal 68080, Dam'ren 68081, Iron Qon 68078
 mod:SetMainBossID(68078)
 mod:SetModelID(46627) -- Iron Qon, 46628 Ro'shak, 46629 Quet'zal, 46630 Dam'ren
@@ -125,8 +125,9 @@ local function notEligable(unit)
 	end
 	--He CAN spear monks that are not in melee (I seen our mistweaver get many spears at ranged).
 	--However, if he targets a monk that IS in melee, he switches to a different target (again, i've seen him target our mistweaver in melee range, then instantly switched to ANOTHER target because the mistweaver was in melee).
-	--So for now, we just flat ignore monks. TODO, if "unit" is a monk, check distance between that unit and the tank. if unit < 15 yards, assume they are in melee? this way we can restore mistweaver's at range target scanning.
-	if select(2, UnitClass(unit)) == "MONK" then
+	--We assume any monk within 15 yards of tank is in melee range, may be wrong once in a blue moon.
+	--Should fairly accurately allow the mod to announce spears on monks outside melee range while ignoring monk targets in melee range
+	if (select(2, UnitClass(unit)) == "MONK") and (GetDistance(unit, "boss2target") <= 15) then
 		return true
 	end
 	return false
