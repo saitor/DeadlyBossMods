555bdcf9d98908501a93d09f14028a9b306a7576
subversion <svn@deadlybossmods.com>
2013-05-11 03:49:38 +0200

    Omega via svn r9494: drycode basic helm of command

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9493 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9494 $"):sub(12, -3)),
 	DisplayVersion = "5.2.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 9413 -- the revision of the latest stable version that is available
 }
