58797a8341df59fae352156f76b1a912e52bcc75
subversion <svn@deadlybossmods.com>
2013-05-12 23:14:24 +0200

    Omega via svn r9512: Add GUI option to advanced special warning config. it is also off by default. This is a feature meant more for experienced users to adjust and as such should be enabled first so the UI isn't cluttered, confusing, or easy to break for new users.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9511 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9512 $"):sub(12, -3)),
 	DisplayVersion = "5.2.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 9413 -- the revision of the latest stable version that is available
 }
@@ -102,6 +102,7 @@ DBM.DefaultOptions = {
 	BlockVersionUpdateNotice = false,
 	ShowSpecialWarnings = true,
 	ShowLHFrame = true,
+	ShowAdvSWSounds = false,
 	AlwaysShowHealthFrame = false,
 	ShowBigBrotherOnCombatStart = false,
 	AutologBosses = false,
