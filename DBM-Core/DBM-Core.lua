a5065a42ebbac0a3970e50d715081d138889f677
subversion <svn@deadlybossmods.com>
2013-05-21 22:07:26 +0200

    tandanu via svn r9594: increment alpha version number

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,8 +44,8 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9592 $"):sub(12, -3)),
-	DisplayVersion = "5.3.0", -- the string that is shown as version
+	Revision = tonumber(("$Revision: 9594 $"):sub(12, -3)),
+	DisplayVersion = "5.3.1 alpha", -- the string that is shown as version
 	ReleaseRevision = 9592 -- the revision of the latest stable version that is available
 }
 
