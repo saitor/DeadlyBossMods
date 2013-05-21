b7f3bc8c312a1a9afc31eee9925fd4fd4bad2c37
subversion <svn@deadlybossmods.com>
2013-05-21 21:43:44 +0200

    increment version number to 5.2.6

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,9 +44,9 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9588 $"):sub(12, -3)),
-	DisplayVersion = "5.2.6 alpha", -- the string that is shown as version
-	ReleaseRevision = 9413 -- the revision of the latest stable version that is available
+	Revision = tonumber(("$Revision: 9589 $"):sub(12, -3)),
+	DisplayVersion = "5.2.6", -- the string that is shown as version
+	ReleaseRevision = 9589 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
