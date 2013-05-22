1479022801557f7a528704f16a162fb0d20d98e7
subversion <svn@deadlybossmods.com>
2013-05-22 22:19:33 +0200

    Omega via svn r9601: apparently lua likes to fire code out of order and sometimes last lines fire before first lines, so have to add pointless nil checks and just have function not work when it executes backwards

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9600 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9601 $"):sub(12, -3)),
 	DisplayVersion = "5.3.1 alpha", -- the string that is shown as version
 	ReleaseRevision = 9592 -- the revision of the latest stable version that is available
 }
@@ -3789,7 +3789,7 @@ function DBM:RoleCheck()
 				roleEventUnregistered = true
 				RolePollPopup:UnregisterEvent("ROLE_POLL_BEGIN")
 			end
-			if role ~= lootrole then
+			if role and lootrole and (role ~= lootrole) then
 				self:AddMsg(DBM_CORE_LOOT_SPEC_REMINDER:format(role, lootrole))
 			end
 		end
