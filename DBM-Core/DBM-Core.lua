a133c68bda870a11ecee54046656525b5a0ca742
subversion <svn@deadlybossmods.com>
2013-05-29 01:40:20 +0200

    Omega via svn r9692: Ru update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9691 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9692 $"):sub(12, -3)),
 	DisplayVersion = "5.3.2 alpha", -- the string that is shown as version
 	ReleaseRevision = 9689 -- the revision of the latest stable version that is available
 }
