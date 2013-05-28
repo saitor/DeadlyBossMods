3e60ac2c27c0b5fcef33649910e587f7137e1789
subversion <svn@deadlybossmods.com>
2013-05-28 02:15:35 +0200

    Omega via svn r9680: Small revert and tweaks to pvp mods. some locals need custom versions of Alliance/Horde Now the code will check for localized version and global so twins and WSG mods don't break

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -44,7 +44,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9679 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9680 $"):sub(12, -3)),
 	DisplayVersion = "5.3.1 alpha", -- the string that is shown as version
 	ReleaseRevision = 9592 -- the revision of the latest stable version that is available
 }
