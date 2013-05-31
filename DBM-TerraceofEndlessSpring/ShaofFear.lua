e3cefbb5faeb84d8699553deb03f10fb9944bf1a
subversion <svn@deadlybossmods.com>
2013-05-31 19:55:00 +0200

    Omega via svn r9720: remove the impossible from sha mod. huddle can never be last

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(709, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9715 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9720 $"):sub(12, -3))
 mod:SetCreatureID(60999)--61042 Cheng Kang, 61046 Jinlun Kun, 61038 Yang Guoshi, 61034 Terror Spawn
 mod:SetUsedIcons(8, 7, 6, 5, 4)
 
@@ -157,21 +157,13 @@ local function startSpecialTimers()
 	if specialsCast == 101 then
 		timerWaterspoutCD:Start()
 	end
-	if specialsCast == 011 then--Should never happen but leaving in case.
-		timerHuddleInTerrorCD:Start()
-		mod:AddMsg("Huddle CAN be cast Last!, tell DBM guys to fix 001 and 010")
-		mod:AddMsg("Huddle CAN be cast Last!, tell DBM guys to fix 001 and 010")
-		mod:AddMsg("Huddle CAN be cast Last!, tell DBM guys to fix 001 and 010")
-	end	
 	if specialsCast == 100 then
 		timerSpoStrCD:Start()
 	end
 	if specialsCast == 010 then--It is believed and backed that huddle is NEVER cast 3rd. Good evidence suggests this is true so changing timers a bit
---		timerHudStrCD:Start()
 		timerHuddleInTerrorCD:Start()
 	end
 	if specialsCast == 001 then
---		timerSpoHudCD:Start()
 		timerHuddleInTerrorCD:Start()
 	end
 end
