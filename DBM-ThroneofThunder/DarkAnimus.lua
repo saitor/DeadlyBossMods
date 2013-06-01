b8f988c74715d7967914638db817f970eb7a934f
subversion <svn@deadlybossmods.com>
2013-06-01 03:17:21 +0200

    Omega via svn r9724: throw in a countdown test function that doesn't do any harm ;) Small failsaves into countdown stuff

--- DBM-ThroneofThunder/DarkAnimus.lua
+++ DBM-ThroneofThunder/DarkAnimus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(824, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9723 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9724 $"):sub(12, -3))
 mod:SetCreatureID(69427)
 mod:SetQuestID(32752)
 mod:SetZone()
@@ -209,6 +209,10 @@ end
 function mod:OnSync(msg, guid)
 	if msg == "WakeTarget" and guid then
 		warnCrimsonWake:Show(DBM:GetFullPlayerNameByGUID(guid))
+	elseif msg == "TestFunction" then
+		countdownAnimaRing:Start(13)
+		timerAnimaRingCD:Start(13)
+		countdownInterruptingJolt:Start(11)
+		timerInterruptingJoltCD:Start(11)
 	end
 end
-
