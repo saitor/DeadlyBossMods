d5d74977b89c06e26e2b7764e332cbce6743cd06
subversion <svn@deadlybossmods.com>
2013-06-03 04:42:09 +0200

    Omega via svn r9739: fix static shock bug

--- DBM-ThroneofThunder/LeiShen.lua
+++ DBM-ThroneofThunder/LeiShen.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(832, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9726 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9739 $"):sub(12, -3))
 mod:SetCreatureID(68397)--Diffusion Chain Conduit 68696, Static Shock Conduit 68398, Bouncing Bolt conduit 68698, Overcharge conduit 68697
 mod:SetQuestID(32756)
 mod:SetZone()
@@ -115,6 +115,7 @@ local staticIcon = 8--Start high and count down
 local overchargeTarget = {}
 local overchargeIcon = 1--Start low and count up
 local helmOfCommandTarget = {}
+local playerName = UnitName("player")
 
 local function warnStaticShockTargets()
 	warnStaticShock:Show(table.concat(staticshockTargets, "<, >"))
@@ -218,12 +219,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then
 			specWarnStaticShock:Show()
 			if not self:IsDifficulty("lfr25") then
-				yellStaticShock:Schedule(7, 1)
-				yellStaticShock:Schedule(6, 2)
-				yellStaticShock:Schedule(5, 3)
-				yellStaticShock:Schedule(4, 4)
+				yellStaticShock:Schedule(7, playerName, 1)
+				yellStaticShock:Schedule(6, playerName, 2)
+				yellStaticShock:Schedule(5, playerName, 3)
+				yellStaticShock:Schedule(4, playerName, 4)
 			end
-			yellStaticShock:Schedule(3, 5)
+			yellStaticShock:Schedule(3, playerName, 5)
 			timerStaticShock:Start()
 			countdownStaticShockFades:Start()
 		else
