e327dd63314f50ee8cb4af2e223c44633d6ed056
root <root@deadlybossmods.com>
2013-06-13 08:55:22 +0200

    Omega via svn r9815: Hopefully fix dark parasite warning on durumu

--- DBM-ThroneofThunder/Durumu.lua
+++ DBM-ThroneofThunder/Durumu.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(818, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9786 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9815 $"):sub(12, -3))
 mod:SetCreatureID(68036)--Crimson Fog 69050, 
 mod:SetQuestID(32750)
 mod:SetZone()
@@ -357,6 +357,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		darkParasiteTargets[#darkParasiteTargets + 1] = args.destName
 		local _, _, _, _, _, duration = UnitDebuff(args.destName, args.spellName)
 		timerDarkParasite:Start(duration, args.destName)
+		self:Unschedule(warnDarkParasiteTargets)
 		if (self:IsDifficulty("heroic25") and #darkParasiteTargets >= 3) or self:IsDifficulty("heroic10") then
 			warnDarkParasiteTargets()
 		else
