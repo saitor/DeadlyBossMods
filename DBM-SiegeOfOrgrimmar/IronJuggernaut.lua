aa7c10cf5d0ae83b4e97e6e2b3760e84640b9d24
root <root@deadlybossmods.com>
2013-06-23 00:32:14 +0200

    Omega via svn r9860: TOC is still 50300 in 5.4, so have to use a diff method for checking Add pandaria mods for 5.4

--- DBM-SiegeOfOrgrimmar/IronJuggernaut.lua
+++ DBM-SiegeOfOrgrimmar/IronJuggernaut.lua
@@ -1,8 +1,8 @@
-if select(4, GetBuildInfo()) < 50400 then return end
+if GetBuildInfo() ~= "5.4.0" then return end
 local mod	= DBM:NewMod(864, "DBM-FallOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9857 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9860 $"):sub(12, -3))
 --mod:SetCreatureID(69465)
 --mod:SetQuestID(32744)
 mod:SetZone()
