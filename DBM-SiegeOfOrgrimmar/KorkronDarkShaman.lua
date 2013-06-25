b15bee3429c196a5b2f547d53da54520b58448bf
root <root@deadlybossmods.com>
2013-06-25 06:18:33 +0200

    Omega via svn r9885: oh, and add other CID

--- DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
+++ DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
@@ -1,8 +1,8 @@
 local mod	= DBM:NewMod(856, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9884 $"):sub(12, -3))
-mod:SetCreatureID(71859)--haromm
+mod:SetRevision(("$Revision: 9885 $"):sub(12, -3))
+mod:SetCreatureID(71859, 71858)--haromm, Kardris
 --mod:SetQuestID(32744)
 mod:SetZone()
 
