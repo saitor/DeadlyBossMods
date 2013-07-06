e1ec50ddd986c25e2acb50fdb3dc366f3e32d8b8
root <root@deadlybossmods.com>
2013-07-06 23:33:23 +0200

    Omega via svn r9965: disable wake yell in LFR

--- DBM-ThroneofThunder/DarkAnimus.lua
+++ DBM-ThroneofThunder/DarkAnimus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(824, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9863 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 9965 $"):sub(12, -3))
 mod:SetCreatureID(69427)
 mod:SetQuestID(32752)
 mod:SetZone()
@@ -194,7 +194,9 @@ function mod:RAID_BOSS_WHISPER(msg, npc)
 		if self:AntiSpam(3, 1) then--This actually doesn't spam, but we ues same antispam here so that the MOVE warning doesn't fire at same time unless you fail to move for 2 seconds
 			specWarnCrimsonWakeYou:Show()
 		end
-		yellCrimsonWake:Yell()
+		if not self:IsDifficulty("lfr25") then
+			yellCrimsonWake:Yell()
+		end
 		soundCrimsonWake:Play()
 		self:SendSync("WakeTarget", UnitGUID("player"))
 	end
