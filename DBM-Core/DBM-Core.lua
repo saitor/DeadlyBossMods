67338f46cc7a3ad51f620f0e7cf06fd32c102630
root <root@deadlybossmods.com>
2013-07-10 05:06:12 +0200

    Omega via svn r9997: filter proving grounds from scenario check. NOt sure what to do with these yet. Probably will just be mods that have no "combat"

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -43,7 +43,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 9996 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 9997 $"):sub(12, -3)),
 	DisplayVersion = "5.3.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 9947 -- the revision of the latest stable version that is available
 }
@@ -1982,7 +1982,7 @@ do
 		LastInstanceMapID = mapID
 		if instanceType == "none" and (mapID ~= 369) and (mapID ~= 1043) and (mapID ~= 974) then return end -- instance type of brawlers guild and DMF are none
 		self:LoadModsOnDemand("mapId", mapID)
-		if instanceType == "scenario" and self:GetModByName("d511") then--mod already loaded
+		if instanceType == "scenario" and (mapID ~= 1148) and self:GetModByName("d511") then--mod already loaded (Filter 1148, which is proving grounds)
 			DBM:InstanceCheck()
 		end
 	end
@@ -2001,7 +2001,7 @@ end
 
 --Scenario mods
 function DBM:InstanceCheck()
-	if combatInfo[LastInstanceMapID] then--Scenarios not yet moved over to LastInstanceMapID
+	if combatInfo[LastInstanceMapID] then
 		for i, v in ipairs(combatInfo[LastInstanceMapID]) do
 			if (v.type == "scenario") and checkEntry(v.msgs, LastInstanceMapID) then
 				DBM:StartCombat(v.mod, 0)
