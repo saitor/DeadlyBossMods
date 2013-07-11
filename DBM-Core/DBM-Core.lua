21f7962668028563659d256bd3f4c24c344e308b
root <root@deadlybossmods.com>
2013-07-11 19:41:27 +0200

    Omega via svn r10023: Lets try this fix for JuJu

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -43,7 +43,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10022 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10023 $"):sub(12, -3)),
 	DisplayVersion = "5.3.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 9947 -- the revision of the latest stable version that is available
 }
@@ -776,6 +776,7 @@ do
 				"LOADING_SCREEN_DISABLED"
 			)
 			self:GROUP_ROSTER_UPDATE()
+			self:LOADING_SCREEN_DISABLED()
 			self:Schedule(1.5, function()
         		combatInitialized = true
 			end)
