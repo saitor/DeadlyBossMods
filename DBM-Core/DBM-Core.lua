6494f84ac3dc27ac85afb131b546d3a13a8bd4d0
root <root@deadlybossmods.com>
2013-07-13 12:57:24 +0200

    ultrashining via svn r10039: zhTW update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -43,7 +43,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10038 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10039 $"):sub(12, -3)),
 	DisplayVersion = "5.3.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 9947 -- the revision of the latest stable version that is available
 }
