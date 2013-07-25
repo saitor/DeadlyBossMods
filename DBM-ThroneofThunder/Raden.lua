f31557c6e95012a89ddeeec278f54e4c08982314
root <root@deadlybossmods.com>
2013-07-25 06:53:28 +0200

    Omega via svn r10081: fix bug that could occur if someone died on raden before infoframe:show is called

--- DBM-ThroneofThunder/Raden.lua
+++ DBM-ThroneofThunder/Raden.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(831, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10044 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10081 $"):sub(12, -3))
 mod:SetCreatureID(69473)--69888
 mod:SetQuestID(32753)
 mod:SetZone()
@@ -302,7 +302,7 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 end
 
 function mod:UNIT_DIED(args)
-	if not args:IsDestTypeHostile() and self.Options.InfoFrame then
+	if not args:IsDestTypeHostile() and self.Options.InfoFrame and DBM.InfoFrame:IsShown() then
 		DBM.InfoFrame:Update("reverseplayerbaddebuff")--Force update so player dies it reflects this
 	end
 end
