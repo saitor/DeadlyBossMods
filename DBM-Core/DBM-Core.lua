8588412f3b899e5fa1937ff027c09da897cc6dba
root <root@deadlybossmods.com>
2013-07-30 23:48:34 +0200

    Omega via svn r10108: improve pvp mod check more to account for fact out of date ones may not load AT ALL

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -43,7 +43,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10107 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10108 $"):sub(12, -3)),
 	DisplayVersion = "5.3.6 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.3.5", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10055 -- the revision of the latest stable version that is available
@@ -2104,9 +2104,13 @@ function DBM:LoadMod(mod)
 			RequestChallengeModeMapInfo()
 			RequestChallengeModeLeaders(mapID)
 		end
-		if instanceType == "pvp" and DBM:GetModByName("z30") and DBM:GetModByName("z30").revision == 0 then
-			--Not the new stand alone pvp mods these are old ones and user needs to remove them or install updated package
-			self:AddMsg(DBM_CORE_OUTDATED_PVP_MODS)
+		if instanceType == "pvp" and IsAddOnLoaded("DBM-PVP") then--Is a battleground and pvp mods are installed
+			if DBM:GetModByName("z30") and DBM:GetModByName("z30").revision >= 3 then--They are loaded and correct revision
+				--Do nothing
+			else--They either aren't loaded or are wrong revision. in either case, it means they have old pvp mods installed that don't load correctly or are out of date
+				--Not the new stand alone pvp mods these are old ones and user needs to remove them or install updated package
+				self:AddMsg(DBM_CORE_OUTDATED_PVP_MODS)
+			end
 		end
 		if not InCombatLockdown() then--We loaded in combat because a raid boss was in process, but lets at least delay the garbage collect so at least load mod is half as bad, to do our best to avoid "script ran too long"
 			collectgarbage("collect")
