c24b8b37ac34b8eaec3ab72e1b1e042d8a000ed4
root <root@deadlybossmods.com>
2013-08-01 22:01:59 +0200

    Omega via svn r10115: Add Galakras engage detection for horde as well. Add missing fire warning for the VERY hard to see fire on ground.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -43,7 +43,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10114 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10115 $"):sub(12, -3)),
 	DisplayVersion = "5.3.6 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.3.5", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10055 -- the revision of the latest stable version that is available
