2022cd1256cf65f5d4bd8b5f8c8e6a12b8b42553
subversion <svn@deadlybossmods.com>
2013-08-13 08:05:08 +0200

    Omega via svn r10146: add armageddon timer/countdown to spoils

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -49,7 +49,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10145 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10146 $"):sub(12, -3)),
 	DisplayVersion = "5.3.6 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.3.5", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10055 -- the revision of the latest stable version that is available
