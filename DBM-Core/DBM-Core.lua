d78c34dc0bcd8d7d3d0314bb0ca6e477edb245df
subversion <svn@deadlybossmods.com>
2013-08-27 03:10:46 +0200

    Omega via svn r10189: More Ru

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10188 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10189 $"):sub(12, -3)),
 	DisplayVersion = "5.3.7 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.3.6", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10174 -- the revision of the latest stable version that is available
