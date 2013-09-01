7c19c64a01009b46b122dc8c69b76b9286954f75
subversion <svn@deadlybossmods.com>
2013-09-01 20:12:59 +0200

    Omega via svn r10209: Going through classic 5 man mods, i realize should add a reflect special warning generic since there are a few of them in the 5 man mods and the fact a lot of the 5 man mods from earlier expansions are still missing languages. Also, take 2 on combat start generic as well

--- DBM-SiegeOfOrgrimmar/Norushen.lua
+++ DBM-SiegeOfOrgrimmar/Norushen.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(866, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10191 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10209 $"):sub(12, -3))
 mod:SetCreatureID(72276)
 mod:SetZone()
 
@@ -50,7 +50,7 @@ local specWarnHurlCorruption			= mod:NewSpecialWarningInterrupt(144649, nil, nil
 local specWarnPiercingCorruption		= mod:NewSpecialWarningSpell(144657)
 
 --Amalgam of Corruption
-local timerCombatStarts					= mod:NewTimer(25, "timerCombatStarts", 2457)
+local timerCombatStarts					= mod:NewCombatTimer(25)
 local timerUnleashedAngerCD				= mod:NewCDTimer(11, 145216, nil, mod:IsTank())
 local timerBlindHatred					= mod:NewBuffActiveTimer(30, 145226)
 local timerBlindHatredCD				= mod:NewNextTimer(30, 145226)
