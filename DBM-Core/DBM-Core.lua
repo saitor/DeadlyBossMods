723a6761505ce2cd178ee34080b0fe75d48b5ac0
subversion <svn@deadlybossmods.com>
2013-09-13 23:47:28 +0200

    Omega via svn r10297: remove unneeded global with 5.4

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10296 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10297 $"):sub(12, -3)),
 	DisplayVersion = "5.4.1 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.0", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10267 -- the revision of the latest stable version that is available
@@ -3518,7 +3518,7 @@ end
 function DBM:GetCurrentInstanceDifficulty()
 	local _, instanceType, difficulty, difficultyName, maxPlayers, _, _, _, instanceGroupSize = GetInstanceInfo()
 	if difficulty == 0 then
-		return "worldboss", DBM_CORE_WORLD_BOSS.." - "
+		return "worldboss", RAID_INFO_WORLD_BOSS.." - "
 	elseif difficulty == 1 then
 		return "normal5", difficultyName.." - "
 	elseif difficulty == 2 then
