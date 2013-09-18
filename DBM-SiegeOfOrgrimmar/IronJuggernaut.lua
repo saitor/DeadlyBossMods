a438dd26785d6903aab50d932c05c886a551d447
subversion <svn@deadlybossmods.com>
2013-09-18 05:37:55 +0200

    Omega via svn r10327: Disabled some buggy tank warnings on darkshaman until i can revisit why they were screwing up. Add tank checks to tank debuffs on galakras and iron jug to avoid bad spam. Fix bug when galakras isn't boss1. Reduce tank radius on galakras to avoid getting tower warnings when on ground or ground warnings when in tower (at least hopefully)

--- DBM-SiegeOfOrgrimmar/IronJuggernaut.lua
+++ DBM-SiegeOfOrgrimmar/IronJuggernaut.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(864, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10206 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10327 $"):sub(12, -3))
 mod:SetCreatureID(71466)
 mod:SetZone()
 
@@ -127,15 +127,18 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 144467 then
-		local amount = args.amount or 1
-		warnIgniteArmor:Show(args.destName, amount)
-		timerIgniteArmor:Start(args.destName)
-		timerIgniteArmorCD:Start()
-		if amount >= 3 then
-			if args:IsPlayer() then
-				specWarnIgniteArmor:Show(amount)
-			else
-				specWarnIgniteArmorOther:Show(args.destName)
+		local uId = DBM:GetRaidUnitId(args.destName)
+		if self:IsTanking(uId, "boss1") then
+			local amount = args.amount or 1
+			warnIgniteArmor:Show(args.destName, amount)
+			timerIgniteArmor:Start(args.destName)
+			timerIgniteArmorCD:Start()
+			if amount >= 3 then
+				if args:IsPlayer() then
+					specWarnIgniteArmor:Show(amount)
+				else
+					specWarnIgniteArmorOther:Show(args.destName)
+				end
 			end
 		end
 	elseif args.spellId == 146325 then
