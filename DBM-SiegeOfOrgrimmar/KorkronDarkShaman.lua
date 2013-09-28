d136e35aefc76615f52ef2871ff6bdb2460b0f07
subversion <svn@deadlybossmods.com>
2013-09-28 17:01:37 +0200

    Omega via svn r10440: Oops, fix a stupid

--- DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
+++ DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(856, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10439 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10440 $"):sub(12, -3))
 mod:SetCreatureID(71859, 71858)--haromm, Kardris
 mod:SetZone()
 mod:SetUsedIcons(5, 4, 3, 2, 1)
@@ -71,7 +71,7 @@ local timerIronPrisonCD				= mod:NewCDTimer(31.5, 144330)--Pretty much a next ti
 local timerIronPrisonSelf			= mod:NewBuffFadesTimer(60, 144330)
 
 local countdownFoulGeyser			= mod:NewCountdown(32.5, 143990, mod:IsTank() or mod:IsRangedDps(), nil, nil, nil, nil, 2)
-local countdownFallingAsh			= mod:NewCountdown(15, 143973)
+local countdownFallingAsh			= mod:NewCountdown(15, 143973, nil, nil, nil, nil, true)
 
 local berserkTimer					= mod:NewBerserkTimer(540)
 
