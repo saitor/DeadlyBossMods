1764600e9a3c5c84cfdc93f2f2e2910d8b236486
subversion <svn@deadlybossmods.com>
2013-10-05 03:55:34 +0200

    nbluewiz via svn r10547: # Immerseus : Prevent false wipe. / Fix timer not freshing when residue stacking.

--- DBM-SiegeOfOrgrimmar/Immerseus.lua
+++ DBM-SiegeOfOrgrimmar/Immerseus.lua
@@ -1,9 +1,10 @@
 local mod	= DBM:NewMod(852, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10407 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10547 $"):sub(12, -3))
 mod:SetCreatureID(71543)--Doesn't die, will need kill detection
 mod:SetReCombatTime(45)--Lets just assume he has same bug as tsulong in advance and avoid problems
+mod:SetWipeTime(20)--Kill yell fires litte lately, prevent wipe.
 mod:SetZone()
 
 mod:RegisterCombat("combat")
@@ -11,6 +12,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
 	"SPELL_PERIODIC_DAMAGE",
 	"SPELL_PERIODIC_MISSED",
@@ -85,6 +87,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		specWarnSwellingCorruptionTarget:Show(args.destName)
 	end
 end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args.spellId == 143459 and args:IsPlayer() then
