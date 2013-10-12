1c548c3b63c0b6d68ba8c32075b8d11353892f1a
subversion <svn@deadlybossmods.com>
2013-10-12 15:03:26 +0200

    nbluewiz via svn r10613: # Spoils : Bliz suck.. Add timer correction code. (untested)

--- DBM-SiegeOfOrgrimmar/SpoilsOfPandria.lua
+++ DBM-SiegeOfOrgrimmar/SpoilsOfPandria.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(870, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10612 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10613 $"):sub(12, -3))
 mod:SetCreatureID(73720, 71512)
 mod:SetZone()
 
@@ -296,10 +296,11 @@ function mod:UPDATE_WORLD_STATES()
 	local text = select(4, GetWorldStateUIInfo(5))
 	local time = tonumber(string.match(text or "", "%d+"))
 	if time > worldTimer then
+		local newTime = time + (time/100) + 1 -- bliz timer litte slow. wtf? If this correction is not right, need to timer update every 30s or 1m.
 		berserkTimer:Cancel()
 		countdownArmageddon:Cancel()
-		berserkTimer:Start(time)
-		countdownArmageddon:Start(time)
+		berserkTimer:Start(newTime)
+		countdownArmageddon:Start(newTime)
 	end
 	worldTimer = time
 end
