1773079e723db18cc458e39d08a425ed5c9903d2
subversion <svn@deadlybossmods.com>
2013-10-17 07:00:15 +0200

    nbluewiz via svn r10648: # Spoils : Fix lua error.

--- DBM-SiegeOfOrgrimmar/SpoilsOfPandria.lua
+++ DBM-SiegeOfOrgrimmar/SpoilsOfPandria.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(870, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10642 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10648 $"):sub(12, -3))
 mod:SetCreatureID(73720, 71512)
 mod:SetZone()
 
@@ -318,6 +318,7 @@ end
 function mod:UPDATE_WORLD_STATES()
 	local text = select(4, GetWorldStateUIInfo(5))
 	local time = tonumber(string.match(text or "", "%d+"))
+	if not time then return end
 	if time > worldTimer then
 		maxTimer = time
 		berserkTimer:Cancel()
