79b258e89266df3883293dbf02b7ea9ec911be84
subversion <svn@deadlybossmods.com>
2013-10-20 20:38:48 +0200

    Omega via svn r10663: kill first adds timer on galakras. leaving comment for now to explain reasoning.

--- DBM-SiegeOfOrgrimmar/Galakras.lua
+++ DBM-SiegeOfOrgrimmar/Galakras.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(868, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10591 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10663 $"):sub(12, -3))
 mod:SetCreatureID(72311, 72560, 72249, 73910, 72302, 72561, 73909)--Boss needs to engage off friendly NCPS, not the boss. I include the boss too so we don't detect a win off losing varian. :)
 mod:SetReCombatTime(180, 15)--fix combat re-starts after killed. Same issue as tsulong. Fires TONS of IEEU for like 1-2 minutes after fight ends.
 mod:SetMainBossID(72249)
@@ -101,7 +101,7 @@ function mod:OnCombatStart(delay)
 	addsCount = 0
 	firstTower = 0
 	flamesCount = 0
-	timerAddsCD:Start(6.5-delay)
+--	timerAddsCD:Start(6.5-delay)--First wave actually seems to have a couple second variation, since timer is so short anyways, just disabling it
 	if not self:IsDifficulty("heroic10", "heroic25") then
 		timerTowerCD:Start(116.5-delay)
 	else
