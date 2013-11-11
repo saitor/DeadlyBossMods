40a8c6ecfc3fec0630656065cf124ccf421a85ab
subversion <svn@deadlybossmods.com>
2013-11-11 20:51:12 +0100

    Omega via svn r10721: mod-->self

--- DBM-SiegeOfOrgrimmar/Galakras.lua
+++ DBM-SiegeOfOrgrimmar/Galakras.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(868, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10707 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10721 $"):sub(12, -3))
 mod:SetCreatureID(72311, 72560, 72249, 73910, 72302, 72561, 73909)--Boss needs to engage off friendly NCPS, not the boss. I include the boss too so we don't detect a win off losing varian. :)
 mod:SetReCombatTime(180, 15)--fix combat re-starts after killed. Same issue as tsulong. Fires TONS of IEEU for like 1-2 minutes after fight ends.
 mod:SetMainBossID(72249)
@@ -103,7 +103,7 @@ function mod:OnCombatStart(delay)
 		timerTowerCD:Start(116.5-delay)
 	else
 		timerTowerGruntCD:Start(6)
-		mod:Schedule(6, TowerGrunt)
+		self:Schedule(6, TowerGrunt)
 	end
 end
 
