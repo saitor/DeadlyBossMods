f6ed3ffdcaf8f05a4e457f374438b8bf375b1951
subversion <svn@deadlybossmods.com>
2013-11-19 06:44:07 +0100

    Omega via svn r10735: Completed tested and working ENCOUNTER_START/END support.

--- DBM-Party-MoP/JadeSerpentTemple/LibraryEvent.lua
+++ DBM-Party-MoP/JadeSerpentTemple/LibraryEvent.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(664, "DBM-Party-MoP", 1, 313)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10728 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10735 $"):sub(12, -3))
 mod:SetCreatureID(59051, 59726, 58826)--59051 (Strife), 59726 (Anger), 58826 (Zao Sunseeker). This event has a random chance to be Zao (solo) or Anger and Strife (together)
 mod:SetEncounterID(1417)
 mod:SetZone()
@@ -12,6 +12,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
 	"UNIT_DIED"
 )
 
@@ -39,6 +40,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnUltimatePower:Show(args.destName)
 		specWarnUltimatePower:Show(args.destName)
 		timerUltimatePower:Start(args.destName)
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 122714 then
+		DBM:EndCombat(self)--Alternte win detection, UNIT_DIED not fire for 59051 (Strife), 59726 (Anger)
 	end
 end
 
