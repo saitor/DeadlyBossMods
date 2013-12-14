e1c28b6702dcbf2d2150343f725f82f904d50ee5
subversion <svn@deadlybossmods.com>
2013-12-14 07:31:53 +0100

    Omega via svn r10792: fix another error

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10791 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10792 $"):sub(12, -3)),
 	DisplayVersion = "5.4.6 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.5", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10737 -- the revision of the latest stable version that is available
@@ -2400,7 +2400,7 @@ do
 	syncHandlers["EE"] = function(sender, eId, success, mod, modRevision)
 		if select(2, IsInInstance()) == "pvp" then return end
 		eId = tonumber(eId or "")
-		success = tonumber(wipe)
+		success = tonumber(success)
 		mod = DBM:GetModByName(mod or "")
 		modRevision = tonumber(modRevision or 0) or 0
 		if eId and success and (not mod.minSyncRevision or modRevision >= mod.minSyncRevision) and not eeSyncSender[sender] then
