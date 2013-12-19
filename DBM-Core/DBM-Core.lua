f1eea47c0b0ccae8d88f02de5f857decaa7c350d
subversion <svn@deadlybossmods.com>
2013-12-19 05:20:17 +0100

    nbluewiz via svn r10826: # Debug to figure out rare icon bugs.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10825 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10826 $"):sub(12, -3)),
 	DisplayVersion = "5.4.6 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.5", -- Needed to work around bigwigs sending improper version information
 	ReleaseRevision = 10737 -- the revision of the latest stable version that is available
@@ -6911,6 +6911,9 @@ do
 	local function clearSortTable()
 		table.wipe(iconSortTable)
 		iconSet = 0
+		if DBM.Options.DebugMode then
+			print("iconSortTable cleared")
+		end
 	end
 
 	function bossModPrototype:SetIconBySortedTable(startIcon, reverseIcon, returnFunc)
