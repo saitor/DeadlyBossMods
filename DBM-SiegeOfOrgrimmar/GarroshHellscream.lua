a799a61ba319fd128e30f88d1ab19b24d151079a
subversion <svn@deadlybossmods.com>
2014-01-04 22:33:03 +0100

    Omega via svn r10891: ALso, make garrosh infoframe healer option. doesn't concern dps/tanks who else in raid doesn't have reduction. if RL isn't healer it does concern them but they can turn it on.

--- DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
+++ DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(869, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10884 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10891 $"):sub(12, -3))
 mod:SetCreatureID(71865)
 mod:SetEncounterID(1623)
 mod:SetHotfixNoticeRev(10828)
@@ -114,7 +114,7 @@ mod:AddBoolOption("yellMaliceFading", false)
 mod:AddSetIconOption("SetIconOnShaman", "ej8294", false, true)
 mod:AddSetIconOption("SetIconOnMC", 145071, false)
 mod:AddSetIconOption("SetIconOnMalice", 147209, false)
-mod:AddBoolOption("InfoFrame")
+mod:AddBoolOption("InfoFrame", mod:IsHealer())
 mod:AddBoolOption("RangeFrame")
 
 mod:Phase(1)
