cd0645e8e1b50d97015818d968fafc5a92716e64
subversion <svn@deadlybossmods.com>
2014-01-13 16:59:07 +0100

    nbluewiz via svn r10989: # Fix scenario error.

--- DBM-Scenario-MoP/BattleontheHighSeas/BattleHighSeas.lua
+++ DBM-Scenario-MoP/BattleontheHighSeas/BattleHighSeas.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("d652", "DBM-Scenario-MoP")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10971 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 10989 $"):sub(12, -3))
 mod:SetZone()
 
 mod:RegisterCombat("scenario", 1099)
@@ -53,7 +53,7 @@ function mod:SPELL_CAST_START(args)
 		timerVerticalSlashCD:Start()
 	elseif args.spellId == 136473 then
 		warnCounterShot:Show()
-		specWarnCounterShot:Shot()
+		specWarnCounterShot:Show()
 		timerCounterShot:Start()
 	end
 end
