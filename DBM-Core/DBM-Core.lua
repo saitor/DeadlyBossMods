e46cbc9ec3c4a7e2c3ba97fde2396cdb596d7271
subversion <svn@deadlybossmods.com>
2014-01-13 10:27:36 +0100

    nbluewiz via svn r10987: # Very little range frame tweaks

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 10986 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 10987 $"):sub(12, -3)),
 	DisplayVersion = "5.4.7 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.6", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 10835-- the revision of the latest stable version that is available
