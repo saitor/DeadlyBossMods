eb25f8fb844c47105753ac12d03dfa2d3683d211
subversion <svn@deadlybossmods.com>
2014-01-20 08:18:21 +0100

    Omega via svn r11016: fix paragons mod announcing invalid 6th target after all 5 found. 6th target was often tank, one of mutate targets cause it was catching him looking at them.

--- DBM-SiegeOfOrgrimmar/ParagonsOfTheKlaxxi.lua
+++ DBM-SiegeOfOrgrimmar/ParagonsOfTheKlaxxi.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(853, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10981 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 11016 $"):sub(12, -3))
 mod:SetCreatureID(71152, 71153, 71154, 71155, 71156, 71157, 71158, 71160, 71161)
 mod:SetEncounterID(1593)
 mod:DisableESCombatDetection()
@@ -470,7 +470,7 @@ function mod:FlashScan(targetname)
 			specWarnFlashNear:Show(targetname)
 		end
 	end
-	if self.vb.whirlCast > expectedWhirlCount or (GetTime() - self.vb.whirlTime) > 20 then
+	if self.vb.whirlCast >= expectedWhirlCount or (GetTime() - self.vb.whirlTime) > 20 then
 		self:StopRepeatedScan("FlashScan")
 	end
 end
