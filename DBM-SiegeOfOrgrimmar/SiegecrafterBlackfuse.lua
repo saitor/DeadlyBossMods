ffc3adc73b90ba48b499adb24ea9f8f62552fd61
subversion <svn@deadlybossmods.com>
2014-03-01 06:00:22 +0100

    Omega via svn r11076: #Fix siegecrafter charge warnings not working for tank strats that never reset their stats (they cap at 10 then only fire SPELL_AURA_REFRESH)

--- DBM-SiegeOfOrgrimmar/SiegecrafterBlackfuse.lua
+++ DBM-SiegeOfOrgrimmar/SiegecrafterBlackfuse.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(865, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 10977 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 11076 $"):sub(12, -3))
 mod:SetCreatureID(71504)--71591 Automated Shredder
 mod:SetEncounterID(1601)
 mod:SetZone()
@@ -15,6 +15,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_SUMMON 143641",
 	"SPELL_AURA_APPLIED 145365 143385 145444 144210 144236 145269 145580 144466 143856",
 	"SPELL_AURA_APPLIED_DOSE 143385 145444 143856",
+	"SPELL_AURA_REFRESS 143385",
 	"SPELL_AURA_REMOVED 143385 144236 145269",
 	"UNIT_DIED",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
@@ -272,6 +273,17 @@ function mod:SPELL_AURA_APPLIED(args)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:SPELL_AURA_REFRESS(args)
+	local spellId = args.spellId
+	if spellId == 143385 and args:IsDestTypePlayer() then
+		local amount = args.amount or 1
+		warnElectroStaticCharge:Show(args.destName, amount)
+		timerElectroStaticCharge:Start(args.destName)
+		timerElectroStaticChargeCD:Start()
+		countdownElectroStatic:Start()
+	end
+end
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
