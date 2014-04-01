c16ea2e86aa31785aedfec77858a92fe86d95bb4
subversion <svn@deadlybossmods.com>
2014-04-01 02:29:29 +0200

    Omega via svn r11153: add optional special warnings a road tank might wand to consider turning on if they are having this problem: http://us.battle.net/wow/en/forum/topic/10487298692?page=2#24

--- DBM-SiegeOfOrgrimmar/Galakras.lua
+++ DBM-SiegeOfOrgrimmar/Galakras.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(868, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 11132 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 11153 $"):sub(12, -3))
 mod:SetCreatureID(72311, 72560, 72249, 73910, 72302, 72561, 73909)--Boss needs to engage off friendly NCPS, not the boss. I include the boss too so we don't detect a win off losing varian. :)
 mod:SetEncounterID(1622)
 mod:DisableESCombatDetection()
@@ -57,6 +57,8 @@ local warnFlamesofGalakrond			= mod:NewStackAnnounce(147029, 2, nil, mod:IsTank(
 local warnPulsingFlames				= mod:NewCountAnnounce(147042, 3)
 
 --Stage 2: Bring Her Down!
+local specWarnAdds					= mod:NewSpecialWarningSpell("ej8553", false)
+local specWarnProto					= mod:NewSpecialWarningSpell("ej8587", false)
 local specWarnWarBanner				= mod:NewSpecialWarningSwitch(147328, not mod:IsHealer())
 local specWarnFracture				= mod:NewSpecialWarningTarget(146899, mod:IsHealer())
 local specWarnChainheal				= mod:NewSpecialWarningInterrupt(146757)
@@ -105,6 +107,9 @@ mod.vb.pulseCount = 0
 local function protos()
 	mod.vb.addsCount = mod.vb.addsCount + 1
 	warnProto:Show(mod.vb.addsCount)
+	if UnitPower("player", 10) == 0 then
+		specWarnProto:Show()
+	end
 	timerAddsCD:Start(nil, mod.vb.addsCount + 1)
 end
 
@@ -318,6 +323,9 @@ function mod:OnSync(msg)
 		self.vb.addsCount = self.vb.addsCount + 1
 		if self.vb.addsCount % 5 == 3 then
 			warnAdd:Show(self.vb.addsCount)
+			if UnitPower("player", 10) == 0 then
+				specWarnAdds:Show()
+			end
 			timerProtoCD:Start(nil, self.vb.addsCount + 1)
 			self:Schedule(55, protos)
 		elseif self.vb.addsCount == 1 then
