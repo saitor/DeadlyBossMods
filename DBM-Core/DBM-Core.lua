0f420189e4def854ce6344a248fba39bae3a16b6
subversion <svn@deadlybossmods.com>
2014-04-19 20:17:34 +0200

    Omega via svn r11205: fix ctimer

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11204 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11205 $"):sub(12, -3)),
 	DisplayVersion = "5.4.14 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.13", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 11193 -- the revision of the latest stable version that is available
@@ -1558,16 +1558,16 @@ do
 		end
 		if not DBM.Options.DontPlayPTCountdown then
 			fakeMod.countdown:Cancel()
-			fakeMod.countdown:Start(timer)
+			fakeMod.countdown:Start(time)
 		end
 		if not DBM.Options.DontShowPTCountdownText then
 			DBM:Unschedule(countDownTextDelay)
 			TimerTracker_OnEvent(TimerTracker, "PLAYER_ENTERING_WORLD")
 			local threshold = DBM.Options.PTCountThreshold
-			if timer > threshold then
-				DBM:Schedule(timer-threshold, countDownTextDelay, threshold)
+			if time > threshold then
+				DBM:Schedule(time-threshold, countDownTextDelay, threshold)
 			else
-				TimerTracker_OnEvent(TimerTracker, "START_TIMER", 2, timer, timer)
+				TimerTracker_OnEvent(TimerTracker, "START_TIMER", 2, time, time)
 			end
 		end
 		self.Bars:CreateBar(time, text, "Interface\\Icons\\Spell_Holy_BorrowedTime")
