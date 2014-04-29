d9b3893c844e7ab2df15b34e14de46d4d539f0b9
subversion <svn@deadlybossmods.com>
2014-04-29 04:55:40 +0200

    Omega via svn r11234: And, as fall back, if setskin is called before options loaded, force load them.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -50,7 +50,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11233 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11234 $"):sub(12, -3)),
 	DisplayVersion = "5.4.14 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.13", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 11193 -- the revision of the latest stable version that is available
