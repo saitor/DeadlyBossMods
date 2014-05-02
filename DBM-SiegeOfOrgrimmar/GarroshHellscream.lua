d1d311a60399cf3509d4c4ac85cebcf1c120b4db
subversion <svn@deadlybossmods.com>
2014-05-02 23:12:55 +0200

    Omega via svn r11246: Change that to a fades

--- DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
+++ DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(869, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 11245 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 11246 $"):sub(12, -3))
 mod:SetCreatureID(71865)
 mod:SetEncounterID(1623)
 mod:SetHotfixNoticeRev(10828)
@@ -117,7 +117,7 @@ local countdownWhirlingCorruption	= mod:NewCountdown(49.5, 144985)
 local countdownTouchOfYShaarj		= mod:NewCountdown("Alt45", 145071, false)--Off by default only because it's a cooldown and it does have a 45-48sec variation
 local countdownRealm				= mod:NewCountdownFades(60.5, "ej8305", nil, nil, 10)
 local countdownBombardment			= mod:NewCountdown(55, 147120)
-local countdownBombardmentEnd		= mod:NewCountdown("Alt13", 147120)
+local countdownBombardmentEnd		= mod:NewCountdownFades("Alt13", 147120)
 
 mod:AddBoolOption("yellMaliceFading", false)
 mod:AddSetIconOption("SetIconOnShaman", "ej8294", false, true)
