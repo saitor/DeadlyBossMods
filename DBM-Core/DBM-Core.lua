7f3188d0227fbe5e5f7754028c3b92b36521a17a
subversion <svn@deadlybossmods.com>
2014-05-14 04:01:14 +0200

    Omega via svn r11260: Seems My nameplate fixing may not be broken after all, since DBM gets to set them before entering combat, and can restore them (on a delay) after leaving combat). TPTP settings changes via DBM still work in combat, so all seems good, for now.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,7 +51,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11259 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11260 $"):sub(12, -3)),
 	DisplayVersion = "5.4.14 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.13", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 11193 -- the revision of the latest stable version that is available
