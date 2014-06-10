263ebc5d59a2c9ce1f1fd097de4959377407e945
subversion <svn@deadlybossmods.com>
2014-06-11 00:29:17 +0200

    Omega via svn r11275: Go back to only blocking cinematics inside instances

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,7 +51,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11274 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11275 $"):sub(12, -3)),
 	DisplayVersion = "5.4.16 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.15", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 11270 -- the revision of the latest stable version that is available
@@ -2456,7 +2456,7 @@ function DBM:UPDATE_BATTLEFIELD_STATUS()
 end
 
 function DBM:CINEMATIC_START()
-	if DBM.Options.MovieFilter == "Never" then return end
+	if IsInInstance() or DBM.Options.MovieFilter == "Never" then return end
 	SetMapToCurrentZone()
 	for itemId, mapId in pairs(blockMovieSkipItems) do
 		if mapId == LastInstanceMapID then
