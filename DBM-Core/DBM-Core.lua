0e18bd7fb4f2deae96fd9d11ffffe8a693fbcc39
subversion <svn@deadlybossmods.com>
2014-06-11 03:23:49 +0200

    Omega via svn r11278: Support custom sounds if a user creates a folder named DBM-CustomSounds and puts ogg files in there following naming format "Custom1.ogg" "Custom2.ogg" etc. Because this is a user created folder, files will never be replaced by a DBM update. Better than asking user to always redrag files into existing DBM folders. After this, since no GUI yet (not sure I'll even add one) you must set CustomSounds to the number of sounds you added so they register with LSM. Setting this wrong will register blank sounds (which is why it's not in GUI)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,7 +51,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11277 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11278 $"):sub(12, -3)),
 	DisplayVersion = "5.4.16 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.15", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 11270 -- the revision of the latest stable version that is available
@@ -116,6 +116,7 @@ DBM.DefaultOptions = {
 	ShowSpecialWarnings = true,
 	ShowFlashFrame = true,
 	ShowAdvSWSound = true,
+	CustomSounds = 0,
 	AlwaysShowHealthFrame = false,
 	ShowBigBrotherOnCombatStart = false,
 	AutologBosses = false,
