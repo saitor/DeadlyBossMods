5df957ef4dee682d8a724ba05b2f27a64dc951a3
subversion <svn@deadlybossmods.com>
2014-08-16 11:28:25 +0200

    nbluewiz via svn r11518: # Grim Rail Depot update. # Fix several bugs.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,7 +51,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11508 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11518 $"):sub(12, -3)),
 	DisplayVersion = "5.4.18 alpha", -- the string that is shown as version
 	DisplayReleaseVersion = "5.4.17", -- Needed to work around old versions of BW sending improper version information
 	ReleaseRevision = 11475 -- the revision of the latest stable version that is available
