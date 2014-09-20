f036d707b649c8463193f18576a9416edf5d9458
subversion <svn@deadlybossmods.com>
2014-09-20 03:01:36 +0200

    Omega via svn r11680: fix some missed text changes

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -44,7 +44,7 @@
 
 
 
-local revision =("$Revision: 11671 $"):sub(12, -3)
+local revision =("$Revision: 11680 $"):sub(12, -3)
 local FrameTitle = "DBM_GUI_Option_"	-- all GUI frames get automatically a name FrameTitle..ID
 
 local PanelPrototype = {}
@@ -1546,9 +1546,9 @@ local function CreateOptionsMenu()
 		CountSoundDropDown:SetPoint("TOPLEFT", RaidWarnSoundDropDown, "TOPLEFT", 0, -40)
 		
 		local countSounds2 = {
-			{	text	= "Mosh (Male)",	value 	= "Mosh"},
+			{	text	= "Moshne (Male)",	value 	= "Mosh"},
 			{	text	= "Corsica (Female)",value 	= "Corsica"},
-			{	text	= "Kolt (Male)",value 	= "Kolt"},
+			{	text	= "Koltrane (Male)",value 	= "Kolt"},
 		}
 		local CountSoundDropDown2 = raidwarnoptions:CreateDropdown(L.CountdownVoice2, countSounds,
 		DBM.Options.CountdownVoice2, function(value)
