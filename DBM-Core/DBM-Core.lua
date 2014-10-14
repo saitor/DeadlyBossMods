e79409db1882304e19081bdfe0f8fd89841c2bd1
subversion <svn@deadlybossmods.com>
2014-10-14 19:41:00 +0200

    tandanu via svn r11768: update version number for alpha builds
    test auto-package, it should work now

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,8 +51,8 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11767 $"):sub(12, -3)),
-	DisplayVersion = "6.0.0", -- the string that is shown as version
+	Revision = tonumber(("$Revision: 11768 $"):sub(12, -3)),
+	DisplayVersion = "6.0.1 alpha", -- the string that is shown as version
 	ReleaseRevision = 11766 -- the revision of the latest stable version that is available
 }
 
