2d8b5d746ddcb0c5be1f75ad7d3264727c62658f
subversion <svn@deadlybossmods.com>
2014-10-23 06:34:17 +0200

    nbluewiz via svn r11817: # Fix info frame bug. # Switch info frame update method to C_Timer.NewTicker.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,7 +51,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11816 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11817 $"):sub(12, -3)),
 	DisplayVersion = "6.0.4 alpha", -- the string that is shown as version
 	ReleaseRevision = 11799 -- the revision of the latest stable version that is available
 }
