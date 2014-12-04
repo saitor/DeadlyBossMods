5b04f63ff42e46f3017692ff806f495d9d8192c2
subversion <svn@deadlybossmods.com>
2014-12-04 23:09:11 +0100

    Omega via svn r11934: Force option default change for icon option.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -51,7 +51,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11933 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11934 $"):sub(12, -3)),
 	DisplayVersion = "6.0.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 11873 -- the revision of the latest stable version that is available
 }
--- DBM-Highmaul/Tectus.lua
+++ DBM-Highmaul/Tectus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1195, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 11930 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 11934 $"):sub(12, -3))
 mod:SetCreatureID(78948, 99999)--78948 Tectus, 80557 Mote of Tectus, 80551 Shard of Tectus
 mod:SetEncounterID(1722)--Hopefully win will work fine off this because otherwise tracking shard deaths is crappy
 mod:SetZone()
@@ -109,7 +109,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnCrystallineBarrageYou:Show()
 		end
 	elseif spellId == 162674 and self.Options.SetIconOnMote and not self:IsLFR() then--Don't mark kill/pickup marks in LFR, it'll be an aoe fest.
-		self:ScanForMobs(args.destGUID, 0, 8, 4, 0.05, 15)
+		self:ScanForMobs(args.destGUID, 0, 8, 4, 0.05, 15)--Find out why this still doesn't work.
 	end
 end
 
