cc617f71d29edfd084e6513b01e2e0b9c902219f
subversion <svn@deadlybossmods.com>
2014-12-06 10:30:48 +0100

    ebmor via svn r11954: deDE update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 11953 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 11954 $"):sub(12, -3)),
 	DisplayVersion = "6.0.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 11873 -- the revision of the latest stable version that is available
 }
