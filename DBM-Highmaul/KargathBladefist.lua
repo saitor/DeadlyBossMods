ff7682c41404869b346a03864fb86c1a27c36769
subversion <svn@deadlybossmods.com>
2014-12-16 07:54:04 +0100

    Omega via svn r12045: Fix voice bug

--- DBM-Highmaul/KargathBladefist.lua
+++ DBM-Highmaul/KargathBladefist.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1128, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12018 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12045 $"):sub(12, -3))
 mod:SetCreatureID(78714)
 mod:SetEncounterID(1721)
 mod:SetZone()
@@ -147,7 +147,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnOnTheHunt:Show(args.destName)
 		if args:IsPlayer() then
 			specWarnOnTheHunt:Show(firePillar)
-			voiceOnTheHunt("159202f") --find the pillar
+			voiceOnTheHunt:Play("159202f") --find the pillar
 		end
 	end
 end
