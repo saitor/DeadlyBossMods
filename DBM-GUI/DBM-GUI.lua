c19d4e1737c2b402a884bb52494b5ef043814627
subversion <svn@deadlybossmods.com>
2014-12-21 22:19:07 +0100

    Omega via svn r12106: Hide that option for now. until I can find a way to fix that lua error. because it's actually not that rare.

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -44,7 +44,7 @@
 
 
 
-local revision =("$Revision: 12086 $"):sub(12, -3)
+local revision =("$Revision: 12106 $"):sub(12, -3)
 local FrameTitle = "DBM_GUI_Option_"	-- all GUI frames get automatically a name FrameTitle..ID
 
 local PanelPrototype = {}
@@ -2432,7 +2432,7 @@ local function CreateOptionsMenu()
 		)
 		blockMovieDropDown:SetPoint("TOPLEFT", filterYell, "TOPLEFT", 0, -40)
 
-		local pingFilterOptions = {
+--[[		local pingFilterOptions = {
 			{	text	= L.Disable,					value 	= 0},
 			{	text	= L.HideApplicantAlertsFull,	value 	= 1},
 			{	text	= L.HideApplicantAlertsNotL,	value 	= 2},
@@ -2442,8 +2442,9 @@ local function CreateOptionsMenu()
 			DBM.Options.HideApplicantAlerts = value
 		end
 		)
-		blockApplicantsDropDown:SetPoint("TOPLEFT", blockMovieDropDown, "TOPLEFT", 0, -40)
-
+		blockApplicantsDropDown:SetPoint("TOPLEFT", blockMovieDropDown, "TOPLEFT", 0, -40)--]]
+		
+		hideBlizzArea:AutoSetDimension()
 		hideBlizzPanel:SetMyOwnHeight()
 	end
 	
