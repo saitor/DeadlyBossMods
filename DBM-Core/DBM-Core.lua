19e55215031f50dd00ff3c3d326429998b890847
subversion <svn@deadlybossmods.com>
2014-12-22 09:34:33 +0100

    nbluewiz via svn r12114: # fix bad placed Vileblood Serum warn.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12113 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12114 $"):sub(12, -3)),
 	DisplayVersion = "6.0.9 alpha", -- the string that is shown as version
 	ReleaseRevision = 11978 -- the revision of the latest stable version that is available
 }
