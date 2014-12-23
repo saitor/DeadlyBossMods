876a9e529e0a9a5ab4de02124c522e0f74f4fa6c
subversion <svn@deadlybossmods.com>
2014-12-23 09:40:10 +0100

    Omega via svn r12137: Increment alpha

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12134 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12137 $"):sub(12, -3)),
 	DisplayVersion = "6.0.9 alpha", -- the string that is shown as version
 	ReleaseRevision = 11978 -- the revision of the latest stable version that is available
 }
