ca3c3efde060dade7084064f8e937c4549fb660e
subversion <svn@deadlybossmods.com>
2014-12-27 03:02:58 +0100

    nbluewiz via svn r12172: # bad cid.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12171 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12172 $"):sub(12, -3)),
 	DisplayVersion = "6.0.10 alpha", -- the string that is shown as version
 	ReleaseRevision = 12134 -- the revision of the latest stable version that is available
 }
