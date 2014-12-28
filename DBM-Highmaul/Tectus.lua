0dc9b64414fb1b34467d8a53f73eb8c0de21908f
subversion <svn@deadlybossmods.com>
2014-12-28 03:36:15 +0100

    Omega via svn r12187: raise scan time on tectus just a little more, so it's sure to catch all 8 in most mythic strats

--- DBM-Highmaul/Tectus.lua
+++ DBM-Highmaul/Tectus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1195, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12160 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12187 $"):sub(12, -3))
 mod:SetCreatureID(78948, 80557, 80551, 99999)--78948 Tectus, 80557 Mote of Tectus, 80551 Shard of Tectus
 mod:SetEncounterID(1722)--Hopefully win will work fine off this because otherwise tracking shard deaths is crappy
 mod:SetZone()
@@ -225,7 +225,7 @@ function mod:SPELL_AURA_APPLIED(args)
 				moteH[args.destGUID] = 0
 			end
 			if self.Options.SetIconOnMote and not self:IsLFR() then--Don't mark kill/pickup marks in LFR, it'll be an aoe fest.
-				self:ScanForMobs(args.destGUID, 0, 8, 8, 0.05, 12)
+				self:ScanForMobs(args.destGUID, 0, 8, 8, 0.05, 15)
 			end
 		end
 	end
