180fdfb2a38d1284b7b0910f1a570b13b3583ca1
subversion <svn@deadlybossmods.com>
2014-12-28 09:35:14 +0100

    nbluewiz via svn r12197: # fix error.

--- DBM-Party-WoD/Skyreach/Rukhran.lua
+++ DBM-Party-WoD/Skyreach/Rukhran.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(967, "DBM-Party-WoD", 7, 476)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12195 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12197 $"):sub(12, -3))
 mod:SetCreatureID(76143)
 mod:SetEncounterID(1700)
 mod:SetZone()
@@ -25,7 +25,7 @@ local specWarnQuills			= mod:NewSpecialWarningSpell(159382, nil, nil, nil, 2)
 local specWarnQuillsEnd			= mod:NewSpecialWarningEnd(159382)
 
 local timerSolarFlareCD			= mod:NewCDTimer(18, 153810)
-local timerQuills				= mod:NewBuffActivTimer(17, 159382)
+local timerQuills				= mod:NewBuffActiveTimer(17, 159382)
 
 local voiceSolarFlare			= mod:NewVoice(153810, not mod:IsTank())
 local voiceQuills				= mod:NewVoice(159382)
