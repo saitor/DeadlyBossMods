495a05495e6e0f38864c0521f03edd3664b9a1a6
subversion <svn@deadlybossmods.com>
2014-12-29 03:30:14 +0100

    nbluewiz via svn r12207: # little 5 man update.

--- DBM-Party-WoD/IronDocks/GrimrailEnforcers.lua
+++ DBM-Party-WoD/IronDocks/GrimrailEnforcers.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1236, "DBM-Party-WoD", 4, 558)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12195 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12207 $"):sub(12, -3))
 mod:SetCreatureID(80805, 80816, 80808)
 mod:SetEncounterID(1748)
 mod:SetZone()
@@ -29,8 +29,8 @@ local specWarnSanguineSphere	= mod:NewSpecialWarningReflect(163689)
 local specWarnSanguineSphereEnd	= mod:NewSpecialWarningEnd(163689)
 local specWarnFlamingSlash		= mod:NewSpecialWarningSpell(163665, nil, nil, nil, 3)--Devastating in challenge modes. move or die.
 local specWarnLavaSwipe			= mod:NewSpecialWarningSpell(165152, nil, nil, nil, 2)
-local specWarnOgreTraps			= mod:NewSpecialWarningSpell(163390, mod:IsRanged())--Pre warning for bomb immediately after. Maybe change to a Soon warning with bomb spellid instead so that's clear?
-local specWarnBigBoom			= mod:NewSpecialWarningSpell(163379)--maybe use switch.
+local specWarnOgreTraps			= mod:NewSpecialWarningSpell("OptionVersion2", 163390, false)--Pre warning for bomb immediately after. Maybe change to a Soon warning with bomb spellid instead so that's clear?
+local specWarnBigBoom			= mod:NewSpecialWarningSpell(163379, nil, nil, nil, 2)--maybe use switch.
 
 local timerSanguineSphere		= mod:NewTargetTimer(15, 163689)
 local timerFlamingSlashCD		= mod:NewNextTimer(29, 163665)
