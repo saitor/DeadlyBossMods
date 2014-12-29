1dd154e65c31202a08fe7cc903b317e6452b6138
subversion <svn@deadlybossmods.com>
2014-12-29 23:14:08 +0100

    Omega via svn r12210: Add balls counter, for mythic. MCs should be every second. because koragh likes two balls.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12209 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12210 $"):sub(12, -3)),
 	DisplayVersion = "6.0.10 alpha", -- the string that is shown as version
 	ReleaseRevision = 12134 -- the revision of the latest stable version that is available
 }
