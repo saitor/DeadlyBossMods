fbb2d7436867096272f047d04ac1caa9666d8569
subversion <svn@deadlybossmods.com>
2015-01-08 05:28:45 +0100

    Omega via svn r12342: speed up icon

--- DBM-Highmaul/Tectus.lua
+++ DBM-Highmaul/Tectus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1195, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12340 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12342 $"):sub(12, -3))
 mod:SetCreatureID(78948, 80557, 80551, 99999)--78948 Tectus, 80557 Mote of Tectus, 80551 Shard of Tectus
 mod:SetEncounterID(1722)--Hopefully win will work fine off this because otherwise tracking shard deaths is crappy
 mod:SetZone()
@@ -229,7 +229,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			end
 		end
 		if self.Options.SetIconOnCrystal and self.vb.healthPhase ~= 3 then
-			self:SetSortedIcon(3, args.destName, 1, 2)--Wait 3 seconds or until we have 2 targets, mobs sometimes stagger casts.
+			self:SetSortedIcon(1, args.destName, 1, 2)--Wait 3 seconds or until we have 2 targets, mobs sometimes stagger casts.
 		end
 	elseif spellId == 162658 then
 		local guid = args.destGUID
