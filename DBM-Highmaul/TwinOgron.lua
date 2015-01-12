3b95213261325aa893cbd82e4e612c8705c5afd3
subversion <svn@deadlybossmods.com>
2015-01-12 09:35:27 +0100

    Omega via svn r12389: Missed some force nova stuff

--- DBM-Highmaul/TwinOgron.lua
+++ DBM-Highmaul/TwinOgron.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1148, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12375 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12389 $"):sub(12, -3))
 mod:SetCreatureID(78238, 78237)--Pol 78238, Phemos 78237
 mod:SetEncounterID(1719)
 mod:SetZone()
@@ -156,7 +156,7 @@ local function updateInfoFrame()
 		if bossPower < 33 then--Shield Charge
 			lines[UnitName("boss1")] = bossPower
 			if UnitBuff("boss1", arcaneTwisted) then--Empowered attack
-				lines["|cFFFF0000"..GetSpellInfo(158134).."|r"] = GetSpellInfo(163336)
+				lines["|cFF9932CD"..GetSpellInfo(158134).."|r"] = GetSpellInfo(163336)
 			else
 				lines[GetSpellInfo(158134)] = ""
 			end
@@ -169,7 +169,7 @@ local function updateInfoFrame()
 		lines[UnitName("boss2")] = bossPower2
 		if bossPower2 < 33 then--Shield Charge
 			if UnitBuff("boss2", arcaneTwisted) then--Empowered attack
-				lines["|cFFFF0000"..GetSpellInfo(158134).."|r"] = GetSpellInfo(163336)
+				lines["|cFF9932CD"..GetSpellInfo(158134).."|r"] = GetSpellInfo(163336)
 			else
 				lines[GetSpellInfo(158134)] = ""
 			end
