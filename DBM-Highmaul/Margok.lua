0c3e21438f26557ee56e290d604efbbec5baac4d
subversion <svn@deadlybossmods.com>
2015-01-12 05:15:30 +0100

    Omega via svn r12384: Fix a really stupid bug

--- DBM-Highmaul/Margok.lua
+++ DBM-Highmaul/Margok.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1197, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12375 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12384 $"):sub(12, -3))
 mod:SetCreatureID(77428, 78623)
 mod:SetEncounterID(1705)
 mod:SetZone()
@@ -673,6 +673,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		voiceGrowingDarkness:Play("runout")
 	end
 end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REFRESH(args)
 	local spellId = args.spellId
