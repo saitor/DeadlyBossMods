bc9ca96c9184b3375ee6bb0ccf326d28b2f58854
subversion <svn@deadlybossmods.com>
2015-01-16 00:05:01 +0100

    Omega via svn r12418: Fix error

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12417 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12418 $"):sub(12, -3)),
 	DisplayVersion = "6.0.12 alpha", -- the string that is shown as version
 	ReleaseRevision = 12328 -- the revision of the latest stable version that is available
 }
@@ -2656,7 +2656,7 @@ do
 			if not targetEventsRegistered then
 				DBM:RegisterShortTermEvents("UPDATE_MOUSEOVER_UNIT", "UNIT_TARGET_UNFILTERED", "SCENARIO_UPDATE")
 				targetEventsRegistered = true
-				self:Debug("RegisterShortTermEvents: UPDATE_MOUSEOVER_UNIT, UNIT_TARGET_UNFILTERED, SCENARIO_UPDATE", 2)
+				DBM:Debug("RegisterShortTermEvents: UPDATE_MOUSEOVER_UNIT, UNIT_TARGET_UNFILTERED, SCENARIO_UPDATE", 2)
 			end
 		else
 			if targetEventsRegistered then
