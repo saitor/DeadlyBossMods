60b75c9b814731d84efca10548cd3abcac4bde87
subversion <svn@deadlybossmods.com>
2015-01-23 01:58:24 +0100

    Omega via svn r12540: Remove an unneeded unschedule that was causing range frame not to update back to replicating nova range value after tank is safe distance away.

--- DBM-Highmaul/Margok.lua
+++ DBM-Highmaul/Margok.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1197, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12496 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12540 $"):sub(12, -3))
 mod:SetCreatureID(77428, 78623)
 mod:SetEncounterID(1705)
 mod:SetZone()
@@ -339,7 +339,7 @@ function mod:SPELL_CAST_START(args)
 			self.vb.RepNovaActive = true
 			self:Schedule(9, delayedRangeUpdate, self)
 			updateRangeFrame(self)
-			--Trhee extra checks to make sure we update 35 to 5 if tank was too close briefly if they came at same time
+			--Three extra checks to make sure we update 35 to 5 if tank was too close briefly if they came at same time
 			self:Schedule(1, updateRangeFrame, self)
 			self:Schedule(2, updateRangeFrame, self)
 			self:Schedule(5, updateRangeFrame, self)
@@ -637,7 +637,6 @@ function mod:SPELL_AURA_APPLIED(args)
 				voiceMarkOfChaos:Play("justrun")
 			end
 		end
-		self:Unschedule(updateRangeFrame)
 		updateRangeFrame(self)
 	elseif spellId == 157801 then
 		specWarnSlow:Show(args.destName)
