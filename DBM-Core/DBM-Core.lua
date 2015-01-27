330bedffe246595679c6f223e5556f105dbf341e
subversion <svn@deadlybossmods.com>
2015-01-27 09:38:07 +0100

    Omega via svn r12579: Add countdown options while at it

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12578 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12579 $"):sub(12, -3)),
 	DisplayVersion = "6.0.14 alpha", -- the string that is shown as version
 	ReleaseRevision = 12542 -- the revision of the latest stable version that is available
 }
