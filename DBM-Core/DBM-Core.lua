b04f759ebd414f69c487cda51c79e50d0a54f5f4
subversion <svn@deadlybossmods.com>
2015-01-27 07:16:10 +0100

    Omega via svn r12576: add GetRemaining timer object to simplify a lot of mods. Reduce yell spam in highmaul trash (multiple debuffs same player)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12575 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12576 $"):sub(12, -3)),
 	DisplayVersion = "6.0.14 alpha", -- the string that is shown as version
 	ReleaseRevision = 12542 -- the revision of the latest stable version that is available
 }
@@ -8250,6 +8250,12 @@ do
 		local id = self.id..pformat((("\t%s"):rep(select("#", ...))), ...)
 		local bar = DBM.Bars:GetBar(id)
 		return bar and (bar.totalTime - bar.timer) or 0, (bar and bar.totalTime) or 0
+	end
+	
+	function timerPrototype:GetRemaining(...)
+		local id = self.id..pformat((("\t%s"):rep(select("#", ...))), ...)
+		local bar = DBM.Bars:GetBar(id)
+		return bar and bar.timer or 0
 	end
 
 	function timerPrototype:IsStarted(...)
