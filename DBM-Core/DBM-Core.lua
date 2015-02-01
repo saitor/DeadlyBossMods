9c768395cce7a7cad4deca43c28ab595976194c4
subversion <svn@deadlybossmods.com>
2015-02-01 11:27:34 +0100

    Omega via svn r12649: User submitted french update for highmaul

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12648 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12649 $"):sub(12, -3)),
 	DisplayVersion = "6.0.14 alpha", -- the string that is shown as version
 	ReleaseRevision = 12542 -- the revision of the latest stable version that is available
 }
