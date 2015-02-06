06121579457a9bcd04e81737626c8a03e69ddc85
subversion <svn@deadlybossmods.com>
2015-02-06 11:02:09 +0100

    Omega via svn r12739: Add engineer announce enable the timer/countdown fully now that i'm confident in the loop.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12738 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12739 $"):sub(12, -3)),
 	DisplayVersion = "6.0.15 alpha", -- the string that is shown as version
 	ReleaseRevision = 12656 -- the revision of the latest stable version that is available
 }
