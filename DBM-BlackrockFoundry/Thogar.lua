c2aed80672b55bf8c278d4c990a43c20ca6fe360
subversion <svn@deadlybossmods.com>
2015-02-07 00:52:00 +0100

    nbluewiz via svn r12745: # fix bad first scheduler.

--- DBM-BlackrockFoundry/Thogar.lua
+++ DBM-BlackrockFoundry/Thogar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1147, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12744 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12745 $"):sub(12, -3))
 mod:SetCreatureID(76906)--81315 Crack-Shot, 81197 Raider, 77487 Grom'kar Firemender, 80791 Grom'kar Man-at-Arms, 81318 Iron Gunnery Sergeant, 77560 Obliterator Cannon, 81612 Deforester
 mod:SetEncounterID(1692)
 mod:SetZone()
@@ -292,11 +292,11 @@ function mod:OnCombatStart(delay)
 		self.Options.ShowedThogarMessage = true
 	end
 	if self:IsMythic() then
+		self:Schedule(9.5, fakeTrainYell, self)
 		timerTrainCD:Start(12-delay, 1)
-		self:Schedule(14.5, fakeTrainYell, self)
 	else
+		self:Schedule(14.5, fakeTrainYell, self)
 		timerTrainCD:Start(17-delay, 1)
-		self:Schedule(19.5, fakeTrainYell, self)
 	end
 	showInfoFrame()
 end
