26d24b6a82e588374abe90df38033555c870dcdf
subversion <svn@deadlybossmods.com>
2015-02-07 01:26:57 +0100

    Omega via svn r12748: SetUsedIcons

--- DBM-BlackrockFoundry/Blackhand.lua
+++ DBM-BlackrockFoundry/Blackhand.lua
@@ -1,11 +1,11 @@
 local mod	= DBM:NewMod(959, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12742 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12748 $"):sub(12, -3))
 mod:SetCreatureID(77325)--68168
 mod:SetEncounterID(1704)
 mod:SetZone()
-mod:SetUsedIcons(1, 2)
+mod:SetUsedIcons(2, 1)
 
 mod:RegisterCombat("combat")
 
