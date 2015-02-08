739575a4c4911114cbde68204ce8bd15d6a258b9
subversion <svn@deadlybossmods.com>
2015-02-08 11:46:52 +0100

    Omega via svn r12801: Tweak

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12800 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12801 $"):sub(12, -3)),
 	DisplayVersion = "6.0.16 alpha", -- the string that is shown as version
 	ReleaseRevision = 12764 -- the revision of the latest stable version that is available
 }
