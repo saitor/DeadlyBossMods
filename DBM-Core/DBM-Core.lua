31717f85af8865e5ae97add975b84056cbab375d
subversion <svn@deadlybossmods.com>
2015-02-08 04:32:38 +0100

    nbluewiz via svn r12782: # adjust some kromog timer by video.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12781 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12782 $"):sub(12, -3)),
 	DisplayVersion = "6.0.16 alpha", -- the string that is shown as version
 	ReleaseRevision = 12764 -- the revision of the latest stable version that is available
 }
