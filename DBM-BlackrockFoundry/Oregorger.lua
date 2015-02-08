0d9fe2c0fa3bce5f61e4bd453b6851b107a0ef5a
subversion <svn@deadlybossmods.com>
2015-02-08 13:22:38 +0100

    nbluewiz via svn r12803: # fix acid torrent cd.

--- DBM-BlackrockFoundry/Oregorger.lua
+++ DBM-BlackrockFoundry/Oregorger.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1202, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12732 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12803 $"):sub(12, -3))
 mod:SetCreatureID(77182)
 mod:SetEncounterID(1696)
 mod:SetZone()
@@ -34,7 +34,7 @@ local specWarnHungerDrive			= mod:NewSpecialWarningSpell(165127, nil, nil, nil, 
 local specWarnHungerDriveEnded		= mod:NewSpecialWarningFades(165127)
 
 local timerBlackrockSpinesCD		= mod:NewCDTimer(20, 156834)--20-23 (cd for barrages themselves too inconsistent and useless. but CD for when he recharges his spines, quite consistent)
-local timerAcidTorrentCD			= mod:NewCDTimer("OptionVersion2", 23, 156240, nil, "Tank|Healer")--Every 23 seconds
+local timerAcidTorrentCD			= mod:NewCDTimer("OptionVersion2", 13, 156240, nil, "Tank|Healer")
 local timerExplosiveShardCD			= mod:NewCDTimer("OptionVersion3", 12, 156390, nil, "MeleeDps")--Every 12-20 seconds
 local timerExplosiveShard			= mod:NewCastTimer(3.5, 156390, nil, "MeleeDps")
 local timerRetchedBlackrockCD		= mod:NewCDTimer("OptionVersion2", 17, 156179, nil, "Ranged")--Every 17-23 seconds
