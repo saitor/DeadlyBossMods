c6fd7b562d40625bdcd5f10bf9ac5e6049d3a2fc
subversion <svn@deadlybossmods.com>
2015-02-08 23:27:33 +0100

    nbluewiz via svn r12809: # fix profile creation error. # fix lua error in kromog.

--- DBM-BlackrockFoundry/Kromog.lua
+++ DBM-BlackrockFoundry/Kromog.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1162, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12783 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12809 $"):sub(12, -3))
 mod:SetCreatureID(77692)
 mod:SetEncounterID(1713)
 mod:SetZone()
@@ -82,7 +82,7 @@ function mod:SPELL_CAST_START(args)
 		else
 			timerGraspingEarthCD:Start()
 			timerStoneBreathCD:Start(31, 1)
-			timertimerRipplingSmashCD:Start(41)
+			timerRipplingSmashCD:Start(41)
 		end
 	elseif spellId == 157054 then
 		specWarnThunderingBlows:Show()
