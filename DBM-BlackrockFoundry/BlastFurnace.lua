83b6b54fcf8faade0ed96ea08b7a62cd0686d6b5
subversion <svn@deadlybossmods.com>
2015-02-08 09:45:10 +0100

    nbluewiz via svn r12796: # fix error.

--- DBM-BlackrockFoundry/BlastFurnace.lua
+++ DBM-BlackrockFoundry/BlastFurnace.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1154, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12795 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12796 $"):sub(12, -3))
 mod:SetCreatureID(76809, 76806)--76809 foreman feldspar, 76806 heart of the mountain, 76809 Security Guard, 76810 Furnace Engineer, 76811 Bellows Operator, 76815 Primal Elementalist, 78463 Slag Elemental, 76821 Firecaller
 mod:SetEncounterID(1690)
 mod:SetZone()
@@ -274,6 +274,7 @@ do
 				powerRate = 5
 			end
 			if self.vb.lastTotal > totalTime then--CD changed
+				self.vb.lastTotal = totalTime
 				warnBlastFrequency:Show(totalTime)
 				local bossPower = UnitPower("boss1") --Get Boss Power
 				local elapsed = bossPower / powerRate
