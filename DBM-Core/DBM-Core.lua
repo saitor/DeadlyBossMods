864823f63bca1449001dc0a9da7c41be5d8186c8
subversion <svn@deadlybossmods.com>
2015-02-10 18:53:55 +0100

    Omega via svn r12839: Hud options for iron maidens

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12838 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12839 $"):sub(12, -3)),
 	DisplayVersion = "6.0.16 alpha", -- the string that is shown as version
 	ReleaseRevision = 12764 -- the revision of the latest stable version that is available
 }
