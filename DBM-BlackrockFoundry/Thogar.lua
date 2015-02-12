6ef0612236b9cd9f9b8bab454cdc3c6d3a3900df
subversion <svn@deadlybossmods.com>
2015-02-12 11:25:53 +0100

    nbluewiz via svn r12888: # increase bomb scanning time for thogar.

--- DBM-BlackrockFoundry/Thogar.lua
+++ DBM-BlackrockFoundry/Thogar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1147, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12887 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12888 $"):sub(12, -3))
 mod:SetCreatureID(76906)--81315 Crack-Shot, 81197 Raider, 77487 Grom'kar Firemender, 80791 Grom'kar Man-at-Arms, 81318 Iron Gunnery Sergeant, 77560 Obliterator Cannon, 81612 Deforester
 mod:SetEncounterID(1692)
 mod:SetZone()
@@ -367,7 +367,7 @@ function mod:SPELL_CAST_START(args)
 		end
 		timerIronbellowCD:Start(12, args.sourceGUID)
 	elseif spellId == 159481 then
-		self:BossTargetScanner(args.sourceGUID, "BombTarget", 0.05, 10)
+		self:BossTargetScanner(args.sourceGUID, "BombTarget", 0.05, 25)
 	end
 end
 
