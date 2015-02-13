4711f51ba9dd17e1b3ae82788529771f68a5ddd9
subversion <svn@deadlybossmods.com>
2015-02-13 01:33:57 +0100

    nbluewiz via svn r12894: # bw version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12893 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12894 $"):sub(12, -3)),
 	DisplayVersion = "6.0.16 alpha", -- the string that is shown as version
 	ReleaseRevision = 12764 -- the revision of the latest stable version that is available
 }
@@ -330,7 +330,7 @@ local iconSetRevision = {}
 local iconSetPerson = {}
 local addsGUIDs = {}
 
-local fakeBWRevision = 12721
+local fakeBWRevision = 12733
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
 local guiRequested = false
