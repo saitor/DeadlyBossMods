3f6fdfc103d10f16bdba513d8d08571e5b4fa67d
subversion <svn@deadlybossmods.com>
2015-02-17 09:40:26 +0100

    Omega via svn r12974: add more debug for blackhand update experiment

--- DBM-BlackrockFoundry/Blackhand.lua
+++ DBM-BlackrockFoundry/Blackhand.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(959, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12968 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12974 $"):sub(12, -3))
 mod:SetCreatureID(77325)--68168
 mod:SetEncounterID(1704)
 mod:SetZone()
@@ -181,9 +181,10 @@ function mod:SPELL_AURA_APPLIED(args)
 				DBM:Debug("Running experimental timerShatteringSmashCD adjust because debugmode is enabled", 2)
 				local elapsed, total = timerShatteringSmashCD:GetTime()
 				local remaining = total - elapsed
-				if (remaining > timer) and (remaining < timer+5) then--Marked for death will come off cd before timerShatteringSmashCD comes off cd and delay the cast
-					DBM:Debug("Delay detected, updating smash timer now")
+				DBM:Debug("Smash Elapsed: "..elapsed.." Smash Total: "..total.." Smash Remaining: "..remaining.." MFD Timer: "..timer, 2)
+				if (remaining > timer) and (remaining < timer+5.5) then--Marked for death will come off cd before timerShatteringSmashCD comes off cd and delay the cast
 					local extend = (timer+5)-remaining
+					DBM:Debug("Delay detected, updating smash timer now. Extend: "..extend)
 					timerShatteringSmashCD:Update(elapsed, total+extend, self.vb.smashCount+1)
 					countdownShatteringSmash:Cancel()
 					countdownShatteringSmash:Start(remaining+extend)
