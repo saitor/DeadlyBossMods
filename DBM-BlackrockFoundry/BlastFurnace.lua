f567408f869163a3d15f63d21cd0250a058504e5
subversion <svn@deadlybossmods.com>
2015-02-17 09:02:00 +0100

    Omega via svn r12972: Fix mythic engineer timers take 2

--- DBM-BlackrockFoundry/BlastFurnace.lua
+++ DBM-BlackrockFoundry/BlastFurnace.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1154, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12971 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12972 $"):sub(12, -3))
 mod:SetCreatureID(76809, 76806)--76809 foreman feldspar, 76806 heart of the mountain, 76809 Security Guard, 76810 Furnace Engineer, 76811 Bellows Operator, 76815 Primal Elementalist, 78463 Slag Elemental, 76821 Firecaller
 mod:SetEncounterID(1690)
 mod:SetZone()
@@ -155,9 +155,9 @@ function mod:OnCombatStart(delay)
 	self.vb.lastTotal = 30
 	self.vb.phase = 1
 	if self:IsMythic() then
-		self:Schedule(45, Engineers, self)
-		timerEngineer:Start(45)
-		countdownEngineer:Start(45)
+		self:Schedule(40, Engineers, self)
+		timerEngineer:Start(40)
+		countdownEngineer:Start(40)
 	else
 		self:Schedule(55, Engineers, self)
 		timerEngineer:Start(55)
