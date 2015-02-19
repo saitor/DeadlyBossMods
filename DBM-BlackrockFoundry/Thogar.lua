dd9740864654dee822cce3bb5efcc270a112b5ed
subversion <svn@deadlybossmods.com>
2015-02-19 01:26:14 +0100

    Omega via svn r12984: No timer, but add info for all 4 trains at once in set 35

--- DBM-BlackrockFoundry/Thogar.lua
+++ DBM-BlackrockFoundry/Thogar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1147, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12975 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12984 $"):sub(12, -3))
 mod:SetCreatureID(76906)--81315 Crack-Shot, 81197 Raider, 77487 Grom'kar Firemender, 80791 Grom'kar Man-at-Arms, 81318 Iron Gunnery Sergeant, 77560 Obliterator Cannon, 81612 Deforester
 mod:SetEncounterID(1692)
 mod:SetZone()
@@ -150,6 +150,7 @@ local otherTrains = {
 	[32] = { [3] = Train },--+4 after 31 (8:01)
 	[33] = { [2] = Train },--+4 after 32 (8:05)
 	[34] = { [1] = Train },--+4 after 33 (8:09)
+	[35] = { [1] = Train, [2] = Train, [3] = Train, [4] = Train },--+? after 34 (8:??)
 }
 
 local function fakeTrainYell(self)
--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 12983 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 12984 $"):sub(12, -3)),
 	DisplayVersion = "6.0.17 alpha", -- the string that is shown as version
 	ReleaseRevision = 12955 -- the revision of the latest stable version that is available
 }
