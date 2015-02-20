cb45077ef37e9792041cfa3b3b137058f4d179b8
subversion <svn@deadlybossmods.com>
2015-02-20 03:28:04 +0100

    nbluewiz via svn r12999: # oops, missed it (blast furnace)

--- DBM-BlackrockFoundry/BlastFurnace.lua
+++ DBM-BlackrockFoundry/BlastFurnace.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1154, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 12998 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 12999 $"):sub(12, -3))
 mod:SetCreatureID(76809, 76806)--76809 foreman feldspar, 76806 heart of the mountain, 76809 Security Guard, 76810 Furnace Engineer, 76811 Bellows Operator, 76815 Primal Elementalist, 78463 Slag Elemental, 76821 Firecaller
 mod:SetEncounterID(1690)
 mod:SetZone()
@@ -375,6 +375,7 @@ function mod:UNIT_DIED(args)
 			self:Unschedule(SecurityGuard)
 			self:Unschedule(FireCaller)
 			self.vb.phase = 3
+			self:UnregisterShortTermEvents()
 			warnPhase3:Show()
 			specWarnHeartoftheMountain:Show()
 			voicePhaseChange:Play("pthree")
