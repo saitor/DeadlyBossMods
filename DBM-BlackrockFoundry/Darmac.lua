104c7e84891bb2c7a607fd12ef7e66a11f9d2e13
subversion <svn@deadlybossmods.com>
2015-02-20 03:39:51 +0100

    Omega via svn r13002: change speed back

--- DBM-BlackrockFoundry/Darmac.lua
+++ DBM-BlackrockFoundry/Darmac.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1122, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13001 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13002 $"):sub(12, -3))
 mod:SetCreatureID(76865)--No need to add beasts to this. It's always main boss that's engaged first and dies last.
 mod:SetEncounterID(1694)
 mod:SetZone()
@@ -456,7 +456,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		specWarnInfernoBreath:Show()
 		timerInfernoBreathCD:Start()
 		voiceInfernoBreath:Play("breathsoon")
-		self:BossTargetScanner(76874, "BreathTarget", 0.05, 25, nil, nil, false)
+		self:BossTargetScanner(76874, "BreathTarget", 0.1, 25, nil, nil, false)
 		DBM:Debug("TESTING THINGS!: "..UnitName(uId.."target"))
 	end
 end
