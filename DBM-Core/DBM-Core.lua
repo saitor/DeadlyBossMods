c47c6087957f1eb710ff9fef0d312da07dcfaf41
subversion <svn@deadlybossmods.com>
2015-02-22 23:24:33 +0100

    Omega via svn r13059: Add massive smash radar for finding and position around correct tank.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13058 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13059 $"):sub(12, -3)),
 	DisplayVersion = "6.0.17 alpha", -- the string that is shown as version
 	ReleaseRevision = 12955 -- the revision of the latest stable version that is available
 }
