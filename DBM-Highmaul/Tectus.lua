6b2248d40145768cf1938022cca8158d0fcdceba
subversion <svn@deadlybossmods.com>
2015-03-01 11:02:33 +0100

    Omega via svn r13168: fix bad global access. no idea if correct fix, I didn?\226?\128?\153t review this mess.

--- DBM-Highmaul/Tectus.lua
+++ DBM-Highmaul/Tectus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1195, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13161 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13168 $"):sub(12, -3))
 mod:SetCreatureID(78948, 80557, 80551, 99999)--78948 Tectus, 80557 Mote of Tectus, 80551 Shard of Tectus
 mod:SetEncounterID(1722)--Hopefully win will work fine off this because otherwise tracking shard deaths is crappy
 mod:SetZone()
@@ -81,6 +81,7 @@ local tectusGUID
 local shardGUID = {}
 local ltectusH, lshardC, lshardT, lmoteC, lmoteT = 1, 1, 1, 1, 1 -- not need to sync.
 
+local newhealth--Review
 function mod:CustomHealthUpdate()
 	local tectusH, shardC, shardT, moteC, moteT = 0, 0, 0, 0, 0
 	local moteGUID = {}
@@ -105,13 +106,13 @@ function mod:CustomHealthUpdate()
 				moteGUID[guid] = true
 				lmoteC = moteC
 				lmoteT = moteH
-				moteH[guid] = newhealth
+				moteH[guid] = newhealth--Review
 			end
 		end
 	end
 	for guid, health in pairs(moteH) do
 		if not moteGUID[guid] then
-			local newhealth = self:GetBossHPByGUID(guid) or health
+			newhealth = self:GetBossHPByGUID(guid) or health--Review
 			if newhealth >= 1 then
 				self.vb.healthPhase = 3
 				moteC = moteC + 1
