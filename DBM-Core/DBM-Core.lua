8d2955b794be9ecd6c00d2f4787c63dc84f1df96
subversion <svn@deadlybossmods.com>
2015-03-05 23:23:55 +0100

    Omega via svn r13214: ptBR update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13213 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13214 $"):sub(12, -3)),
 	DisplayVersion = "6.1.2 alpha", -- the string that is shown as version
 	ReleaseRevision = 13132 -- the revision of the latest stable version that is available
 }
