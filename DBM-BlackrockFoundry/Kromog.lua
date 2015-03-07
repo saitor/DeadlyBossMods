64110f7a7b2a0241a2527cd26700b60fccc4870d
subversion <svn@deadlybossmods.com>
2015-03-07 01:35:51 +0100

    Omega via svn r13227: Fix bug where stone breath timer was getting canceled in wrong place. Fixed another bug where count would be wrong now that stone breath doesn?\226?\128?\153t reset count

--- DBM-BlackrockFoundry/Kromog.lua
+++ DBM-BlackrockFoundry/Kromog.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1162, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13212 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13227 $"):sub(12, -3))
 mod:SetCreatureID(77692)
 mod:SetEncounterID(1713)
 mod:SetZone()
@@ -74,17 +74,17 @@ function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 157060 then
 		specWarnGraspingEarth:Show(RUNES)
+		timerStoneBreathCD:Cancel()
 		if self:IsLFR() then
 			timerThunderingBlowsCD:Start(20.5)
 			countdownThunderingBlows:Start(20.5)
-			timerStoneBreathCD:Start(28, 1)
+			timerStoneBreathCD:Start(28, self.vb.stoneBreath+1)
 		else
 			timerThunderingBlowsCD:Start()
 			countdownThunderingBlows:Start()
-			timerStoneBreathCD:Start(31, 1)--Verified it happens on mythic, if rune of trembling earth doesn't come first
+			timerStoneBreathCD:Start(31, self.vb.stoneBreath+1)--Verified it happens on mythic, if rune of trembling earth doesn't come first
 		end
 		timerSlamCD:Cancel()
-		timerStoneBreathCD:Cancel()
 		timerRipplingSmashCD:Cancel()
 		timerWarpedArmorCD:Cancel()
 		voiceGraspingEarth:Play("157060")
