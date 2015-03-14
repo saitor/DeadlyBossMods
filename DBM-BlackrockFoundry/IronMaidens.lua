5944b2e9c93536c3849d0d53c5ccea08eada4674
subversion <svn@deadlybossmods.com>
2015-03-14 19:06:21 +0100

    nbluewiz via svn r13307: # phase 2 warn fix for iron maidens.

--- DBM-BlackrockFoundry/IronMaidens.lua
+++ DBM-BlackrockFoundry/IronMaidens.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1203, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13306 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13307 $"):sub(12, -3))
 mod:SetCreatureID(77557, 77231, 77477)
 mod:SetEncounterID(1695)
 mod:SetZone()
@@ -525,7 +525,7 @@ end
 
 function mod:UNIT_HEALTH_FREQUENT(uId)
 	local hp = UnitHealth(uId) / UnitHealthMax(uId)
-	if hp < 0.20 and not self.vb.phase == 2 then
+	if hp < 0.20 and self.vb.phase ~= 2 then
 		timerShipCD:Cancel()
 		self.vb.phase = 2
 		warnPhase2:Show()
