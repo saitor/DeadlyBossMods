a17df25d12686fd5dd80b262ea62fe1df87d7c17
subversion <svn@deadlybossmods.com>
2015-03-16 10:24:19 +0100

    nbluewiz via svn r13323: # fix Cauterizing Bolt Dispel.

--- DBM-BlackrockFoundry/Thogar.lua
+++ DBM-BlackrockFoundry/Thogar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1147, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13322 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13323 $"):sub(12, -3))
 mod:SetCreatureID(76906)--81315 Crack-Shot, 81197 Raider, 77487 Grom'kar Firemender, 80791 Grom'kar Man-at-Arms, 81318 Iron Gunnery Sergeant, 77560 Obliterator Cannon, 81612 Deforester
 mod:SetEncounterID(1692)
 mod:SetZone()
@@ -561,7 +561,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		if amount >= 2 then
 			specWarnBurning:Show(amount)
 		end
-	elseif spellId == 160140 then
+	elseif spellId == 160140 and args:IsDestTypeHostile() then
 		specWarnCauterizingBoltDispel:Show(args.destName)
 	end
 end
