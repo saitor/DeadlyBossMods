4c90e1a2f6f1cbf4fcc55deb1cfd20d52c0774ca
subversion <svn@deadlybossmods.com>
2015-03-19 07:00:26 +0100

    Omega via svn r13355: Disable changemt voice on kromog. swaps can occur anywhere between 2-4 stacks depending purely on rng, therefor voice really just cannot be used with any level of accuracy without super complicated code that counts number of tanks, checks who has debuffs and who doesn?\226?\128?\153t. and makes sure there is a viable stackless tank that can taunt. It?\226?\128?\153s just a mess, so removed.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13354 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13355 $"):sub(12, -3)),
 	DisplayVersion = "6.1.3 alpha", -- the string that is shown as version
 	ReleaseRevision = 13295 -- the revision of the latest stable version that is available
 }
