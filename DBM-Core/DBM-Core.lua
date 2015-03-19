934309b2dd78b90edd6e63052c7407b33e912f83
subversion <svn@deadlybossmods.com>
2015-03-19 15:55:37 +0100

    Omega via svn r13357: bw revision

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13356 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13357 $"):sub(12, -3)),
 	DisplayVersion = "6.1.3 alpha", -- the string that is shown as version
 	ReleaseRevision = 13295 -- the revision of the latest stable version that is available
 }
@@ -346,7 +346,7 @@ local iconSetRevision = {}
 local iconSetPerson = {}
 local addsGUIDs = {}
 
-local fakeBWRevision = 12950
+local fakeBWRevision = 12982
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
 local guiRequested = false
