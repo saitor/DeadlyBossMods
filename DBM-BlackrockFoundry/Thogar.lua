9143e9fe6454538012e195a07e8f7beed36c4bb1
subversion <svn@deadlybossmods.com>
2015-03-21 13:53:08 +0100

    nbluewiz via svn r13385: # fix mythic 35 train.

--- DBM-BlackrockFoundry/Thogar.lua
+++ DBM-BlackrockFoundry/Thogar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1147, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13377 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13385 $"):sub(12, -3))
 mod:SetCreatureID(76906)--81315 Crack-Shot, 81197 Raider, 77487 Grom'kar Firemender, 80791 Grom'kar Man-at-Arms, 81318 Iron Gunnery Sergeant, 77560 Obliterator Cannon, 81612 Deforester
 mod:SetEncounterID(1692)
 mod:SetZone()
@@ -637,9 +637,9 @@ function mod:CHAT_MSG_MONSTER_YELL(msg, npc, _, _, target)
 			if mythicVoice[count] and not adjusted then
 				voiceTrain:Play("Thogar\\"..mythicVoice[count])
 			end
-			if count == 1 or count == 2 or count == 11 or count == 12 or count == 13 or count == 25 or count == 26 or count == 31 or count == 34 then
+			if count == 1 or count == 2 or count == 11 or count == 12 or count == 13 or count == 25 or count == 26 or count == 31 then
 				expectedTime = 5
-				if count == 1 or count == 11 or count == 26 then
+				if count == 1 or count == 11 or count == 26 or count == 34 then
 					showHud(self, count)
 				elseif count == 2 then
 					self:Schedule(14-fakeAdjust, showHud, self, count)
