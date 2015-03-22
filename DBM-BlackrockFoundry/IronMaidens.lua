d3f191f9e04a26f76fa0f5ca9c180ed8a4902bd4
subversion <svn@deadlybossmods.com>
2015-03-22 21:07:24 +0100

    Omega via svn r13397: CN update Update iron maidens filter to apply to ranged only. Melee generally have to run out every time and don?\226?\128?\153t get to stand in one place without moving.

--- DBM-BlackrockFoundry/IronMaidens.lua
+++ DBM-BlackrockFoundry/IronMaidens.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1203, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13373 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13397 $"):sub(12, -3))
 mod:SetCreatureID(77557, 77231, 77477)
 mod:SetEncounterID(1695)
 mod:SetZone()
@@ -128,8 +128,8 @@ mod:AddSetIconOption("SetIconOnBloodRitual", 158078, true)
 mod:AddSetIconOption("SetIconOnHeartSeeker", 158010, true)
 mod:AddHudMapOption("HudMapOnRapidFire", 156631)--Green markers
 mod:AddHudMapOption("HudMapOnBloodRitual", 158078)--Red markers
-mod:AddBoolOption("filterBladeDash", true)
-mod:AddBoolOption("filterBloodRitual", true)
+mod:AddBoolOption("filterBladeDash2", "Ranged")
+mod:AddBoolOption("filterBloodRitual2", "Ranged")
 
 mod.vb.phase = 1
 mod.vb.ship = 0
@@ -237,7 +237,7 @@ end
 function mod:BladeDashTarget(targetname, uId)
 	if self:IsMythic() then
 		if targetname == UnitName("player") then
-			if UnitDebuff("player", GetSpellInfo(170395)) and self.Options.filterBladeDash then return end
+			if UnitDebuff("player", GetSpellInfo(170395)) and self.Options.filterBladeDash2 then return end
 			specWarnBladeDash:Show()
 		elseif self:CheckNearby(8, targetname) then
 			specWarnBladeDashOther:Show(targetname)
@@ -406,7 +406,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 		if args:IsPlayer() then
 			yellBloodRitual:Yell()
-			if UnitDebuff("player", GetSpellInfo(170405)) and self.Options.filterBloodRitual then return end
+			if UnitDebuff("player", GetSpellInfo(170405)) and self.Options.filterBloodRitual2 then return end
 			specWarnBloodRitual:Show()
 			--voiceBloodRitual:Play("???")--Player needs a different warning than "far away from lines". player IS the line so they can't be far away from lines
 		else
