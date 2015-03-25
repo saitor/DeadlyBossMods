c2351fd675343a8619b09f1c54c729f34e91c9ce
subversion <svn@deadlybossmods.com>
2015-03-25 22:04:58 +0100

    Omega via svn r13422: *Remove useless not working warning from Blast Furnace. Although that cast was visible on ptr, it didn?\226?\128?\153t make it to live. *tweak breath warnings on darmac to be dodge warnings *Count jumps on Hansgar #Iron Maidens Update *Fixed bug where icons wouldn?\226?\128?\153t work if icon setter went to a boat. *Made rapid fire, penetrating shot, convulsive shadows, dark hunt, bloadsoaked heartseeker all count warnings/timers *Removed savedabilities table, forgot to kill that in last update. *Added new optional special warning for penetrating shot on other, something paladins want for hand of protection strat.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13421 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13422 $"):sub(12, -3)),
 	DisplayVersion = "6.1.4 alpha", -- the string that is shown as version
 	ReleaseRevision = 13418 -- the revision of the latest stable version that is available
 }
