2c425ac71f673d573f8df1434239723ef90bc182
subversion <svn@deadlybossmods.com>
2015-03-26 04:01:06 +0100

    Omega via svn r13425: speed up firestorm warning on Kragraz

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13424 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13425 $"):sub(12, -3)),
 	DisplayVersion = "6.1.4 alpha", -- the string that is shown as version
 	ReleaseRevision = 13418 -- the revision of the latest stable version that is available
 }
