ca5c31c7e2e1f19ed604e19e2200a6dc5d4aab41
subversion <svn@deadlybossmods.com>
2015-03-26 08:16:14 +0100

    Omega via svn r13434: adjust some thogar coords, fix split triangle movements and combined them into one. Split ranged and melee to greatly improve pulse grenade drops. It?\226?\128?\153s common strat to split into two clumps, so make the hud be smart about this. It?\226?\128?\153s most common for ranged east and melee west. Unless specific move requires moving as unison to center for something such as a deforester.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13433 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13434 $"):sub(12, -3)),
 	DisplayVersion = "6.1.4 alpha", -- the string that is shown as version
 	ReleaseRevision = 13418 -- the revision of the latest stable version that is available
 }
