753a11c48bf3350d9c25568aa50293a700173c32
subversion <svn@deadlybossmods.com>
2015-03-29 06:38:24 +0200

    Omega via svn r13457: Faster phase change warning on oregorger. Often people waiting for dbm alert and are too slow to get back into position. but boss stops rolling at 100 energy so this faster warn should be reliable to return correct position.

--- DBM-BlackrockFoundry/Oregorger.lua
+++ DBM-BlackrockFoundry/Oregorger.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1202, "DBM-BlackrockFoundry", nil, 457)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13452 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13457 $"):sub(12, -3))
 mod:SetCreatureID(77182)
 mod:SetEncounterID(1696)
 mod:SetZone()
@@ -105,8 +105,6 @@ function mod:SPELL_AURA_REMOVED(args)
 	if spellId == 155819 then
 		self.vb.torrentCount = 0
 		self:UnregisterShortTermEvents()
-		specWarnHungerDriveEnded:Show()
-		voicePhaseChange:Play("phasechange")
 		timerRetchedBlackrockCD:Start(5)
 		timerExplosiveShardCD:Start(6)--7-9
 		timerAcidTorrentCD:Start(11, 1)--11-12
@@ -174,5 +172,9 @@ function mod:UNIT_POWER_FREQUENT()
 	if (self:AntiSpam(10) or ore == 100) and lastOre ~= ore then
 		lastOre = ore
 		warnCollectOre:Show(ore)
+		if ore == 100 then
+			specWarnHungerDriveEnded:Show()
+			voicePhaseChange:Play("phasechange")
+		end
 	end
 end
--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -53,7 +53,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13456 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13457 $"):sub(12, -3)),
 	DisplayVersion = "6.1.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 13435 -- the revision of the latest stable version that is available
 }
