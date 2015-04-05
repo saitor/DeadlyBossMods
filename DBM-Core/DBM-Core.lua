b6d42afb9a4b46a72ff262acb9b28ec074c97366
subversion <svn@deadlybossmods.com>
2015-04-06 00:05:51 +0200

    Omega via svn r13542: Fix lua error, otherwise, seems to work

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13541 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13542 $"):sub(12, -3)),
 	DisplayVersion = "6.1.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 13486 -- the revision of the latest stable version that is available
 }
@@ -6663,7 +6663,7 @@ do
 		local modId = self.id
 		local scanDuration = scanTime or 1.5
 		targetMonitor = modId.."\t"..unitId.."\t"..returnFunc
-		self:Schedule(scanDuration, "BossUnitTargetScannerAbort")--In case of BossUnitTargetScanner firing too late, and boss already having changed target before monitor started, it needs to abort after x seconds
+		self:ScheduleMethod(scanDuration, "BossUnitTargetScannerAbort")--In case of BossUnitTargetScanner firing too late, and boss already having changed target before monitor started, it needs to abort after x seconds
 	end
 
 	function bossModPrototype:BossTargetScanner(cidOrGuid, returnFunc, scanInterval, scanTimes, scanOnlyBoss, isEnemyScan, isFinalScan, targetFilter, tankFilter)
