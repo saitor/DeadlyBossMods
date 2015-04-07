6eedc014f54db4a19578d436e262e9ef50bcb251
subversion <svn@deadlybossmods.com>
2015-04-07 08:32:49 +0200

    Omega via svn r13550: #Blast Furnace Update *Fix infoframe bomb counter if regulators aren?\226?\128?\153t boss 3 and boss 4 *Fixed spam if user gets two volatile fires at same time *Changed yell default to apply because the countdown default is a spammy mess and most do not like it (10 targets get debuff at same time). If people prefer countdown they can change it. *Fixed bug where tank warning was saying to dodge an attack that can?\226?\128?\153t be dodged. Changed sound to 1 since attack isn?\226?\128?\153t a near one shotting dodgable attack like it was in beta *Added timer (off by default) for volatile Fire *Changed Rupture to a higher priority warning. good placement of it is critical, especially phase 2 (if dropped on an elementalists it?\226?\128?\153s pretty much a wipe since melee can?\226?\128?\153t go near it). *Tweaks to blast timer, almost always saw CD start before expires messages, for like 8 out of 10 blasts. *0.5 second tweak on first adds (and all subsequent ones as well)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13549 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13550 $"):sub(12, -3)),
 	DisplayVersion = "6.1.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 13486 -- the revision of the latest stable version that is available
 }
