86024a8ff9aa1081efece775d6317be3c2ab844b
subversion <svn@deadlybossmods.com>
2015-04-22 14:25:49 +0200

    increment version number to 6.1.7

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,9 +52,9 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13633 $"):sub(12, -3)),
-	DisplayVersion = "6.1.7 alpha", -- the string that is shown as version
-	ReleaseRevision = 13591 -- the revision of the latest stable version that is available
+	Revision = tonumber(("$Revision: 13634 $"):sub(12, -3)),
+	DisplayVersion = "6.1.7", -- the string that is shown as version
+	ReleaseRevision = 13634 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
