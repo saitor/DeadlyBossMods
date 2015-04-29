2a297db185d6169adae74c8b4ac79d3a1bb5dc30
subversion <svn@deadlybossmods.com>
2015-04-29 22:12:09 +0200

    Omega via svn r13680: Fix more teluur errors

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13678 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13680 $"):sub(12, -3)),
 	DisplayVersion = "6.1.8 alpha", -- the string that is shown as version
 	ReleaseRevision = 13634 -- the revision of the latest stable version that is available
 }
