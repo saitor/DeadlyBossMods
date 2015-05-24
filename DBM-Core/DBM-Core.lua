5da8d404b29498cb6c9cfe84bc91abdfb944fcb1
subversion <svn@deadlybossmods.com>
2015-05-24 22:21:37 +0200

    Omega via svn r13806: enable special yells even in LFR, if IsTestBuild() is true, for testing.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13805 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13806 $"):sub(12, -3)),
 	DisplayVersion = "6.1.9 alpha", -- the string that is shown as version
 	ReleaseRevision = 13725 -- the revision of the latest stable version that is available
 }
