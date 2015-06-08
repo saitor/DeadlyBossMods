8c994a59a4dd50b48f80c1d88b3483d6ab1ed196
subversion <svn@deadlybossmods.com>
2015-06-08 05:01:09 +0200

    Omega via svn r13856: Thought I fixed this ages ago. heat should be 2 stacks not 3

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13855 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13856 $"):sub(12, -3)),
 	DisplayVersion = "6.1.10 alpha", -- the string that is shown as version
 	ReleaseRevision = 13817 -- the revision of the latest stable version that is available
 }
