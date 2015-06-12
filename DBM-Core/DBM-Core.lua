c6610bdb6bf61131d6cafe9d594e6e4c52d7442c
subversion <svn@deadlybossmods.com>
2015-06-12 09:34:11 +0200

    Omega via svn r13877: Update iskar for normal modes timers

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13876 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13877 $"):sub(12, -3)),
 	DisplayVersion = "6.1.11 alpha", -- the string that is shown as version
 	ReleaseRevision = 13867 -- the revision of the latest stable version that is available
 }
