e21123cc22b7fbf7f754f41dfd146b1902eb6630
subversion <svn@deadlybossmods.com>
2015-06-18 09:59:52 +0200

    Omega via svn r13895: Attempt to find befouled a different way, unfortunately radar is disabled with this method since no way to detect it expiring without a spammy repeat check which I?\226?\128?\153m not going to do.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13894 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13895 $"):sub(12, -3)),
 	DisplayVersion = "6.1.11 alpha", -- the string that is shown as version
 	ReleaseRevision = 13867 -- the revision of the latest stable version that is available
 }
