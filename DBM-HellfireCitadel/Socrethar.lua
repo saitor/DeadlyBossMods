1588f75c157d5ed231a30f2b57099cbe017c4a38
subversion <svn@deadlybossmods.com>
2015-06-26 00:49:57 +0200

    Omega via svn r13926: Fix bug on socrethar where interrupt warnings didn?\226?\128?\153t give target Added basic icon auto marking to felcasters Added basic add timers for normal/heroic. sides not included yet. Mythic not included yet. I?\226?\128?\153m not even sure these will stay. Add timers are HIGHLY variable. don?\226?\128?\153t match log to log. they can provide approximation at best. Added spawn announces and counts Re-Enabled the interrupt warning for volley, since it?\226?\128?\153s now actually interruptable (before transform)

--- DBM-HellfireCitadel/Socrethar.lua
+++ DBM-HellfireCitadel/Socrethar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1427, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13913 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13926 $"):sub(12, -3))
 mod:SetCreatureID(92330)
 mod:SetEncounterID(1794)
 mod:SetZone()
@@ -234,10 +234,10 @@ function mod:SPELL_CAST_START(args)
 			timerApocalypseCD:Start()
 		end
 	elseif spellId == 184239 and self:CheckInterruptFilter(args.sourceGUID) then
-		specWarnShadowWordAgony:Show()
+		specWarnShadowWordAgony:Show(args.sourceName)
 		voiceShadowWordAgony:Play("kickcast")
 	elseif spellId == 182392 and self:CheckInterruptFilter(args.sourceGUID) then
-		specWarnShadowBoltVolley:Show()
+		specWarnShadowBoltVolley:Show(args.sourceName)
 		voiceShadowBoltVolley:Play("kickcast")
 	elseif spellId == 188693 then
 		self.vb.felBurstCount = self.vb.felBurstCount + 1
