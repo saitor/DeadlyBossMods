e5e3aa89f6d58435c628ebc5a6179798b863668c
subversion <svn@deadlybossmods.com>
2015-06-26 10:35:51 +0200

    Omega via svn r13930: Shorten option text, it?\226?\128?\153s only way it works

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13929 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13930 $"):sub(12, -3)),
 	DisplayVersion = "6.2.1 alpha", -- the string that is shown as version
 	ReleaseRevision = 13904 -- the revision of the latest stable version that is available
 }
