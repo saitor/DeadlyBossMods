39d50207d9ddaa99c28e48748df8a88a674722ad
subversion <svn@deadlybossmods.com>
2015-06-28 09:15:27 +0200

    Omega via svn r13947: Fix a bad bug

--- DBM-HellfireCitadel/Zakuun.lua
+++ DBM-HellfireCitadel/Zakuun.lua
@@ -1,12 +1,12 @@
 local mod	= DBM:NewMod(1391, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13946 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13947 $"):sub(12, -3))
 mod:SetCreatureID(89890)
 mod:SetEncounterID(1777)
 mod:SetZone()
 mod:SetUsedIcons(6, 5, 4, 3, 2, 1)--Seeds ever go over 5?
-mod:SetHotfixNoticeRev(13945)
+mod:SetHotfixNoticeRev(13947)
 mod:SetRespawnTime(30)
 
 mod:RegisterCombat("combat")
@@ -132,7 +132,7 @@ local function warnSeeds(self)
 	--Sort alphabetical to match bigwigs, and since combat log order may diff person to person
 	table.sort(seedsTargets)
 	for i = 1, #seedsTargets do
-		local targetName = UnitName(seedsTargets[i])
+		local targetName = seedsTargets[i]
 		if targetName == playerName then
 			if self.Options.SpecWarn181508you then
 				specWarnSeedPosition:Show(currentType[i])
