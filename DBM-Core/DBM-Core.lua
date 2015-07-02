1cf58cfb2625ca0d941086d4bd9f12fd2d3e2629
subversion <svn@deadlybossmods.com>
2015-07-02 02:03:25 +0200

    Omega via svn r13992: Bump HF

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 13991 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 13992 $"):sub(12, -3)),
 	DisplayVersion = "6.2.3 alpha", -- the string that is shown as version
 	ReleaseRevision = 13976 -- the revision of the latest stable version that is available
 }
--- DBM-HellfireCitadel/HellfireCouncil.lua
+++ DBM-HellfireCitadel/HellfireCouncil.lua
@@ -1,11 +1,12 @@
 local mod	= DBM:NewMod(1432, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 13990 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 13992 $"):sub(12, -3))
 mod:SetCreatureID(92142, 92144, 92146)--Blademaster Jubei'thos (92142). Dia Darkwhisper (92144). Gurthogg Bloodboil (92146) 
 mod:SetEncounterID(1778)
 mod:SetZone()
 --mod:SetUsedIcons(8, 7, 6, 4, 2, 1)
+mod:SetHotfixNoticeRev(13990)
 mod:SetBossHPInfoToHighest()
 --mod.respawnTime = 20
 
