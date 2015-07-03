fe0fbff72e0f7805a768dc5f88ee5ac0c2c17d56
subversion <svn@deadlybossmods.com>
2015-07-03 23:09:30 +0200

    Omega via svn r14007: Push 2 more, and better color defaults for first 4

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14006 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14007 $"):sub(12, -3)),
 	DisplayVersion = "6.2.4 alpha", -- the string that is shown as version
 	ReleaseRevision = 13998 -- the revision of the latest stable version that is available
 }
