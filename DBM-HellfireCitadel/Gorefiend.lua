e02b875b4527eb3b66906c6f812f10df8a0753be
subversion <svn@deadlybossmods.com>
2015-07-07 07:46:23 +0200

    Omega via svn r14056: Fix shared fate timer not using correct count

--- DBM-HellfireCitadel/Gorefiend.lua
+++ DBM-HellfireCitadel/Gorefiend.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1372, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14031 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14056 $"):sub(12, -3))
 mod:SetCreatureID(90199)
 mod:SetEncounterID(1783)
 mod:SetZone()
@@ -184,7 +184,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 		self.vb.sharedFateCount = self.vb.sharedFateCount + 1
 		local cooldown = sharedFateTimers[self.vb.sharedFateCount+1]
 		if cooldown then
-			timerSharedFateCD:Start(cooldown, self.vb.shadowOfDeathCount+1)
+			timerSharedFateCD:Start(cooldown, self.vb.sharedFateCount+1)
 		end
 	end
 end
