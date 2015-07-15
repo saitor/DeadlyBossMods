103ccd58992e99062b6f5e1d8eb03f0d8df08173
subversion <svn@deadlybossmods.com>
2015-07-15 23:51:23 +0200

    Omega via svn r14088: cleanup locals

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14087 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14088 $"):sub(12, -3)),
 	DisplayVersion = "6.2.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 14050 -- the revision of the latest stable version that is available
 }
