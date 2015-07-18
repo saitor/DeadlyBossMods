6fc3b63e1cf2ce2680228d6ff30c7b4739653b28
subversion <svn@deadlybossmods.com>
2015-07-18 23:42:06 +0200

    Omega via svn r14106: Apply shorter berserk back to normal

--- DBM-HellfireCitadel/Iskar.lua
+++ DBM-HellfireCitadel/Iskar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1433, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14057 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14106 $"):sub(12, -3))
 mod:SetCreatureID(90316)
 mod:SetEncounterID(1788)
 mod:DisableESCombatDetection()--Remove if blizz fixes trash firing ENCOUNTER_START
@@ -153,7 +153,11 @@ function mod:OnCombatStart(delay)
 			timerShadowRiposteCD:Start(9.5-delay)
 		end
 	end
-	berserkTimer:Start(-delay)
+	if self:IsNormal() then--Harder berserk on normal vs all other difficulties. Kromog all over again.
+		berserkTimer:Start(480-delay)
+	else
+		berserkTimer:Start(-delay)
+	end
 end
 
 function mod:OnCombatEnd()
