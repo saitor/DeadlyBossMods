a27459b390ef572de067d588ce3418f8ecffe3be
subversion <svn@deadlybossmods.com>
2015-07-25 00:55:13 +0200

    Omega via svn r14142: use correct spellId for splash hud

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14141 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14142 $"):sub(12, -3)),
 	DisplayVersion = "6.2.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 14116 -- the revision of the latest stable version that is available
 }
