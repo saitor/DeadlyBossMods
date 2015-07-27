0dab66fedc3a5048d2269efe495cb937f3fa9d58
subversion <svn@deadlybossmods.com>
2015-07-27 23:04:47 +0200

    Omega via svn r14150: Delay artillery run out message so maybe tanks and dps stop running out WAY too early. 7 seconds should be enough time.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14149 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14150 $"):sub(12, -3)),
 	DisplayVersion = "6.2.6 alpha", -- the string that is shown as version
 	ReleaseRevision = 14116 -- the revision of the latest stable version that is available
 }
