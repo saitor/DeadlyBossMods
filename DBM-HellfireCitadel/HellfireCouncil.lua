be05836647856140a16121cd718380ca8376e885
subversion <svn@deadlybossmods.com>
2015-07-28 05:39:00 +0200

    Omega via svn r14153: Remove unneeded debug

--- DBM-HellfireCitadel/HellfireCouncil.lua
+++ DBM-HellfireCitadel/HellfireCouncil.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1432, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14136 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14153 $"):sub(12, -3))
 mod:SetCreatureID(92142, 92144, 92146)--Blademaster Jubei'thos (92142). Dia Darkwhisper (92144). Gurthogg Bloodboil (92146) 
 mod:SetEncounterID(1778)
 mod:SetZone()
@@ -233,7 +233,6 @@ function mod:SPELL_AURA_REMOVED(args)
 end--]]
 
 function mod:UNIT_DIED(args)
-	DBM:Debug("UNIT_DIED fired", 2)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 92144 then--Dia Darkwhisper
 		DBM:Debug("Dia died", 2)
