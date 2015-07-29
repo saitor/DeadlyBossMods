7a2aa7c87daec9124115efe58468f1c4c49dcff2
subversion <svn@deadlybossmods.com>
2015-07-29 04:08:37 +0200

    Omega via svn r14165: Upgrade archimond hud to have many lines!

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14162 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14165 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -520,13 +520,6 @@ function mod:SPELL_AURA_APPLIED(args)
 					warnWroughtChaos:Show(self.vb.wroughtWarned, FRIENDS_FRIENDS_CHOICE_EVERYONE)--"Everyone"
 				end
 			end
-			if self.Options.HudMapOnWrought then
-				if self:IsMythic() then--Entire raid gets it, must use a small hud
-					DBMHudMap:RegisterRangeMarkerOnPartyMember(spellId, "highlight", args.destName, 2.5, 5, 1, 1, 0, 0.5, nil, true, 1):Pulse(0.5, 0.5)--Yellow
-				else
-					DBMHudMap:RegisterRangeMarkerOnPartyMember(spellId, "highlight", args.destName, 5, 5, 1, 1, 0, 0.5, nil, true, 1):Pulse(0.5, 0.5)--Yellow
-				end
-			end
 		end
 	elseif spellId == 185014 then--Focused Chaos
 		if args:IsPlayer() then
@@ -545,10 +538,10 @@ function mod:SPELL_AURA_APPLIED(args)
 				warnWroughtChaos:CombinedShow(0.3, self.vb.wroughtWarned, args.destName)
 			end
 			if self.Options.HudMapOnWrought then
-				if self:IsMythic() then--Entire raid gets it, must use a small hud
-					DBMHudMap:RegisterRangeMarkerOnPartyMember(spellId, "highlight", args.destName, 2.5, 5, 1, 0, 0, 0.5, nil, true, 2):Pulse(0.5, 0.5)--Red
-				else
-					DBMHudMap:RegisterRangeMarkerOnPartyMember(spellId, "highlight", args.destName, 5, 5, 1, 0, 0, 0.5, nil, true, 2):Pulse(0.5, 0.5)--Red
+				if args.sourceName == playerName or args.destName == playerName then--Players connection, green lines
+					DBMHudMap:AddEdge(0, 1, 0, 0.5, 5, args.sourceName, args.destName, nil, nil, nil, nil)
+				else--red lines for non player lines
+					DBMHudMap:AddEdge(1, 0, 0, 0.5, 5, args.sourceName, args.destName, nil, nil, nil, nil)
 				end
 			end
 		end
