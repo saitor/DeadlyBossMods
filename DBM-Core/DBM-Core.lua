470193d207187095dd50e5973a775de16755e29c
subversion <svn@deadlybossmods.com>
2015-07-29 02:14:26 +0200

    Omega via svn r14162: A lot of archimond mythic

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14161 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14162 $"):sub(12, -3)),
 	DisplayVersion = "6.2.7 alpha", -- the string that is shown as version
 	ReleaseRevision = 14159 -- the revision of the latest stable version that is available
 }
