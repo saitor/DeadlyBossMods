c79dabb2b473530d660b1fbbed4d2742d92f72e8
subversion <svn@deadlybossmods.com>
2015-07-29 22:48:16 +0200

    Omega via svn r14172: Chakram hud Fix bug in mark of the legion on archimond

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14171 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14172 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -617,6 +617,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif spellId == 187050 then
 		self.vb.markOfLegionRemaining = self.vb.markOfLegionRemaining + 1
 		legionTargets[#legionTargets+1] = args.destName
+		self:Unschedule(showMarkOfLegion)
 		if #legionTargets == 4 then
 			showMarkOfLegion(self)
 		else
