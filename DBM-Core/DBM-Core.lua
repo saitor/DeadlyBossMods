3f48e8407af05e96c1f29ab66f6034db9b944310
subversion <svn@deadlybossmods.com>
2015-08-02 20:08:02 +0200

    Omega via svn r14213: add debuff filter to hudar. Tweak shackles to filter other shackle targets as well. Change behavior back to only caring about self for NON shackled targets

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14212 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14213 $"):sub(12, -3)),
 	DisplayVersion = "6.2.7 alpha", -- the string that is shown as version
 	ReleaseRevision = 14159 -- the revision of the latest stable version that is available
 }
