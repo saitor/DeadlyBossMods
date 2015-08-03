637d6a1d177e09d2f2fe75e5709f957b98dde267
subversion <svn@deadlybossmods.com>
2015-08-03 05:04:30 +0200

    Omega via svn r14224: Use yellow lines here while at it.

--- DBM-HellfireCitadel/Gorefiend.lua
+++ DBM-HellfireCitadel/Gorefiend.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1372, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14223 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14224 $"):sub(12, -3))
 mod:SetCreatureID(90199)
 mod:SetEncounterID(1783)
 mod:SetZone()
@@ -119,13 +119,13 @@ local function sharedFateDelay(self)
 		specWarnSharedFate:Show(self.vb.rootedFate2)
 		voiceSharedFate:Play("linegather")
 		if self.Options.HudMapOnSharedFate then
-			DBMHudMap:AddEdge(0, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate2)
+			DBMHudMap:AddEdge(1, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate2)
 		end
 	elseif self.vb.rootedFate then
 		specWarnSharedFate:Show(self.vb.rootedFate)
 		voiceSharedFate:Play("linegather")
 		if self.Options.HudMapOnSharedFate then
-			DBMHudMap:AddEdge(0, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate)
+			DBMHudMap:AddEdge(1, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate)
 		end
 	end
 end
