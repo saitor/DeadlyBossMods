609e493de6c02cf92a93581c837c9739ec3d8894
subversion <svn@deadlybossmods.com>
2015-08-03 21:05:24 +0200

    Omega via svn r14232: patch to callbacks

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14231 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14232 $"):sub(12, -3)),
 	DisplayVersion = "6.2.7 alpha", -- the string that is shown as version
 	ReleaseRevision = 14159 -- the revision of the latest stable version that is available
 }
@@ -1217,13 +1217,12 @@ do
 	end
 
 	function DBM:IsCallbackRegistered(event, f)
-		if not event then
-			error("Usage: DBM:IsCallbackRegistered(event, callbackFunc(optional))", 2)
+		if not event or type(f) ~= "function" then
+			error("Usage: IsCallbackRegistered(event, callbackFunc)", 2)
 		end
-		if f then
-			return callbacks[event][f] and true or false
-		else
-			return callbacks[event] and true or false
+		if not callbacks[event] then return end
+		for i = 1, #callbacks[event] do
+			if callbacks[event][i] == f then return true end
 		end
 		return false
 	end
@@ -1238,11 +1237,13 @@ do
 	end
 
 	function DBM:UnregisterCallback(event, f)
-		if not event or not callbacks[event] then return end
-		if f then
-			callbacks[event][f] = nil
-		else
-			callbacks[event] = nil
+		if not event or type(f) ~= "function" then
+			error("Usage: UnregisterCallback(event, callbackFunc)", 2)
+		end
+		if not callbacks[event] then return end
+		--> checking from the end to start and not stoping after found one result in case of a func being twice registered.
+		for i = #callbacks[event], 1, -1 do
+			if callbacks[event][i] == f then tremove (callbacks[event], i) end
 		end
 	end
 end
