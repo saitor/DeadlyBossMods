a03d2d98b04b800f9e684e98144cc9557e023395
subversion <svn@deadlybossmods.com>
2015-08-05 22:42:42 +0200

    Omega via svn r14250: improve infoframe headers on mannoroth

--- DBM-HellfireCitadel/Mannoroth.lua
+++ DBM-HellfireCitadel/Mannoroth.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1395, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14147 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14250 $"):sub(12, -3))
 mod:SetCreatureID(91349)--91305 Fel Iron Summoner
 mod:SetEncounterID(1795)
 mod:SetZone()
@@ -209,7 +209,7 @@ local function warnGazeTargts(self)
 		end
 	end
 	if self.Options.InfoFrame and not DBM.InfoFrame:IsShown() then
-		--DBM.InfoFrame:SetHeader(gaze1)
+		DBM.InfoFrame:SetHeader(gaze1)
 		DBM.InfoFrame:Show(8, "function", updateInfoFrame, sortInfoFrame)
 	end
 end
@@ -424,8 +424,11 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnWrathofGuldan:Show()
 			yellWrathofGuldan:Yell()
 		end
-		if self.Options.InfoFrame and not DBM.InfoFrame:IsShown() then
-			DBM.InfoFrame:Show(8, "function", updateInfoFrame, sortInfoFrame)
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:SetHeader(args.spellName)--Always set header to wrath if wrath is present
+			if not DBM.InfoFrame:IsShown() then
+				DBM.InfoFrame:Show(8, "function", updateInfoFrame, sortInfoFrame)
+			end
 		end
 	end
 end
