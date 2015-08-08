0317dd96882c1149087e67edecfedba56b40f066
subversion <svn@deadlybossmods.com>
2015-08-08 20:49:08 +0200

    Omega via svn r14294: Enable auto icon marking on mythic infernals too

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14293 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14294 $"):sub(12, -3)),
 	DisplayVersion = "6.2.7 alpha", -- the string that is shown as version
 	ReleaseRevision = 14159 -- the revision of the latest stable version that is available
 }
