9d6f24813ddbbc9133c530983703402ec0c467b1
subversion <svn@deadlybossmods.com>
2015-08-10 20:19:47 +0200

    Omega via svn r14310: Make the shackled torment code more robust, it should no longer error on a wipe if there are less than 3 shackled torment targets on mythic Fix countdown bug on mannoroth

--- DBM-HellfireCitadel/Mannoroth.lua
+++ DBM-HellfireCitadel/Mannoroth.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1395, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14305 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14310 $"):sub(12, -3))
 mod:SetCreatureID(91349)--91305 Fel Iron Summoner
 mod:SetEncounterID(1795)
 mod:SetZone()
@@ -378,7 +378,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			yellCurseofLegion:Schedule(debuffTime - 1, 1)
 			yellCurseofLegion:Schedule(debuffTime - 2, 2)
 			yellCurseofLegion:Schedule(debuffTime - 3, 3)
-			yellCurseofLegion:Schedule(debuffTime - 2, 4)
+			yellCurseofLegion:Schedule(debuffTime - 4, 4)
 			yellCurseofLegion:Schedule(debuffTime - 5, 5)
 		else
 			warnCurseoftheLegion:Show(self.vb.doomlordCount, args.destName)
