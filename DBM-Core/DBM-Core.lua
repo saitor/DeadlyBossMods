ff63e574106d63ae951b1e3f22e9c0f75410c5f3
subversion <svn@deadlybossmods.com>
2015-08-11 01:13:40 +0200

    Omega via svn r14312: bwversion

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14311 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14312 $"):sub(12, -3)),
 	DisplayVersion = "6.2.8 alpha", -- the string that is shown as version
 	ReleaseRevision = 14306 -- the revision of the latest stable version that is available
 }
@@ -390,7 +390,7 @@ local statusWhisperDisabled = false
 local wowTOC = select(4, GetBuildInfo())
 local dbmToc = 0
 
-local fakeBWRevision = 13588
+local fakeBWRevision = 13601
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
 local guiRequested = false
