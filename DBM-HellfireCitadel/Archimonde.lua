f6e6742dc3ee319c5441b6c439d7e84055cce4ff
subversion <svn@deadlybossmods.com>
2015-08-11 17:26:19 +0200

    Omega via svn r14318: Add nil checks in case someone ninja leaves raid mid pull. cause this happens apparently.

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14311 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14318 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -244,7 +244,7 @@ local function updateInfoFrame()
 		if i == 9 then break end--It's a wipe, plus can't do more than 8 of these with icons
 		local name = felburstTargets[i]
 		local uId = DBM:GetRaidUnitId(name)
-		if UnitDebuff(uId, felburstDebuff) then
+		if uId and UnitDebuff(uId, felburstDebuff) then
 			total = total + 1
 			lines[name] = i
 		end
@@ -252,7 +252,7 @@ local function updateInfoFrame()
 	for i = 1, #shacklesTargets do
 		local name = shacklesTargets[i]
 		local uId = DBM:GetRaidUnitId(name)
-		if UnitDebuff(uId, shackledDebuff) then
+		if uId and UnitDebuff(uId, shackledDebuff) then
 			total = total + 1
 			lines[name] = i
 		end
