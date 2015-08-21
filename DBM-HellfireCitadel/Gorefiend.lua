25104654a9c2679f0241d5e0a51373d7fc9398b4
subversion <svn@deadlybossmods.com>
2015-08-21 22:56:36 +0200

    Omega via svn r14373: Fix error

--- DBM-HellfireCitadel/Gorefiend.lua
+++ DBM-HellfireCitadel/Gorefiend.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1372, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14370 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14373 $"):sub(12, -3))
 mod:SetCreatureID(90199)
 mod:SetEncounterID(1783)
 mod:SetZone()
@@ -374,7 +374,6 @@ function mod:SPELL_AURA_REMOVED(args)
 			DBMHudMap:FreeEncounterMarkerByTarget(179908, args.destName)
 		end
 	elseif spellId == 181295 then
-		self.vb.playersWithDigest = self.vb.playersWithDigest - 1
 		if args:IsPlayer() then
 			timerDigest:Cancel()
 			countdownDigest:Cancel()
