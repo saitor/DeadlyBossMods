0b0def41d3db9529ec82805d7e5e87472c25036a
subversion <svn@deadlybossmods.com>
2015-08-23 22:47:14 +0200

    Omega via svn r14398: Update libchatanims

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,7 +52,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14397 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14398 $"):sub(12, -3)),
 	DisplayVersion = "6.2.9 alpha", -- the string that is shown as version
 	ReleaseRevision = 14339 -- the revision of the latest stable version that is available
 }
