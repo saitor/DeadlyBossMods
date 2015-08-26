4447403c8d73f515f3310e7d6da2afe3be1b96db
subversion <svn@deadlybossmods.com>
2015-08-26 12:34:20 +0200

    Omega via svn r14427: Add example

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14426 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14427 $"):sub(12, -3)),
 	DisplayVersion = "6.2.10 alpha", -- the string that is shown as version
 	ReleaseRevision = 14421 -- the revision of the latest stable version that is available
 }
