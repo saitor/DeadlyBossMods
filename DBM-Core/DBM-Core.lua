2958aeb3105fad939df25be114296ddcb3cccc1c
subversion <svn@deadlybossmods.com>
2015-08-26 02:59:23 +0200

    Omega via svn r14424: Improve range frame on mannoroth to factor doomspike on mythic

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14423 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14424 $"):sub(12, -3)),
 	DisplayVersion = "6.2.10 alpha", -- the string that is shown as version
 	ReleaseRevision = 14421 -- the revision of the latest stable version that is available
 }
