578027b4583c06350a389d43b30202abbf73ec4e
subversion <svn@deadlybossmods.com>
2015-08-27 15:29:23 +0200

    TOM_RUS via svn r14444: ruRU update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14443 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14444 $"):sub(12, -3)),
 	DisplayVersion = "6.2.10 alpha", -- the string that is shown as version
 	ReleaseRevision = 14421 -- the revision of the latest stable version that is available
 }
