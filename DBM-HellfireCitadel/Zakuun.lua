6e79e2339ed175adeddb0f76e1a1f78f0450ea04
subversion <svn@deadlybossmods.com>
2015-09-08 07:17:02 +0200

    Omega via svn r14473: Tweaks

--- DBM-HellfireCitadel/Zakuun.lua
+++ DBM-HellfireCitadel/Zakuun.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1391, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14471 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14473 $"):sub(12, -3))
 mod:SetCreatureID(89890)
 mod:SetEncounterID(1777)
 mod:SetZone()
@@ -338,12 +338,13 @@ function mod:SPELL_AURA_APPLIED(args)
 		updateRangeFrame(self)
 	elseif spellId == 179407 then
 		warnDisembodied:CombinedShow(0.3, self.vb.SoulCleaveCount, args.destName)
-		local uId = DBM:GetRaidUnitId(args.destName)
-		if not args:IsPlayer() and self:IsTanking(uId, "boss1") then
-			specWarnDisembodied:Show(args.destName)
-		end
 		if args:IsPlayer() then
 			countdownDisembodied:Start()
+		else
+			local uId = DBM:GetRaidUnitId(args.destName)
+			if self:IsTanking(uId, "boss1") then
+				specWarnDisembodied:Show(args.destName)
+			end
 		end
 	end
 end
