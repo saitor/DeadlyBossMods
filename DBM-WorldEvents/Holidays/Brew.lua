ecfc1c76fab8a1b43f830914d7cea524dff724b9
subversion <svn@deadlybossmods.com>
2015-09-21 23:01:29 +0200

    Omega via svn r14532: Check music checkbox too, and just flat out mute if music isn?\226?\128?\153t even turned on.

--- DBM-WorldEvents/Holidays/Brew.lua
+++ DBM-WorldEvents/Holidays/Brew.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Brew", "DBM-WorldEvents", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14531 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14532 $"):sub(12, -3))
 --mod:SetCreatureID(15467)
 --mod:SetModelID(15879)
 mod:SetReCombatTime(10)
@@ -45,9 +45,10 @@ CheckEventActive()
 local function setDialog(self, set)
 	if not self.Options.NormalizeVolume then return end
 	if set then
+		local musicEnabled = GetCVarBool("Sound_EnableMusic") or true
 		local musicVolume = tonumber(GetCVar("Sound_MusicVolume"))
 		self.Options.SoundOption = tonumber(GetCVarBool("Sound_DialogVolume")) or 1
-		if musicVolume then--Normalize volume to music volume level
+		if musicEnabled and musicVolume then--Normalize volume to music volume level
 			DBM:Debug("Setting normalized volume to music volume of: "..musicVolume)
 			SetCVar("Sound_DialogVolume", musicVolume)
 		else--Just mute it
