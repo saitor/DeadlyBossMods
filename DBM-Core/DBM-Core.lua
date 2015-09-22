e85d712182c2f232144c35b4d2bb877fac6fea33
subversion <svn@deadlybossmods.com>
2015-09-22 07:56:35 +0200

    Omega via svn r14536: Fix bug that caused voice pack sounds to not suppress special warning sounds for some warnings.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14535 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14536 $"):sub(12, -3)),
 	DisplayVersion = "6.2.12 alpha", -- the string that is shown as version
 	ReleaseRevision = 14486 -- the revision of the latest stable version that is available
 }
@@ -9499,7 +9499,7 @@ do
 				self:AddMsg(DBM_CORE_VOICE_PACK_OUTDATED)
 				SWFilterDisabed = self.VoiceVersions[value]--Set disable to version on current voice pack
 			else
-				SWFilterDisabed = 3
+				SWFilterDisabed = 5
 			end
 		end
 	end
