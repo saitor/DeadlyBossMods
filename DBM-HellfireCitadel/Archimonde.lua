c804195ca600a016978fefec99306d790093392f
subversion <svn@deadlybossmods.com>
2015-10-01 07:51:54 +0200

    Omega via svn r14571: Put a check here too so that timer doesn?\226?\128?\153t activate in timer correction code

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14570 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14571 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -532,7 +532,7 @@ local function updateAllTimers(self, ICD)
 			countdownShackledTorment:Cancel()
 			countdownShackledTorment:Start(ICD)
 		end
-		if timerWroughtChaosCD:GetRemaining() < ICD then
+		if not self:IsFaceroll() and timerWroughtChaosCD:GetRemaining() < ICD then
 			local elapsed, total = timerWroughtChaosCD:GetTime()
 			local extend = ICD - (total-elapsed)
 			DBM:Debug("timerWroughtChaosCD extended by: "..extend, 2)
@@ -558,7 +558,7 @@ local function updateAllTimers(self, ICD)
 			countdownShackledTorment:Cancel()
 			countdownShackledTorment:Start(ICD)
 		end
-		if timerWroughtChaosCD:GetRemaining() < ICD then
+		if not self:IsFaceroll() and timerWroughtChaosCD:GetRemaining() < ICD then
 			local elapsed, total = timerWroughtChaosCD:GetTime()
 			local extend = ICD - (total-elapsed)
 			DBM:Debug("timerWroughtChaosCD extended by: "..extend, 2)
