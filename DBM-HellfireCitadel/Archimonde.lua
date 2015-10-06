20ff7f9d558089412110849d7affebd142feb3eb
subversion <svn@deadlybossmods.com>
2015-10-06 09:50:20 +0200

    Omega via svn r14584: More efficient fix. Plus extra failsafe just in case

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14583 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14584 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -955,8 +955,10 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnNetherBanishOther:Show(self.vb.netherBanish2, args.destName)
 		end
 		updateRangeFrame(self)
-	elseif spellId == 189895 and (self:IsMythic() or playerBanished or not self.Options.FilterOtherPhase) then
-		warnVoidStarFixate:CombinedShow(0.3, args.destName)--5 on mythic
+	elseif spellId == 189895 then
+		if (playerBanished or not self.Options.FilterOtherPhase) then
+			warnVoidStarFixate:CombinedShow(0.3, args.destName)--5 on mythic
+		end
 		if args:IsPlayer() then
 			specWarnVoidStarFixate:Show()
 			yellVoidStarFixate:Yell()
@@ -1158,6 +1160,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 			timerShackledTormentCD:Start(55, self.vb.tormentCast+1)
 			countdownShackledTorment:Start(55)
 		else
+			playerBanished = true
 			--All need work, actual logs would be nice
 			table.wipe(shacklesTargets)--Just to reduce infoframe overhead
 			timerWroughtChaosCD:Cancel()
