f9a2304082004c931f2dd25eef7120de522dcdd1
subversion <svn@deadlybossmods.com>
2015-10-13 08:06:36 +0200

    Omega via svn r14594: Move edict to SUCCESS for timer. It can miss target if they just happened to have an immunity up when targeted.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14593 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14594 $"):sub(12, -3)),
 	DisplayVersion = "6.2.13 alpha", -- the string that is shown as version
 	ReleaseRevision = 14565 -- the revision of the latest stable version that is available
 }
