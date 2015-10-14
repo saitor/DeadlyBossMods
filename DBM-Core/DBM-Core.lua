bb734af933855eb8e599ac1d6885d2a02e612667
subversion <svn@deadlybossmods.com>
2015-10-14 23:26:53 +0200

    Omega via svn r14595: #Mannoroth tweaks *Better player messages for doom, so they can get position from that too instead of looking at own yell to figure it out. This includes voice pack alert as well, which will say ?\226?\128?\156move to <mark>?\226?\128?\157 instead of ?\226?\128?\156run out?\226?\128?\157 on mythic difficulty. *Doom icons are now ON by default (but now disabled in LFR) #Archmonde Tweek *Mark of the legion now has voice pack support. This was a blatant oversight and I?\226?\128?\153m sorry for that. Not enough voice pack users to notice it earlier :\

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14594 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14595 $"):sub(12, -3)),
 	DisplayVersion = "6.2.13 alpha", -- the string that is shown as version
 	ReleaseRevision = 14565 -- the revision of the latest stable version that is available
 }
@@ -7048,6 +7048,13 @@ function bossModPrototype:CheckBigWigs(name)
 	end
 end
 
+do
+	local iconStrings = {[1] = RAID_TARGET_1, [2] = RAID_TARGET_2, [3] = RAID_TARGET_3, [4] = RAID_TARGET_4, [5] = RAID_TARGET_5, [6] = RAID_TARGET_6, [7] = RAID_TARGET_7, [8] = RAID_TARGET_8,}
+	function bossModPrototype:IconNumToString(number)
+		return iconStrings[number]
+	end
+end
+
 bossModPrototype.AntiSpam = DBM.AntiSpam
 bossModPrototype.GetUnitCreatureId = DBM.GetUnitCreatureId
 bossModPrototype.GetCIDFromGUID = DBM.GetCIDFromGUID
@@ -9426,6 +9433,10 @@ do
 	
 	function bossModPrototype:NewSpecialWarningYouCount(text, optionDefault, ...)
 		return newSpecialWarning(self, "youcount", text, nil, optionDefault, ...)
+	end
+	
+	function bossModPrototype:NewSpecialWarningYouPos(text, optionDefault, ...)
+		return newSpecialWarning(self, "youpos", text, nil, optionDefault, ...)
 	end
 
 	function bossModPrototype:NewSpecialWarningTarget(text, optionDefault, ...)
