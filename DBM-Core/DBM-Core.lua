5820d6d7db30e590297472f922cad3a102eb6b18
subversion <svn@deadlybossmods.com>
2015-11-19 04:41:54 +0100

    Omega via svn r14658: Tweak

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14657 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14658 $"):sub(12, -3)),
 	DisplayVersion = "6.2.15 alpha", -- the string that is shown as version
 	ReleaseRevision = 14621 -- the revision of the latest stable version that is available
 }
--- DBM-HellfireCitadel/Mannoroth.lua
+++ DBM-HellfireCitadel/Mannoroth.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1395, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14654 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14658 $"):sub(12, -3))
 mod:SetCreatureID(91349)--91305 Fel Iron Summoner
 mod:SetEncounterID(1795)
 mod:SetZone()
@@ -621,13 +621,13 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, npc)
 				timerWrathofGuldanCD:Start(10)
 			end
 			--Detect when adds timers will reset (by 181301) before they come off cd, and cancel them early
-			if timerFelImplosionCD:GetRemaining(self.vb.impCount+1) > 15 then
+			if timerFelImplosionCD:GetRemaining(self.vb.impCount+1) > 10 then
 				timerFelImplosionCD:Cancel()
 			end
-			if timerInfernoCD:GetRemaining(self.vb.infernalCount+1) > 15 then
+			if timerInfernoCD:GetRemaining(self.vb.infernalCount+1) > 10 then
 				timerInfernoCD:Cancel()
 			end
-			if timerCurseofLegionCD:GetRemaining(self.vb.doomlordCount+1) > 15 then
+			if timerCurseofLegionCD:GetRemaining(self.vb.doomlordCount+1) > 10 then
 				timerCurseofLegionCD:Cancel()
 			end
 		elseif self.vb.phase == 4 then
