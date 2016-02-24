8bfe5d7185164be93e11422acba9aadcef7b5215
subversion <svn@deadlybossmods.com>
2016-02-24 01:37:12 +0100

    Omega via svn r14808: Fix logic error

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14807 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14808 $"):sub(12, -3)),
 	DisplayVersion = "6.2.19 alpha", -- the string that is shown as version
 	ReleaseRevision = 14770 -- the revision of the latest stable version that is available
 }
