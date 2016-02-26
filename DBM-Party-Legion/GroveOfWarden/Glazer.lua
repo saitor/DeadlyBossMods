436aa554e5a395d4d3f8e37e1f9f47644476ca7c
subversion <svn@deadlybossmods.com>
2016-02-26 01:01:28 +0100

    Omega via svn r14818: Use this voice

--- DBM-Party-Legion/GroveOfWarden/Glazer.lua
+++ DBM-Party-Legion/GroveOfWarden/Glazer.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1469, "DBM-Party-Legion", 10, 707)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14815 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14818 $"):sub(12, -3))
 mod:SetCreatureID(95887)
 mod:SetEncounterID(1817)
 mod:SetZone()
@@ -17,11 +17,12 @@ mod:RegisterEventsInCombat(
 --TODO, maybe voice for mirrors to bounce beam back into boss? But how do you say this short
 local warnGaze						= mod:NewSpellAnnounce(194942, 2)
 
-local specWarnFocused				= mod:NewSpecialWarningSpell(194289, nil, nil, nil, 2)
+local specWarnFocused				= mod:NewSpecialWarningSpell(194289, nil, nil, nil, 2, 2)
 local specWarnGazeGTFO				= mod:NewSpecialWarningMove(194289, nil, nil, nil, 1, 2)
 
 local timerGazeCD					= mod:NewCDTimer(19.4, 194289, nil, nil, nil, 3)
 
+local voiceFocused					= mod:NewVoice(194289)--specialsoon
 local voiceGazeGTFO					= mod:NewVoice(194289)--runaway
 
 function mod:OnCombatStart(delay)
@@ -32,6 +33,7 @@ function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 194289 then
 		specWarnFocused:Show()
+		voiceFocused:Play("specialsoon")
 	elseif spellId == 194942 then
 		warnGaze:Show()
 		timerGazeCD:Start()
