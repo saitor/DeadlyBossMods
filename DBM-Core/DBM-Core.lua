70b79c61a10f369b43b2531eab2996d56b6f486d
subversion <svn@deadlybossmods.com>
2016-03-22 21:00:17 +0100

    tandanu via svn r14864: increment alpha version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,8 +40,8 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14862 $"):sub(12, -3)),
-	DisplayVersion = "6.2.20", -- the string that is shown as version
+	Revision = tonumber(("$Revision: 14864 $"):sub(12, -3)),
+	DisplayVersion = "6.2.21 alpha", -- the string that is shown as version
 	ReleaseRevision = 14862 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
