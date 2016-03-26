0628426602e12ca11a79aee6e8a17501380ecd61
subversion <svn@deadlybossmods.com>
2016-03-26 04:59:44 +0100

    Omega via svn r14871: Switch this one back

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14870 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14871 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -1003,7 +1003,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			end
 		end
 		if not playerBanished or not self.Options.FilterOtherPhase then
-			local time = 4.5
+			local time = 5
 			if not self:IsMythic() then
 				warnWroughtChaos:CombinedShow(0.3, self.vb.wroughtWarned, args.destName)
 			else
