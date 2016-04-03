829438087cd1842004cd0278489a38c293d835fe
subversion <svn@deadlybossmods.com>
2016-04-03 06:03:20 +0200

    Omega via svn r14889: Push Ilgynoth

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14888 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14889 $"):sub(12, -3)),
 	DisplayVersion = "6.2.22 alpha", -- the string that is shown as version
 	ReleaseRevision = 14865 -- the revision of the latest stable version that is available
 }
