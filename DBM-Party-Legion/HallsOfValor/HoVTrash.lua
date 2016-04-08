333ed27c371e7933f5eb71a2e7044bbf2affacf4
subversion <svn@deadlybossmods.com>
2016-04-08 04:47:48 +0200

    Omega via svn r14905: More halls of valor additions

--- DBM-Party-Legion/HallsOfValor/HoVTrash.lua
+++ DBM-Party-Legion/HallsOfValor/HoVTrash.lua
@@ -1,14 +1,14 @@
 local mod	= DBM:NewMod("HoVTrash", "DBM-Party-Legion", 4, 721)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14904 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14905 $"):sub(12, -3))
 --mod:SetModelID(47785)
 mod:SetZone()
 
 mod.isTrashMod = true
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 199805 192563"
+	"SPELL_CAST_START 199805 192563 199726"
 )
 
 local warnCrackle					= mod:NewTargetAnnounce(199805, 2)
@@ -16,9 +16,11 @@ local warnCrackle					= mod:NewTargetAnnounce(199805, 2)
 local specWarnCrackle				= mod:NewSpecialWarningDodge(199805, nil, nil, nil, 1, 2)
 local yellCrackle					= mod:NewYell(199805)
 local specWarnCleansingFlame		= mod:NewSpecialWarningInterrupt(192563, "HasInterrupt", nil, nil, 1, 2)
+local specWarnUnrulyYell			= mod:NewSpecialWarningInterrupt(199726, "HasInterrupt", nil, nil, 1, 2)
 
 local voiceCrackle					= mod:NewVoice(194966)--watchstep
 local voiceCleansingFlame			= mod:NewVoice(192563, "HasInterrupt")--kickcast
+local voiceUnrulyYell				= mod:NewVoice(199726, "HasInterrupt")--kickcast
 
 mod:RemoveOption("HealthFrame")
 
@@ -44,5 +46,8 @@ function mod:SPELL_CAST_START(args)
 	elseif spellId == 192563 and self:CheckInterruptFilter(args.sourceGUID) then
 		specWarnCleansingFlame:Show(args.sourceName)
 		voiceCleansingFlame:Play("kickcast")
+	elseif spellId == 199726 and self:CheckInterruptFilter(args.sourceGUID) then
+		specWarnUnrulyYell:Show(args.sourceName)
+		voiceUnrulyYell:Play("kickcast")
 	end
 end
