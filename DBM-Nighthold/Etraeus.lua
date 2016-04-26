d6ac076b108bf1fa4c046836416f203604f9fe85
subversion <svn@deadlybossmods.com>
2016-04-26 06:32:27 +0200

    Omega via svn r14937: Finished Krosus mod

--- DBM-Nighthold/Etraeus.lua
+++ DBM-Nighthold/Etraeus.lua
@@ -1,12 +1,13 @@
 local mod	= DBM:NewMod(1732, "DBM-Nighthold", nil, 786)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14936 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14937 $"):sub(12, -3))
 mod:SetCreatureID(103758)
 mod:SetEncounterID(1863)
 mod:SetZone()
 --mod:SetUsedIcons(8, 7, 6, 3, 2, 1)
 --mod:SetHotfixNoticeRev(12324)
+mod.respawnTime = 30--or 35 or 40
 
 mod:RegisterCombat("combat")
 
