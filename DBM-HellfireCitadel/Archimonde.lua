5c3976ed3d502c47e52f0f7270298671ba0a149b
subversion <svn@deadlybossmods.com>
2016-05-14 04:20:52 +0200

    Omega via svn r14958: Push Calamir and Anamouz

--- DBM-HellfireCitadel/Archimonde.lua
+++ DBM-HellfireCitadel/Archimonde.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1438, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14938 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 14958 $"):sub(12, -3))
 mod:SetCreatureID(91331)--Doomfire Spirit (92208), Hellfire Deathcaller (92740), Felborne Overfiend (93615), Dreadstalker (93616), Infernal doombringer (94412)
 mod:SetEncounterID(1799)
 mod:SetMinSyncRevision(13964)
@@ -992,7 +992,6 @@ function mod:SPELL_AURA_APPLIED(args)
 			if not self:IsMythic() then
 				specWarnFocusedChaos:Show()
 				voiceFocusedChaos:Play("185014")
-				yellFocusedChaos:Yell(5)
 				yellFocusedChaos:Schedule(3, 2)
 				yellFocusedChaos:Schedule(2, 3)
 				yellFocusedChaos:Schedule(1, 4)
