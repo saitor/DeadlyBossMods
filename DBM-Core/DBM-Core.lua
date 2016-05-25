0c38ea49ad5574ffe97b792f7c080f198c1de0f6
subversion <svn@deadlybossmods.com>
2016-05-25 14:15:56 +0200

    ultrashining via svn r14981: zhTW update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 14980 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 14981 $"):sub(12, -3)),
 	DisplayVersion = "6.2.23 alpha", -- the string that is shown as version
 	ReleaseRevision = 14943 -- the revision of the latest stable version that is available
 }
