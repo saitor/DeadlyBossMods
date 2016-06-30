f7a6fe07f3afbe831c7861a560143979f47eee79
subversion <svn@deadlybossmods.com>
2016-06-30 07:40:21 +0200

    Omega via svn r15037: Add range frame for volatile rot for Nythendra Added antispam to nightmare explosion for Ilgynoth Added syncing to mob deaths to ilgynoth do to combat log range issues that could cause info frame to miscount Fixed spider phase detection for 2nd and later spider phase on Renferal now that I have a valid event that I can use. Fixed spammy warnings for Venom debuff scanner for renferal and tweaked timing some so first warning should be less likely to have missing targets. Fixed some timers that didn?\226?\128?\153t show for 4th cast of each phases debuffs on renferal Fixed missing razorwing timers on renferal Fixed bad Roc phase timer for renferal

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -40,7 +40,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15036 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15037 $"):sub(12, -3)),
 	DisplayVersion = "6.2.24 alpha", -- the string that is shown as version
 	ReleaseRevision = 14998 -- the revision of the latest stable version that is available
 }
--- DBM-HellfireCitadel/Mannoroth.lua
+++ DBM-HellfireCitadel/Mannoroth.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1395, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 15036 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 15037 $"):sub(12, -3))
 mod:SetCreatureID(91349)--91305 Fel Iron Summoner
 mod:SetEncounterID(1795)
 mod:SetZone()
@@ -839,7 +839,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		countdownGlaiveCombo:Start(25.2)
 		timerFelImplosionCD:Start(27.4, 1)
 		timerFelSeekerCD:Start(40.2)
-		timerGazeCD:Start(50.7)--50.8-53
+		timerGazeCD:Start(49.5)--49.5-53
 		timerInfernoCD:Start(53, 1)
 	--Backup phase detection. a bit slower than CHAT_MSG_RAID_BOSS_EMOTE (5.5 seconds slower)
 	elseif spellId == 182263 and self.vb.phase == 2 then--Phase 3
