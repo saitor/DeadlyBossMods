b90e1e3347a642072fb12dad7a187b41602deb94
subversion <svn@deadlybossmods.com>
2016-07-18 01:16:27 +0200

    Omega via svn r15058: Fixed some bugs that caused most of Trilliax timers not to fire. Mainly that the SPELL_CAST_START event no longer exists because all of his transforms are now instant casts and use SPELL_CAST_SUCCESS only

--- DBM-Nighthold/ChronoAnomaly.lua
+++ DBM-Nighthold/ChronoAnomaly.lua
@@ -1,12 +1,12 @@
 local mod	= DBM:NewMod(1725, "DBM-Nighthold", nil, 786)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 15057 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 15058 $"):sub(12, -3))
 mod:SetCreatureID(104415)--104731 (Depleted Time Particle). 104676 (Waning Time Particle). 104491 (Accelerated Time particle). 104492 (Slow Time Particle)
 mod:SetEncounterID(1865)
 mod:SetZone()
 --mod:SetUsedIcons(5, 4, 3, 2, 1)--sometimes it was 5 targets, sometimes it was whole raid. even post nerf. have to see
---mod:SetHotfixNoticeRev(12324)
+mod:SetHotfixNoticeRev(15057)
 mod.respawnTime = 29
 
 mod:RegisterCombat("combat")
