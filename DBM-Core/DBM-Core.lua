b23211100916810ad00aadce52a8a4a1683494b6
subversion <svn@deadlybossmods.com>
2016-07-18 09:12:37 +0200

    Omega via svn r15059: More refactoring since LFR had it?\226?\128?\153s own data. inputted data for 5 sequences of LFR. sadly group didn?\226?\128?\153t suck bad enough to get more data!

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15058 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15059 $"):sub(12, -3)),
 	DisplayVersion = "6.2.24 alpha", -- the string that is shown as version
 	ReleaseRevision = 14998 -- the revision of the latest stable version that is available
 }
