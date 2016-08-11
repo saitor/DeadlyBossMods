43f51ab35716e49d28c6a747d0cd81209252d738
subversion <svn@deadlybossmods.com>
2016-08-11 04:52:15 +0200

    Omega via svn r15105: Demon Invasions mod

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15104 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15105 $"):sub(12, -3)),
 	DisplayVersion = "7.0.2 alpha", -- the string that is shown as version
 	ReleaseRevision = 15086 -- the revision of the latest stable version that is available
 }
