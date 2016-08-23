20b0c04669a07b387b3dbcaa6836b33967fb3945
subversion <svn@deadlybossmods.com>
2016-08-23 23:23:56 +0200

    Omega via svn r15156: More work on restriction mode. apply restrictions to emerald nightmare mods and any draenor mods (except iron maidens, the mod is literally neutered without position checks for boat phase. I can only hope blizzard adds an event for boat ending or that mod is totally screwed)

--- DBM-Highmaul/Koragh.lua
+++ DBM-Highmaul/Koragh.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(1153, "DBM-Highmaul", nil, 477)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 14994 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 15156 $"):sub(12, -3))
 mod:SetCreatureID(79015)
 mod:SetEncounterID(1723)
 mod:SetZone()
@@ -296,10 +296,12 @@ function mod:SPELL_AURA_APPLIED(args)
 			timerExpelMagicFel:Start()
 			countdownFel:Start()
 			yellExpelMagicFel:Schedule(11)--Yell right before expire, not apply
-			lastX, LastY = UnitPosition("player")
-			self:Schedule(7, returnPosition, self)
-			if self.Options.HudMapForFel then
-				DBMHudMap:RegisterStaticMarkerOnPartyMember(spellId, "highlight", args.destName, 3, 12, 0, 1, 0, 0.5, nil, nil, 4):Pulse(0.5, 0.5)
+			if not DBM.Options.EnablePatchRestrictions then
+				lastX, LastY = UnitPosition("player")
+				self:Schedule(7, returnPosition, self)
+				if self.Options.HudMapForFel then
+					DBMHudMap:RegisterStaticMarkerOnPartyMember(spellId, "highlight", args.destName, 3, 12, 0, 1, 0, 0.5, nil, nil, 4):Pulse(0.5, 0.5)
+				end
 			end
 		end
 		if self.Options.SetIconOnFel then
