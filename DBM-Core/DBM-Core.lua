d56686c04fb4439f0d99314669ea06ea080a2861
subversion <svn@deadlybossmods.com>
2016-09-08 01:37:53 +0200

    Omega via svn r15195: Forgot to localize the trash mod, oops. well it won?\226?\128?\153t hurt release much. just look silly.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15194 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15195 $"):sub(12, -3)),
 	DisplayVersion = "7.0.5 alpha", -- the string that is shown as version
 	ReleaseRevision = 15192 -- the revision of the latest stable version that is available
 }
