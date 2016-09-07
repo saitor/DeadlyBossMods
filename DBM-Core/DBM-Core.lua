de0f29afd8857ac47dc0d44738512fdd693f813b
subversion <svn@deadlybossmods.com>
2016-09-07 18:35:55 +0200

    Omega via svn r15192: oops, TOC

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15191 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15192 $"):sub(12, -3)),
 	DisplayVersion = "7.0.4 alpha", -- the string that is shown as version
 	ReleaseRevision = 15178 -- the revision of the latest stable version that is available
 }
