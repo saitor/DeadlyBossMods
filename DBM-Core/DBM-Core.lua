ceb81afb208a24cf2aae0f3086903e8d3e876ff2
subversion <svn@deadlybossmods.com>
2016-09-25 10:11:26 +0200

    Omega via svn r15266: Add some additional failsafes to ursok and fix a bug where overwhelm warnings would fail if stack went above 1 Fixed bug where unit name on rend flesh warning wasn?\226?\128?\153t displayed.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15265 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15266 $"):sub(12, -3)),
 	DisplayVersion = "7.0.8 alpha", -- the string that is shown as version
 	ReleaseRevision = 15263 -- the revision of the latest stable version that is available
 }
@@ -419,7 +419,7 @@ local dbmToc = 0
 local isTalkingHeadLoaded = false
 local talkingHeadUnregistered = false
 
-local fakeBWVersion, fakeBWHash = 11, "8d2614b"
+local fakeBWVersion, fakeBWHash = 12, "97ac161"
 local versionQueryString, versionResponseString = "Q:%d-%s", "V:%d-%s"
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
