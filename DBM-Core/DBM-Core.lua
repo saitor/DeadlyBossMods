64a387a4256c030b1ad8d8788b9cacda166e0d07
subversion <svn@deadlybossmods.com>
2016-09-28 09:49:57 +0200

    Omega via svn r15280: Add dragon breath timers

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -41,7 +41,7 @@
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = tonumber(("$Revision: 15279 $"):sub(12, -3)),
+	Revision = tonumber(("$Revision: 15280 $"):sub(12, -3)),
 	DisplayVersion = "7.0.8 alpha", -- the string that is shown as version
 	ReleaseRevision = 15263 -- the revision of the latest stable version that is available
 }
